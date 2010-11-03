91e14eb6e1510e7a69d4235de758750dec88d65c
창하 이 <yielding@yielding-iMac.local>
2010-11-03 18:06:24 +0900

    update plist.cpp

--- app/plist/plist.cpp
+++ app/plist/plist.cpp
@@ -5,8 +5,27 @@ using namespace std;
 
 int main(int argc, char const* argv[])
 {
+  char const* sources[] = {
+    "AppleSupport",
+    "Network",
+    "VPN",
+    "WiFi",
+    "UserDatabases",
+    "CrashReporter",
+    "tmp",
+    "SystemConfigurations",
+    "Caches",
+    "MobileWirelessSync",
+    "Lockdown",
+    "MobileInstallation",
+    "SafeHarbor",
+    NULL
+  };
+
   plist_t arr = plist_new_array();
-  plist_array_append_item(arr, plist_new_string("UserDatabases"));
+  for (int i=0; sources[i] != NULL; ++i)
+    plist_array_append_item(arr, plist_new_string(sources[i]));
+
 
   plist_t dict = plist_new_dict();
   plist_dict_insert_item(dict, "Sources", arr);
@@ -17,8 +36,9 @@ int main(int argc, char const* argv[])
 
   cout << buffer << endl;
 
+  delete [] buffer;
 
-  char* message = 
+  char const* message = 
     "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
     "<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">"
     "<plist version=\"1.0\">"
