1d1a5f483ebccd3845815ddaa14f5da9262b919a
Lee Chang Ha <yielding@gmail.com>
2010-12-10 21:48:28 +0900

    add assign

--- cpp/stl.standard/vector.cpp
+++ cpp/stl.standard/vector.cpp
@@ -1,44 +1,43 @@
-#include "stlexam.h"
-#pragma hdrstop
 #include <vector>
 #include <iostream>
+#include <iterator>
 
 using namespace std;
 
 ostream& operator<< (ostream& out, const vector<int,allocator<int> >& v)
 {
-    copy(v.begin(), v.end(), ostream_iterator<int,char,char_traits<char> >(out, " "));
-    return out;
+  copy(v.begin(), v.end(), ostream_iterator<int,char,char_traits<char> >(out, " "));
+  return out;
 }
 
 int main ()
 {
-    //
-    // Create a vector of doubles, and one of integers.
-    //
-    vector<int,allocator<int> >         vi;
-    int                 i;
+  //
+  // Create a vector of doubles, and one of integers.
+  //
+  vector<int> vi;
+  int i;
 
-    for (i=0; i<10; ++i)
-        //
-        // Insert values before the beginning.
-        //
-        vi.insert(vi.begin(), i);
+  for (i=0; i<10; ++i)
     //
-    // Print out the vector.
+    // Insert values before the beginning.
     //
-    cout << vi << endl;
-    //
-    // Now let's erase half of the elements.
-    //
-    int half = vi.size() / 2;
+    vi.insert(vi.begin(), i);
+  //
+  // Print out the vector.
+  //
+  cout << vi << endl;
+  //
+  // Now let's erase half of the elements.
+  //
+  int half = vi.size() / 2;
 
-    for (i = 0; i < half; ++i)
-        vi.erase(vi.begin());
-    //
-    // Print it out again.
-    //
-    cout << vi << endl;
+  for (i = 0; i < half; ++i)
+    vi.erase(vi.begin());
+  //
+  // Print it out again.
+  //
+  cout << vi << endl;
 
-    return 0;
+  return 0;
 }
