0df44ffc8a4e9387fbc929933e55a3d60429f336
Lee Chang Ha <yielding@gmail.com>
2010-12-20 11:40:55 +0900

    update history file size

--- cpp.boost/ptree/partial_parse.cpp
+++ cpp.boost/ptree/partial_parse.cpp
@@ -4,30 +4,39 @@
 #include <iostream>
 
 using namespace std;
+using boost::property_tree::ptree;
+
+void print_all(ptree& pt)
+{
+}
+
+void print_selected(ptree& pt)
+{
+    ptree::iterator beg = pt.get_child("plist.dict.array.dict").begin();
+    ptree::iterator end = pt.get_child("plist.dict.array.dict").end();
+
+    for (int i=0; beg != end; ++beg, i++)
+    {
+        if (i == 8)
+            break;
+
+        cout << beg->second.data() << endl;
+    }
+}
 
 int main()
 {
-  try
-  {
-    using boost::property_tree::ptree;
-    ptree pt;
-
-    read_xml("History.xml", pt);
-
-    ptree::iterator beg = pt.get_child("plist.dict.array.dict").begin();
-    ptree::iterator end = pt.get_child("plist.dict.array.dict").end();
-    for (int i=0; beg != end; ++beg, i++)
+    try
     {
-      if (i == 8)
-        break;
+        ptree pt;
+        read_xml("history.xml", pt);
+        print_selected(pt);
 
-      cout << beg->second.data() << endl;
     }
-  }
-  catch (std::exception &e)
-  {
-    std::cout << "Error: " << e.what() << "\n";
-  }
+    catch (std::exception &e)
+    {
+        std::cout << "error: " << e.what() << "\n";
+    }
 
-  return 0;
+    return 0;
 }
