09443957f8ab2c6030380cf37a2531c9068d9dd0
Lee Chang Ha <yielding@gmail.com>
2011-01-11 21:25:23 +0900

    add two files for google map-tile analyzer

---
+++ app/parse_maptiles.rb
@@ -0,0 +1,20 @@
+file = ARGV[0] || '/Users/yielding/Maptiles.sql'
+
+dir  = "./maptiles-output"
+Dir.mkdir(dir, 0755) if not Dir.exists?(dir)
+
+File.open(file) { |f|
+  f.readlines { |line| 
+    next unless line =~/^INSERT/
+    fields = line.split(/\(|\)/)[1]
+    zoom, x, y, flags, length, image = fields.split(/\,/)
+    puts "#{zoom}, #{x}, #{y}, #{flags}, #{length}"
+    data = image.sub(/X'/, '').sub(/'$/, '')
+    next if image.length < 128
+    
+    filename = "./#{dir}/#{x},#{y}\@#{zoom}.png"
+    File.open(filename, "w") { |f2|
+      0.step(image.length, 2) { |i| f2 <<"0x#{data[i, 2]}".hex.chr }
+    }
+  }
+}
