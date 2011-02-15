e62a6d451a3e2653913a93a0bbe5eed9ea8360bc
Lee Chang Ha <yielding@gmail.com>
2011-02-15 09:31:58 +0900

    added uni_convert.rb and deleted utf8_source.rb

---
+++ ruby/uni_convert.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby -wKU
+#-*- coding:utf-8 -*-
+
+if ARGV.length == 0
+  puts "uni_convert.rb 스트링"
+  exit
+end
+
+s = ARGV.join
+s.unpack("U*").each_with_index { |b, index|
+  printf("(%X: %s) ", b, s[index])
+  puts if index % 5 == 0
+}
