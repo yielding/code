82cfb1114ed0b138a0238278f5f50d4b3efcedd8
Lee Chang Ha <yielding@gmail.com>
2011-02-16 11:24:35 +0900

    updated memmove test

--- posix.api/test_memmove.cpp
+++ posix.api/test_memmove.cpp
@@ -9,37 +9,50 @@ using namespace std;
 
 class MemmoveTest: public testing::Test
 {
+public:
+  enum { BUF_SIZE = 40960 };
+
 protected:
   virtual void SetUp()
   {
-    // m_s = "abcdefghijklmnopqrstuvwxyz";
+    m_s1 = "abcdefghijklmnopqrstuvwxyz";
+    m_s2 = m_s1;
+
+    for (int i=0; i<BUF_SIZE; ++i) m_buffer[i] = 0;
   }
 
   virtual void TearDown()
   {
   }
 
-  string m_s;
+  string m_s1, m_s2;
+  char   m_buffer[BUF_SIZE];
 };
-
 
 TEST_F(MemmoveTest, Memmove)
 {
-  for (size_t i=0; i<m_s.length(); ++i)
+  for (size_t i=0; i<m_s1.length(); ++i)
   {
-    char c = m_s[0];
-    memmove(&m_s[0], &m_s[1], m_s.length());
-    m_s[m_s.length()-1] = c;
-    cout << m_s << endl;
+    char c = m_s1[0];
+    memmove(&m_s1[0], &m_s1[1], m_s1.length());
+    m_s1[m_s1.length()-1] = c;
+    rotate(m_s2.begin(), m_s2.begin()+1, m_s2.end());
+    ASSERT_TRUE(m_s1 == m_s2);
   }
 }
 
-TEST_F(MemmoveTest, Rotate)
+TEST_F(MemmoveTest, MemmoveSpeed)
 {
-  ASSERT_TRUE(true);
-  for (size_t i=0; i<m_s.length(); ++i)
+  for (int i=0; i<BUF_SIZE; ++i) 
   {
-    rotate(m_s.begin(), m_s.begin()+1, m_s.end());
-    cout << m_s << endl;
+    char c = m_buffer[0];
+    memmove(m_buffer, m_buffer+1, BUF_SIZE);
+    m_buffer[BUF_SIZE-1] = c;
   }
+}
+
+TEST_F(MemmoveTest, RotateSpeed)
+{
+  for (int i=0; i<BUF_SIZE; ++i) 
+    rotate(m_buffer, m_buffer + 1, m_buffer + BUF_SIZE);
 }
