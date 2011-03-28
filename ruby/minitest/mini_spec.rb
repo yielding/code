22d461f66e73bfc82bd9da5610ea099f3cbe8f4b
Lee Chang Ha <yielding@gmail.com>
2011-03-28 18:32:23 +0900

    update

---
+++ ruby/minitest/mini_spec.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby -wKU
+
+require "minitest/spec"
+require "minitest/autorun"
+
+describe "Awesome" do
+  describe 'Class' do
+    it "discovers something AMAZING" do
+      (10 + 10).must_equal 20
+    end
+
+    it "matches something AMAZING" do
+      "vuvuzela".must_match /vuvu/
+    end
+
+    it "raises something AMAZING" do
+      -> { raise }.must_raise(RuntimeError)
+    end
+  end
+end
+
