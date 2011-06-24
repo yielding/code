3f979b1281cc3a43162e359b3a81019982812f7b
Chang Ha Lee <yielding@gmail.com>
2011-06-24 18:40:06 +0900

    update io test code

--- ruby/io/lib/mbdb.rb
+++ ruby/io/lib/mbdb.rb
@@ -1,30 +1,86 @@
 #!/usr/bin/env ruby -wKU
 
+require "pp"
+
 class Mbdb
+  attr_reader :offset, :path
+  attr_reader :mbdb
+
   def initialize(path)
-    @path = path
-    @file = nil    
+    @path, @file, @ok = path, nil, false
+    @mbdb   = {}
+    @offset = 0
   end
 
   def open
-    @file = File.open(@path, "rb")
+    @file   = File.open(@path, "rb")
+    @ok     = @file.read(6).unpack("C*") == [109, 98, 100, 98, 5, 0]
+    @offset = 6
   end
 
   def open?
-    not @file.nil?
-  end
-
-  def path
-     @path 
+    @ok
   end
 
   def close
     File.close(@file) unless @file.nil?
   end
 
-  def read_header
-    @file.seek(0, IO::SEEK_SET)
-    @file.read(6)
+  def process_mbdb
+    while @offset < @file.size
+      fileinfo = {
+        :offset => @offset,
+        :domain => get_string,
+        :path   => get_string,
+        :linktg => get_string,
+        :data_hash => get_string,
+        :unknown1  => get_string,
+        :mode      => get_int2,
+        :unknown2  => get_int4,
+        :unknown3  => get_int4,
+        :user_id   => get_int4,
+        :group_id  => get_int4,
+        :mtime     => get_int4,
+        :atime     => get_int4,
+        :ctime     => get_int4,
+        :file_len  => get_int8,
+        :flag      => get_int1,
+        :num_props => get_int1,
+        :props     => {}
+      }
+
+      1.upto(fileinfo[:num_props]) { fileinfo[:props][get_string] = get_string }
+
+      @mbdb[fileinfo[:offset]] = fileinfo
+    end
+
   end
 
+  def read_record
+  end
+
+  def get_string
+    size = get_int2 
+    return '' if size == 0xFFFF
+
+    @offset += size
+    return @file.read(size)
+  end
+
+  def get_int1; get_int(1) end
+  def get_int2; get_int(2) end
+  def get_int4; get_int(4) end
+  def get_int8; get_int(8) end
+
+  def get_int size
+    @offset += size
+    @file.read(size).each_byte.reduce(0) { |res, b| (res << 8) + b }
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  @db = Mbdb.new "/Users/yielding/code/ruby/io/data/5_Manifest.mbdb"
+  @db.open
+  @db.process_mbdb
+  pp @db.mbdb
 end
--- ruby/io/test/tc_mbdb.rb
+++ ruby/io/test/tc_mbdb.rb
@@ -8,23 +8,26 @@ class TestMbdb < Test::Unit::TestCase
   def setup
     @file1 = File.expand_path(File.dirname(__FILE__)) + "/../data/5_Manifest.mbdb"
     @file1 = "/Users/yielding/code/ruby/io/data/5_Manifest.mbdb"
-    @md  = Mbdb.new @file1
-    @md.open 
+    @db = Mbdb.new @file1
+    @db.open 
   end
 
   def teardown
   end
 
   def test_open
-    assert @md.open?
+    assert @db.open?
   end
 
   def test_path
-    assert_equal(@file1, @md.path)
+    assert_equal(@file1, @db.path)
   end
 
   def test_read_header
-    assert_equal [109, 98, 100, 98, 5, 0], @md.read_header.unpack("C*")
+    assert_equal 6, @db.offset
   end
 
+  def test_process
+    @db.process_mbdb
+  end
 end
