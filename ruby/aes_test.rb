451f5943b61c22b6b5bbd21736b869372f26d58d
Chang Ha Lee <yielding@gmail.com>
2011-09-09 11:22:42 +0900

    add aes test ocde

---
+++ ruby/aes_test.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby -wKU
+#encoding: utf-8
+
+require 'fast-aes'
+
+key = '42#3b%c$dxyT,7a5=+5fUI3fa7352&^:'
+aes = FastAES.new(key)
+
+text = "Hey there, how are you?"
+data = aes.encrypt(text)
+p data
+p aes.decrypt(data)
