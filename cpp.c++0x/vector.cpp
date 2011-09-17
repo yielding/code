05ae19d8b21efc2df99f4a25fd79e9958c3bc23e
yielding <yielding@gmail.com>
2011-09-18 03:00:48 +0900

    ok

--- cpp.c++0x/vector.cpp
+++ cpp.c++0x/vector.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(int argc, char const* argv[])
 {
-  vector<int> v = {1, 2, 3};
+  vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-  for (auto i=v.begin(); i != v.end(); ++i)
-    cout << *i << endl;
+  auto is_even =[](int x) { return x % 2 == 0; };
+  v.erase(remove_if(v.begin(), v.end(), is_even), v.end());
+
+  for (auto x: v) cout << x << " ";
   
   return 0;
 }
