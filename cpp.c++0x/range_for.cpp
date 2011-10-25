faf0d64c8318df35a049f33d9671a5aa747bf6af
Chang Ha Lee <yielding@gmail.com>
2011-10-25 19:00:40 +0900

    updated c++11 example

--- cpp.c++0x/range_for.cpp
+++ cpp.c++0x/range_for.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char const* argv[])
 {
   int a[] = { 1, 2, 3, 4, 5, 6, 7,8, 9, 10 };
+  vector<int> v(a, a + 10);
 
-  for (auto i : a) cout << i;
+  for_each(v.begin(), v.end(), [](int& v) {
+    cout << v;
+    v++;
+  });
+
+  cout << "\n";
+  for (auto i : v) cout << i << " ";
 
   return 0;
 }
