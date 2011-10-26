3f1d38decda450ba65c49fc3ca8e72a7bf8ccba7
Chang Ha Lee <yielding@gmail.com>
2011-10-26 18:22:16 +0900

    added an ARC test

---
+++ objc/arc/MyObj.h
@@ -0,0 +1,17 @@
+//
+//  MyObj.h
+//  test_objc_interface
+//
+//  Created by Lee Chang Ha on 11. 10. 10..
+//  Copyright (c) 2011년 heaven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyObj : NSObject {
+    int result;
+}
+
+- (int) doSomeThing;
+
+@end
---
+++ objc/arc/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  test_objc_interface
+//
+//  Created by Lee Chang Ha on 11. 10. 10..
+//  Copyright (c) 2011년 heaven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MyObj.h"
+
+int main (int argc, const char * argv[])
+{
+  @autoreleasepool {
+    MyObj* obj = [[MyObj alloc] init];
+    
+    // insert code here...
+    NSLog(@"Hello, World! %d", [obj doSomeThing]);
+    
+  }
+  
+  return 0;
+}
+
