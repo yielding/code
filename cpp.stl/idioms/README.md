f04df1b651912ed7f3bc7e575a2c3eda07d96e94
Chang Ha Lee <yielding@gmail.com>
2011-11-04 11:27:14 +0900

    updated

--- cpp.stl/idioms/README.md
+++ cpp.stl/idioms/README.md
@@ -1,4 +1,4 @@
 BUILD
 =====
 * reserve.test.cpp
-  g++-mp-4.6 -std=c++0x reserve.test.cpp -I/opt/local/include -L/opt/local/lib -lboost_chrono-mt -lboost_system-mt
+  g++-mp-4.6 -std=c++0x reserve.test.cpp -lboost_chrono-mt -lboost_system-mt
--- cpp.stl/idioms/reserve.test.cpp
+++ cpp.stl/idioms/reserve.test.cpp
@@ -1,35 +1,64 @@
 #include <boost/chrono/chrono.hpp>
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 using namespace boost::chrono;
 
+int* buffer;
+uint64_t const size = 10000000ul;
+
 void test(vector<uint64_t>& v)
 {
-  uint64_t const size = 10000000ul;
-
   auto t0 = system_clock::now();
   for (uint64_t i=0; i<size; ++i)
-    v.push_back(i);
+    v.push_back(buffer[i]);
+    // copy(buffer, buffer+size, back_inserter(v));
 
   auto t1 = system_clock::now();
-  cout << "elapsed: " << (t1 - t0).count() << endl;
+  cout << "elapsed: " << (t1 - t0).count();
+  cout << "size: " << v.size() << endl;
+}
+
+void test_assign(vector<uint64_t>& v)
+{
+  auto t0 = system_clock::now();
+  v.assign(buffer, buffer+size);
+
+  auto t1 = system_clock::now();
+  cout << "elapsed: " << (t1 - t0).count();
+  cout << "size: " << v.size() << endl;
+}
+
+void alloc()
+{
+  auto t0 = system_clock::now();
+
+  buffer = new int[size];
+  for (uint64_t i=0; i<size; i++)
+    buffer[i] = i;
+
+  auto t1 = system_clock::now();
+  cout << "alloc elapsed: " << (t1 - t0).count() << endl;
 }
 
 int main(int argc, char const* argv[])
 {
-  uint64_t const size = 10000000ul;
+  alloc();
 
-  for(size_t i=0; i<100000;i++)
-    ;
+  vector<uint64_t> v2;
+  test_assign(v2);
 
-  vector<uint64_t> v1;
-  test(v1);
 
   vector<uint64_t> v;
   v.reserve(size);
   test(v);
+
+  vector<uint64_t> v1;
+  test(v1);
+
+  delete [] buffer;
 
   return 0;
 }
