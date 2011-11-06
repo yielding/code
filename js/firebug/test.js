cb7464c6b9fd2c84e3f543028396a08ebe7cd01b
Chang Ha Lee <yielding@gmail.com>
2011-11-06 19:01:20 +0900

    ..

--- js/firebug/test.js
+++ js/firebug/test.js
@@ -1,28 +1,28 @@
 //implementing a stack, create a object
-function Stack(){
+function Stack() {
   this.list = [];
 }
 
-Stack.prototype.push = function(value){
+Stack.prototype.push = function(value) {
   this.list.push(value);
 }
 
-Stack.prototype.pop = function(){
+Stack.prototype.pop = function() {
   return this.list.pop();
 }
 
-Stack.prototype.size = function(){
+Stack.prototype.size = function() {
   return this.list.length;
 }
 
 //create a custom toString function,
 //event though Array object has its own
 //we want to customize it a little bit
-Stack.prototype.toString = function(){
+Stack.prototype.toString = function() {
   var temp = '[ ';
-  for(var i = 0, length = this.list.length; i < length; i++){
+  for (var i = 0, length = this.list.length; i < length; i++) {
     temp += this.list[i];
-    if(length - i != 1){
+    if (length - i != 1) {
       temp += ', ';
     }
   }
