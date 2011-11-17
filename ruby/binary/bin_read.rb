da90c4eb52d9af733b3f3349a803719706715a82
Chang Ha Lee <yielding@gmail.com>
2011-11-17 23:27:20 +0900

    ...

--- ruby/binary/bin_read.rb
+++ ruby/binary/bin_read.rb
@@ -4,21 +4,19 @@ require 'bindata'
 
 class Point < BinData::Record
   endian :little
-
   uint16 :x 
   uint16 :y 
 end
 
-# big endian: 제일 큰 숫자가 저장 장치의 작은 번지에 먼저 쓰여진다.
-#             사람이 글을 읽는 방식
 class Points < BinData::Record
   endian :little
-  array  :points, :initial_length => 128 do
-    uint16 :x
-    uint16 :y
+  array  :points, :initial_length => 2 do
+    uint8 :x
+    uint8 :y
   end
 end
 
 io = File.open("log.bin")
 pt = Points.read(io)
+p pt.snapshot
 pt.points.each { |pt|  puts "x: #{pt.x}, y: #{pt.y}" }
