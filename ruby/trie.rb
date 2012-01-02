575327faa6e28c99aa9ff9819af7409230ae8ce7
Chang Ha Lee <yielding@gmail.com>
2012-01-03 01:59:11 +0900

    add trie.rb

---
+++ ruby/trie.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby -wKU
+
+class Trie
+  def initialize
+    @trie = Hash.new()
+  end
+ 
+  def build(str) 
+    node = @trie    
+    str.each_char do |ch| # leech
+      prev = node
+      node = node[ch]
+      if node == nil 
+        prev[ch] = Hash.new()
+        node = prev[ch]
+      end
+    end
+  end
+ 
+  def find(str) 
+    node = @trie
+    str.each_char do |ch|
+      node = node[ch]
+      return false if node.nil?
+    end
+    true 
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  t = Trie.new
+  t.build("leech")
+  t.build("kamin")
+
+  p t.find("kamin")
+  p t.find("leeech")
+  p t.find("leech")
+end
