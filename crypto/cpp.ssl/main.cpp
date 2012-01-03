2d52c85d824c7ec80dccaa2102c417df49dbcbb4
Chang Ha Lee <yielding@gmail.com>
2012-01-03 23:05:14 +0900

    minor updates

---
+++ crypto/cpp.ssl/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+  cout << "openssl porting main()" << endl;
+
+  return 0;
+}
---
+++ crypto/cpp.ssl/ossl.h
@@ -0,0 +1,7 @@
+#include <openssl/opensslv.h>
+#include <openssl/ssl.h>
+#include <openssl/bio.h>
+#include <openssl/dh.h>
+#include <openssl/bn.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
