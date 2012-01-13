bd9159028e39e8ee81158bfb5db4db6a2b79cc69
Chang Ha Lee <yielding@gmail.com>
2012-01-13 18:34:31 +0900

    ...

---
+++ ruby/kids/game.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby -wKU
+
+require "gosu"
+
+class MyGame < Gosu::Window
+  def initialize
+    super(300, 400, false)
+  end
+
+  def update
+  end
+
+  def draw
+  end
+end
+
+window = MyGame.new
+window.show
