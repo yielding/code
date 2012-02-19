75477d32b09b8c137c3d160d15c4505262c22fd6
Chang Ha Lee <yielding@gmail.com>
2012-02-20 08:10:01 +0900

    ...

--- cpp.boost/phoenix2/new_delete_tests.cpp
+++ cpp.boost/phoenix2/new_delete_tests.cpp
@@ -17,30 +17,26 @@ struct X
 {
   X(int, int, int) { cout << "new X(int, int, int)" << endl; ++n; }
   X() { cout << "new X" << endl; ++n; }
-  ~X() { cout << "delete X" << endl; --n; }
+ ~X() { cout << "delete X" << endl; --n; }
 };
 
 int main()
 {
   vector<X*> v(10);
 
-  {
-    // lazy new_ for_each의 loop 안에서 호출된다.
-    for_each(v.begin(), v.end(), arg1 = new_<X>());
-    for_each(v.begin(), v.end(), delete_(arg1));
+  // lazy new_ for_each의 loop 안에서 호출된다.
+  for_each(v.begin(), v.end(), arg1 = new_<X>());
+  for_each(v.begin(), v.end(), delete_(arg1));
 
-    for_each(v.begin(), v.end(), arg1 = new_<X>(1, 2, 3));
-    for_each(v.begin(), v.end(), delete_(arg1));
-  }
+  for_each(v.begin(), v.end(), arg1 = new_<X>(1, 2, 3));
+  for_each(v.begin(), v.end(), delete_(arg1));
 
-  {
-    //using boost::shared_ptr;
-    typedef boost::shared_ptr<X> Xsp;
+  //using boost::shared_ptr;
+  typedef boost::shared_ptr<X> Xsp;
 
-    vector<Xsp> v1(10);
-    for_each(v1.begin(), v1.end(),
-        arg1 = construct<Xsp>(new_<X>()));
-  }
+  vector<Xsp> v1(10);
+  for_each(v1.begin(), v1.end(),
+      arg1 = construct<Xsp>(new_<X>()));
 
   BOOST_TEST(n == 0);
   return boost::report_errors();
