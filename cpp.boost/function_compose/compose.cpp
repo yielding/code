693bb953150dcd5c3a8bd2fd3926564207c85866
Chang Ha Lee <yielding@gmail.com>
2012-03-08 11:05:59 +0900

    added function_compose sample for the pre-requisite for Boost.Proto study

---
+++ cpp.boost/function_compose/compose.cpp
@@ -0,0 +1,98 @@
+#include <boost/utility/result_of.hpp>
+#include <iostream>
+
+struct _;
+ 
+template<typename Signature>
+struct composed_fn;
+ 
+// An improved composed_fn template that uses the TR1 result_of
+// protocol to declare and compute function return types.
+template<typename Fn1, typename F>
+struct composed_fn<Fn1(F)>
+{
+    // Recursively compose all nested function types to build
+    // a composed function object out of them.
+    typedef composed_fn<F> Fn2;
+
+    Fn1 fn1;
+    Fn2 fn2;
+ 
+    explicit composed_fn(Fn1 f1 = Fn1(), Fn2 f2 = Fn2())
+      : fn1(f1), fn2(f2)
+    {}
+ 
+    template<typename Signature>
+    struct result;
+ 
+    template<typename This, typename T>
+    struct result<This(T)>
+    {
+        typedef typename boost::result_of<Fn2(T)>::type U;
+        typedef typename boost::result_of<Fn1(U)>::type type;
+    };
+ 
+    template<typename T>
+    typename result<composed_fn(T)>::type
+    operator()(T x) const
+    //auto operator()(T x) const -> decltype(fn1(fn2(x)))
+    {
+        return fn1(fn2(x));
+    }
+};
+ 
+// This specialization ends the recursion begun on line 21.
+// "composed_fn<Fn(_)> fn; fn(3);" is equivalent to "Fn fn; fn(3);"
+template<typename Fn>
+struct composed_fn<Fn(_)> : Fn {};
+ 
+// TRICKSY!!! Read below for why this specialization is necessary.
+template<typename Fn>
+struct composed_fn<Fn *> : composed_fn<Fn> {};
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+struct inc_t
+{   
+  template<typename Signature>
+  struct result;
+ 
+  template<typename This, typename T>
+  struct result<This(T)> { typedef T type; };
+
+  template <typename T> 
+  T operator()(T x) const { return x + 1; }
+};
+
+struct square_t
+{
+  template<typename Signature>
+  struct result;
+ 
+  template<typename This, typename T>
+  struct result<This(T)> { typedef T type; };
+
+  template <typename T> 
+  T operator()(T x) const { return x * x; }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+  composed_fn< inc_t(square_t(inc_t(square_t(_)))) > fn;
+  //composed_fn< square_t(inc_t(_)) > fn;
+
+  cout << fn(3) << endl;
+  
+  return 0;
+}
