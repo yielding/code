6bbb2c5e8e9cc63766532d3614d9ad8b3a07ecb7
Chang Ha Lee <yielding@gmail.com>
2012-03-15 01:16:56 +0900

    ...

---
+++ cpp.boost/proto/fill_map3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+#include <boost/proto/proto.hpp>
+
+namespace proto = boost::proto;
+//
+//
+//   After calculator
+//
+//
+int main(int argc, char const* argv[])
+{
+    return 0;
+}
