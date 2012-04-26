ed64fc55a9e6fcd250ed3ddb083657d179796faa
Chang Ha Lee <yielding@gmail.com>
2012-04-26 10:15:04 +0900

    added work

--- app/scp/scp/develop/src/SSHHostList.cpp
+++ app/scp/scp/develop/src/SSHHostList.cpp
@@ -1,9 +1,7 @@
-#include <boost/algorithm/string.hpp>
-#include <boost/format.hpp>
+#include "SSHHostList.h"
+
 #include <boost/fusion/include/adapt_struct.hpp>
-#include <boost/fusion/include/io.hpp>
 #include <boost/spirit/include/qi.hpp>
-#include <boost/tuple/tuple.hpp>
 
 #include <vector>
 #include <iostream>
@@ -11,38 +9,38 @@
 #include <sstream>
 #include <stdexcept>
 
-using std::string;
-
-namespace utility { namespace comm { namespace ssh {
-    struct HostInfo { string host, id, pw; };
-}
-}
-}
+using namespace std;
+using namespace boost;
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-// NOTICE: It seems that it should be outside of namespace
+//
 //
 ////////////////////////////////////////////////////////////////////////////////
+namespace impl {
+    struct HostInfo 
+    { 
+        string host, id, pw; 
+    };
+}
+
+//
+// BOOST_FUSION_ADAPT_STRUCT cannot be located inside the namespace
+//
 BOOST_FUSION_ADAPT_STRUCT(
-    utility::comm::ssh::HostInfo,
+    impl::HostInfo,
     (string, host) (string, id) (string, pw)
 )
 
-namespace utility { namespace comm { namespace ssh {
-////////////////////////////////////////////////////////////////////////////////
-//
-// 
-//
-////////////////////////////////////////////////////////////////////////////////
-namespace    qi = boost::spirit::qi;
-namespace ascii = boost::spirit::ascii;
+namespace impl { 
+
+namespace    qi = spirit::qi;
+namespace ascii = spirit::ascii;
 
 template <typename Iterator>
 struct HostInfoParser : qi::grammar<Iterator, HostInfo(), ascii::space_type>
 {
-    HostInfoParser()
-        : HostInfoParser::base_type(start)
+    HostInfoParser() : HostInfoParser::base_type(start)
     {
         using namespace qi;
         qs    %= lexeme['"' >> +(char_ - '"') >> '"'];
@@ -53,19 +51,22 @@ struct HostInfoParser : qi::grammar<Iterator, HostInfo(), ascii::space_type>
     qi::rule<Iterator, HostInfo(), ascii::space_type> start;
 };
 
-} // end of ssh
-} // end of comm
-} // end of utility
+} // end of impl
 
-class SSHHosts
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+class SSHHostListImpl
 {
 public:
     typedef string::const_iterator Iterator;
 
 public:
-    bool init_with_file(string&& file)
+    bool init_with_file(string const& file)
     {
-        std::ifstream ifs; 
+        ifstream ifs; 
         ifs.open(file.c_str());
         if (!ifs.is_open())
             throw std::runtime_error("can't open file");
@@ -73,7 +74,7 @@ public:
         return init_with_stream(ifs);
     }
 
-    bool init_with_string(string&& str)
+    bool init_with_string(string const& str)
     {
         std::stringstream ss(str);
         return init_with_stream(ss);
@@ -81,7 +82,7 @@ public:
 
     bool init_with_stream(std::istream& stream)
     {
-        using namespace utility::comm::ssh;
+        using namespace impl;
 
         HostInfoParser<Iterator> g;
         string line;
@@ -105,7 +106,7 @@ public:
     auto nth(int index) -> boost::tuple<string, string, string>
     {
         if (index >= m_hosts.size() || index < 0)
-            throw std::runtime_error("woring index reference");
+            throw std::runtime_error("wrong index reference");
 
         auto& h = m_hosts[index];
         return boost::make_tuple(h.host, h.id, h.pw);
@@ -113,35 +114,85 @@ public:
 
 private:
     template<typename Iterator>
-    auto parse_line(utility::comm::ssh::HostInfoParser<Iterator>& g, string const& line, 
-            utility::comm::ssh::HostInfo& host) -> bool
+    auto parse_line(impl::HostInfoParser<Iterator>& g, string const& line, 
+            impl::HostInfo& host) -> bool
     {
         Iterator it = line.begin(), 
                 end = line.end();
-        auto result = phrase_parse(it, end, g, boost::spirit::ascii::space, host); 
+        auto result = phrase_parse(it, end, g, spirit::ascii::space, host); 
         return (result && it == end);
     }
 
 private:
-    std::vector<utility::comm::ssh::HostInfo> m_hosts;
+    vector<impl::HostInfo> m_hosts;
 };
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+SSHHostList::SSHHostList()
+{
+    _pimpl = new SSHHostListImpl();
+}
+
+SSHHostList::~SSHHostList()
+{
+    delete _pimpl;
+}
+
+bool SSHHostList::init_with_file(string const& file)
+{
+    return _pimpl->init_with_file(file);
+}
+
+bool SSHHostList::init_with_string(string const& str)
+{
+    return _pimpl->init_with_string(str);
+}
+
+bool SSHHostList::init_with_stream(std::istream& stream)
+{
+    return _pimpl->init_with_stream(stream);
+}
+
+uint32_t SSHHostList::count() const
+{
+    return _pimpl->count();
+}
+
+boost::tuple<string, string, string>
+SSHHostList::nth(int index)
+{
+    return _pimpl->nth(index);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+#if 0
+
+#include <boost/format.hpp>
 
 int main(int argc, char const* argv[])
 {
-    using namespace std;
     using namespace boost;
 
     auto filename = "/Users/yielding/.passwd";
     auto string_  = "host { \"127.0.0.1\", \"y12lding\", \"abc\" }\n"
                     "host { \"127.0.0.2\", \"y12lding\", \"abc\" }\n"
                     "host { \"127.0.0.3\", \"y12lding\", \"abc\" }";
-    SSHHosts h;
-    h.init_with_string(string_); // h.init_with_file(filename);
-    for (int i=0; i<h.count(); i++)
+    SSHHostList list;
+    list.init_with_string(string_); // h.init_with_file(filename);
+    for (int i=0; i<list.count(); i++)
     {
-        string host, id, pw; tie(host, id, pw) = h.nth(i);
+        string host, id, pw; tie(host, id, pw) = list.nth(i);
         cout << str(format("host: %s id : %s pw: %s\n") % host % id % pw);
     }
 
     return 0;
 }
+#endif
