eb4687afc2c0e592eb78a14cf85ce7d63dc07bf0
Chang Ha Lee <yielding@gmail.com>
2012-05-24 20:56:35 +0900

    .....

--- posix.api/memmove.cpp
+++ posix.api/memmove.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <cstring>
 #include <string>
 
@@ -10,10 +11,12 @@ int main(int argc, char const* argv[])
 
   for (size_t i=0; i<m_s.length(); ++i)
   {
-    char c = m_s[0];
-    memmove(&m_s[0], &m_s[1], m_s.length());
-    m_s[m_s.length()-1] = c;
-    // rotate(m_s.begin(), m_s.begin()+1, m_s.end());
+    // char c = m_s[0];
+    // memmove(&m_s[0], &m_s[1], m_s.length());
+    // m_s[m_s.length()-1] = c;
+
+    rotate(m_s.begin(), m_s.begin()+1, m_s.end());
+
     cout << m_s << endl;
   }
 
