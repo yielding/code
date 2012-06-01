d6f4cd42a4c3e699a8f1b7b523687561edb9ca22
Chang Ha Lee <yielding@gmail.com>
2012-06-01 19:57:32 +0900

    .update jukebox.. and plz confer time.c

--- ruby/mruby/jukebox/jukebox.cpp
+++ ruby/mruby/jukebox/jukebox.cpp
@@ -4,35 +4,93 @@
 #include <mruby.h>
 #include <mruby/array.h>
 #include <mruby/variable.h>
+#include <mruby/data.h>
 #include <mruby/proc.h>
 #include <mruby/compile.h>
 
 using namespace std;
-////////////////////////////////////////////////////////////////////////////////
-//
-// CDJukebox
-//
-////////////////////////////////////////////////////////////////////////////////
-namespace {
-  
-//mrb_value cd_alloc(mrb_state* mrb, mrb_value klass)
-//{
-  //CDJukebox* jukebox;
-//}
 
-mrb_value cd_initialize(mrb_state* mrb, mrb_value self, mrb_value unit)
+namespace {
+////////////////////////////////////////////////////////////////////////////////
+//
+// native CDJukebox
+//
+////////////////////////////////////////////////////////////////////////////////
+struct CDJukebox 
 {
+  CDJukebox()  { cout << "CDJukebox constructed\n"; }
+  ~CDJukebox() { cout << "CDJukebox destructed\n";  }
+
+  void seek(int disk, int track)
+  {
+  }
+
+  double avg_seek_time()
+  {
+  }
+
+  void uint_id(int id)
+  {
+    unit_id = id;
+  }
+
+  int   status;
+  int   request;
+  void* data;
+  char  pending;
+  int   unit_id;
+  void* stats;
+};
+ 
+static struct RClass* cCDJukebox;
+
+void jukebox_deleter(mrb_state* mrb, void* p)
+{
+  auto jukebox = (CDJukebox*)p;
+  delete jukebox;
+
+  cout << "jukebox_deleter is called\n";
+}
+
+struct mrb_data_type jukebox_type = {
+  "CDJukebox", jukebox_deleter
+};
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// ruby wrapper for CDJukebox
+// TODO: time.c 참고
+//
+////////////////////////////////////////////////////////////////////////////////
+mrb_value cd_initialize(mrb_state* mrb, mrb_value self)
+{
+  auto jb = (CDJukebox*)mrb_get_datatype(mrb, self, &jukebox_type);
+  if (jb)
+    jukebox_deleter(mrb, jb);
+
+  if (mrb->ci->argc == 0)
+  {
+  }
+  else
+  {
+    // mrb_get_args(mrb, "oo....", );
+    // jb
+  }
+
+  jb = new CDJukebox;
+
+  DATA_PTR(self)  = (void*)jb;
+  DATA_TYPE(self) = &jukebox_type;
+
   return self;
 }
 
-static struct RClass* cCDJukebox;
 void init_jukebox(mrb_state* mrb)
 {
   cCDJukebox = mrb_define_class(mrb, "CDJukebox", mrb->object_class);
 
   mrb_define_method(mrb, cCDJukebox, "initialize", cd_initialize, ARGS_REQ(1));
 }
-
 
 }
 
