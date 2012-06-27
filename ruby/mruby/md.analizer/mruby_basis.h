323e9061eab540642dca6e167a497b2d0098539f
Chang Ha Lee <yielding@gmail.com>
2012-06-28 08:13:09 +0900

    ..

---
+++ ruby/mruby/md.analizer/mruby_basis.h
@@ -0,0 +1,14 @@
+#ifndef MRUBY_BASIS_H
+#define MRUBY_BASIS_H
+
+#include <mruby.h>
+#include <mruby/data.h>
+#include <mruby/hash.h>
+#include <mruby/string.h>
+#include <mruby/class.h>
+#include <mruby/variable.h>
+
+#include <mruby/proc.h>
+#include <mruby/compile.h>
+
+#endif
