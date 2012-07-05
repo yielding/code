04540134bc540d3fcc7ef792b3c29f777c242045
Chang Ha Lee <yielding@gmail.com>
2012-07-05 19:59:10 +0900

    ...

--- ruby.motion/GraphicsSample/app/sample_for_uiimage.rb
+++ ruby.motion/GraphicsSample/app/sample_for_uiimage.rb
@@ -1,12 +1,8 @@
-class DrawImageTest < UIView 
-  def initWithFrame(frame)
-    super 
+#class DrawImageTest < UIImageView 
+class DrawImageTest < UIImageView 
+  def initWithImage(image)
+    @image = image if super.init
     self
-  end
-
-  def initWithImage(image, frame)
-    @image = image
-    initWithFrame(frame)
   end
 
   def drawRect(rect)
@@ -17,9 +13,9 @@ end
 class SampleForUIImage < UIViewController
   def viewDidLoad
     super
-    image = UIImage.imageNamed("doc.jpg")
-    test = DrawImageTest.alloc.initWithImage(image, view.bounds)
-    test.frame = view.bounds
+    image = UIImage.imageNamed("dog.jpg")
+    test  = DrawImageTest.alloc.initWithImage(image)
+    test.frame = self.view.bounds
     test.autoresizingMask =
       UIViewAutoresizingFlexibleWidth or 
       UIViewAutoresizingFlexibleHeight
