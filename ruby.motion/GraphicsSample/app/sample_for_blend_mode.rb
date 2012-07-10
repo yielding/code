36abdb89e23cf8936b4909220ed1e6fcf4630ee0
Chang Ha Lee <yielding@gmail.com>
2012-07-10 09:50:38 +0900

    .sdjfklskld

--- ruby.motion/GraphicsSample/app/sample_for_blend_mode.rb
+++ ruby.motion/GraphicsSample/app/sample_for_blend_mode.rb
@@ -3,6 +3,7 @@ class BlendModeTest < UIView
     if super
       @back_iamge = UIImage.imageNamed("back.png")
       @fore_image = UIImage.imageNamed("dog.png")
+      @blend_mode = KCGBlendModeNormal
       new_frame = self.frame
       new_frame.size = @fore_image.size
       self.frame = new_frame
@@ -26,6 +27,27 @@ end
 class SampleForBlendMode < UIViewController
   def viewDidLoad
     super
+    t = BlendModeTest.alloc.init 
+    t.center = self.view.center
+    t.autoresizingMask = UIViewAutoresizingFlexibleTopMargin or
+                         UIViewAutoresizingFlexibleBottomMargin
+    view.addSubview(t)
+    label = UILabel.alloc.init 
+    label.frame = [[0, view.bounds.size.height - 100], [view.bounds.size.width, 20]]
+    label.autoresizingMask = UIViewAutoresizingFlexibleTopMargin or
+                             UIViewAutoresizingFlexibleBottomMargin
+    label.textAlignment = UITextAlignmentCenter
+    view.addSubview(label)
+    change_label
 
+    back_image = UIImage.imageNamed("back.png")
+    back_iview = UIImageView.alloc.initWithImage(back_image)
+    back_iview.frame = [[0, 0], [90, 83]]
+
+    # back_iview.frame = [[320 - 90, 0], [90, 83]]
+
+  end
+
+  def change_label
   end
 end
--- shell/enscript.md
+++ shell/enscript.md
@@ -1,1 +1,1 @@
-enscript -2rG --highlight=fortran --color=1
+enscript -2rG --highlight=python --color=1 --line-numbers --margins=2:10::0 -p - ~/iphone-dataprotection/python_scripts/nand/nand.py | pstopdf -i -o ~/out.pdf
