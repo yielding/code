d95a344256bc3872c01934da1639d73623db0b52
Chang Ha Lee <yielding@gmail.com>
2012-07-11 12:47:39 +0900

    sdjfklsjfkl

---
+++ ruby.motion/ch6/app/sample_for_responder_chain.rb
@@ -0,0 +1,15 @@
+class TouchableLabelForResponder < UILabel 
+  def init
+    if super.init 
+
+    end
+    self
+  end
+end
+
+class SampleForResponderChain < UIViewController
+  def viewDidLoad
+    super
+    
+  end
+end
