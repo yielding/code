4d6273483276beafcefb43b44d35c6a619ad5190
Chang Ha Lee <yielding@gmail.com>
2012-07-16 13:01:44 +0900

    sdjkfsjklf

--- ruby.motion/ch6/app/root_view_controller.rb
+++ ruby.motion/ch6/app/root_view_controller.rb
@@ -3,6 +3,7 @@ class RootViewController < UITableViewController
     super
     self.title = "Menu"
     @items = [
+      "SampleForPinch",
       "SampleForMotion",
       "SampleForSlide",
       "SampleForTripleTap",
