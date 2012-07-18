ca62e8abb40cc49235bef5a052560a94f0e96432
Chang Ha Lee <yielding@gmail.com>
2012-07-18 13:44:50 +0900

    sdjfklsdjklf

---
+++ app/nand/nand/nand/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  nand
+//
+//  Created by 창하 이 on 12. 7. 18..
+//  Copyright (c) 2012년 yielding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
