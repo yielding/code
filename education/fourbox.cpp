1d35cf15ece4b54d2174c06e910766c019ba2d02
Chang Ha Lee <yielding@gmail.com>
2012-07-19 12:49:16 +0900

    sjdkfljksdlf

--- education/fourbox.cpp
+++ education/fourbox.cpp
@@ -4,8 +4,7 @@
 #include <string>
 #include <vector>
 #include <tuple>
-
-#include <boost/algorithm/string.hpp>
+#include <sstream>
 
 using namespace std;
 
@@ -16,11 +15,8 @@ struct coord
 
   bool operator<(coord const& rhs) const
   {
-    if (x > rhs.x) 
-      return false;
-
-    if (x == rhs.x)
-      return y < rhs.y;
+    if (x > rhs.x)  return false;
+    if (x == rhs.x) return y < rhs.y;
 
     return true;
   }
@@ -36,17 +32,11 @@ public:
 public:
   FourBox(string in)
   {
-    // 1. identify four lines
-    vector<string> lines; 
-    boost::split(lines, in, boost::is_any_of("\n"));
-    vector<rect_t> rects;
-
-    // 2. identify four coordinates for each line
-    for (auto it=lines.begin(); it!=lines.end(); ++it)
+    stringstream ss(in);
+    for (int i=0; i<4; i++)
     {
-      auto& line = *it;
-      int x1, x2, y1, y2;
-      sscanf(line.c_str(), "%d %d %d %d", &x1, &y1, &x2, &y2);
+      int x1, y1, x2, y2; ss >> x1 >> y1 >> x2 >> y2;
+
       _rects.push_back(make_tuple(x1, y1, x2, y2));
     }
   }
@@ -72,21 +62,11 @@ private:
 
 int main(int argc, const char *argv[])
 {
-  string in1 = 
-    "0 0 200 200\n"
-    "0 0 200 200\n"
-    "0 0 200 200\n"
-    "0 0 200 200";
-
+  string in1 = "0 0 200 200    0 0 200 200    0 0 200 200    0 0 200 200";
   FourBox boxes1(in1);
   assert(boxes1.area() == 40000);
 
-  string in2 = 
-    "1 2 4 4\n"
-    "2 3 5 7\n"
-    "3 1 6 5\n"
-    "7 3 8 6";
-
+  string in2 = "1 2 4 4    2 3 5 7    3 1 6 5     7 3 8 6";
   FourBox boxes2(in2);
   assert(boxes2.area() == 26);
 
---
+++ ruby.motion/ch8/app/root_view_controller.rb
@@ -0,0 +1,43 @@
+class RootViewController < UITableViewController
+  def viewDidLoad
+    super
+    self.title = "메뉴"
+    @items = [
+      "SampleForTextView"
+    ]
+  end
+
+  def viewWillAppear(animated)
+    super animated
+    navigationController.setNavigationBarHidden(false, animated:false)
+    navigationController.setToolbarHidden(false, animated:false)
+
+    UIApplication.sharedApplication.statusBarStyle = UIStatusBarStyleDefault
+    navigationController.navigationBar.barStyle = UIBarStyleDefault
+    navigationController.navigationBar.translucent = false
+    navigationController.toolbar.barStyle = UIBarStyleDefault
+    navigationController.toolbar.translucent = false
+
+    UIView.setAnimationsEnabled(true)
+  end
+
+  def tableView(tv, numberOfRowsInSection: section)
+    @items.size 
+  end
+
+  CELLID = "Cell"
+  def tableView(tv, cellForRowAtIndexPath:ip)
+    cell = tv.dequeueReusableCellWithIdentifier(CELLID) || begin 
+      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:CELLID)
+      cell
+    end
+
+    cell.textLabel.text = @items[ip.row].gsub(/SampleFor/, '')
+    cell
+  end
+
+  def tableView(tv, didSelectRowAtIndexPath:ip)
+   vc = Object.const_get(@items[ip.row]).alloc.init 
+   navigationController.pushViewController(vc, animated:true) unless vc.nil?
+  end
+end
