7e453554b4357369ee8510006f42640a6581cc46
Chang Ha Lee <yielding@gmail.com>
2012-09-03 10:58:59 +0900

    ...

---
+++ education/fourbox.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def area(boxes):
+    s = set([])
+    for x1, y1, x2, y2 in boxes:
+        for x in xrange(x1, x2):
+            for y in xrange(y1, y2):
+                s.add((x, y))
+
+    return len(s)
+
+boxes = [[0, 0, 200, 200]] * 4
+print area(boxes)
+
+boxes = [[1, 2, 4, 4], [2, 3, 5, 7], [3, 1, 6, 5], [7, 3, 8, 6]]
+print area(boxes)
