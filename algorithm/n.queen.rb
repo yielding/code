1b2c483e4bf05e4074f5d2c83a09a8fd753ae5aa
Chang Ha Lee <yielding@gmail.com>
2012-09-06 22:06:42 +0900

    ..

--- algorithm/n.queen.rb
+++ algorithm/n.queen.rb
@@ -1,10 +1,8 @@
 require "set"
 
-n = 8
 a = [*0...8]
-a.permutation.each { |cd| 
-  s = [ Set.new(cd.zip(a).map { |e| e.reduce(:+) }),
-        Set.new(cd.zip(a).map { |e| e.reduce(:-) }) ]
-
-  p cd if s.all? { |x| x.size == n }
+a.permutation.each { |c| 
+  s = [ Set.new(c.zip(a).map { |e| e.reduce(:+) }), 
+        Set.new(c.zip(a).map { |e| e.reduce(:-) }) ]
+  p c if s.all? { |x| x.size == 8 }
 }
