e51e9778c886ef2d1d063c597afe5d126561d8ba
Chang Ha Lee <yielding@gmail.com>
2012-09-08 07:16:25 +0900

    ...

---
+++ algorithm/8.queens.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from functools import partial
+import operator
+
+def each_cons_iter(listin, n): 
+    i = 0
+    while (i<len(listin)-n+1):
+        yield listin[i:i+n]
+        i += 1
+
+each_cons = partial(each_cons_iter, n = 5)
+
+def product(l):
+  return reduce(operator.mul, l)
+
+a = [i for i in range(1, 10)]
+b = [i for i in each_cons(a)]
+
+print b
+print map(product, b)
+print max(map(product, b))
