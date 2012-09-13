d8660de49409e782e7430b5521ecdb5030269a3a
Chang Ha Lee <yielding@gmail.com>
2012-09-13 20:44:49 +0900

    ...

--- algorithm/euler/15.routes-dynamic.rb
+++ algorithm/euler/15.routes-dynamic.rb
@@ -3,9 +3,7 @@
 size = 21
 grid = [[0] * size] * size
 
-0.upto(20) { |i| 
-  grid[size-1][i] = grid[i][size-1] = 1
-}
+0.upto(20) { |i| grid[size-1][i] = grid[i][size-1] = 1 }
 
 (size-2).downto(0) { |i|
   (size-2).downto(0) { |j| grid[i][j] = grid[i+1][j] + grid[i][j+1] }
