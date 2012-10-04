fcd62066d981b915546ab838253bbec6eca091f0
Chang Ha Lee <yielding@gmail.com>
2012-10-05 01:44:12 +0900

    ...

---
+++ algorithm/euler/029.uniq.terms.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby19
+
+=begin
+res = []
+for a in 2..100
+  for b in 2..100
+    res << a ** b
+  end
+end
+p res.uniq.size
+=end
+
+a = [*2..100]
+p a.product(a).map {|a, b| a ** b}.uniq.size
