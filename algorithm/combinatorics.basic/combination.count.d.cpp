759d13a87effa200aab14bbec55bbdfe2d817ecc
Chang Ha Lee <yielding@gmail.com>
2012-10-07 21:08:18 +0900

    ...

--- algorithm/combinatorics.basic/combination.count.d.cpp
+++ algorithm/combinatorics.basic/combination.count.d.cpp
@@ -10,14 +10,10 @@ uint64_t C(uint64_t n, uint64_t k)
   for (int i=0; i<=n; i++) cache[i].resize(n+1, 0);
 
   for (int i=0; i<=n; i++)
-  {
-    for (int j=0; j<=std::min<int>(i, k); j++) {
-      if (j == 0 || j == i) 
-        cache[i][j] = 1;
-      else
-        cache[i][j] = cache[i-1][j-1] + cache[i-1][j];
-    }
-  }
+    for (int j=0; j<=std::min<int>(i, k); j++)
+        cache[i][j] = (j == 0 || j == i) 
+          ? 1
+          : cache[i-1][j-1] + cache[i-1][j];
 
   return cache[n][k];
 }
--- algorithm/combinatorics.basic/combination.count.r.cpp
+++ algorithm/combinatorics.basic/combination.count.r.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 uint64_t C(uint64_t n, uint64_t k)
 {
-  if (k == n || k == 0)
-    return 1;
-
-  return C(n-1, k-1) + C(n-1, k);
+  return (k == n || k == 0)
+    ? 1
+    : C(n-1, k-1) + C(n-1, k);
 }
 
 int main(int argc, const char *argv[])
