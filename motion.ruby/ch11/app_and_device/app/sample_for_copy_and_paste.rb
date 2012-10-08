e2bf587e02646820023fe7f2063e0eee5b09466a
Chang Ha Lee <yielding@gmail.com>
2012-10-09 01:55:45 +0900

    ...

--- motion.ruby/ch11/app_and_device/app/sample_for_copy_and_paste.rb
+++ motion.ruby/ch11/app_and_device/app/sample_for_copy_and_paste.rb
@@ -1,25 +1,20 @@
-
 class SampleForCopyAndPaste < UIViewController
-  def viewDidLoad
-    super
-  end
-
   def viewWillAppear animated
     super
     self.view.backgroundColor = UIColor.whiteColor
 
     image = UIImage.imageNamed("bug1.png")
-    b1 = UIImageView.alloc.initWithIamge(image)
+    b1 = UIImageView.alloc.initWithImage(image)
     b1.center = self.view.center
     self.view.addSubview(b1)
 
     image = UIImage.imageNamed("bug2.png")
-    b2 = UIImageView.alloc.initWithIamge(image)
+    b2 = UIImageView.alloc.initWithImage(image)
     b2.center = [200, 300]
     self.view.addSubview(b2)
 
     image = UIImage.imageNamed("bug3.png")
-    b3 = UIImageView.alloc.initWithIamge(image)
+    b3 = UIImageView.alloc.initWithImage(image)
     b3.center = [50, 80]
     self.view.addSubview(b3)
   end
@@ -28,9 +23,9 @@ class SampleForCopyAndPaste < UIViewController
     true
   end
 
-  def touchEnded(touches, withEvent:event)
-    touch = touch.anyObject
-    if self.becomeFirstResponder and (touch.tabCount > 1)
+  def touchesEnded(touches, withEvent:event)
+    touch = touches.anyObject
+    if self.becomeFirstResponder and (touch.tapCount > 1)
       menu = UIMenuController.sharedMenuController
       @touch_point = touch.locationInView(self.view)
       min_rect = CGRect.new
@@ -40,20 +35,61 @@ class SampleForCopyAndPaste < UIViewController
     end
   end
 
-  def canPerformAction(action, withEvent:sender)
-    # TODO
+  def canPerformAction(action, withSender:sender)
+    if action == :"copy:"
+      return true if self.imageContainsPoint(@touch_point)
+    elsif action == :"cut:"
+      return true if self.imageContainsPoint(@touch_point)
+    elsif action == :"paste:"
+      return true unless UIPasteboard.generalPasteboard.image.nil?
+    end
+
+    false
   end
 
   def imageContainsPoint(point)
+    view.subviews.each { |view|  
+      if CGRectContainsPoint(view.frame, point)
+        if view.class == UIImageView
+          puts "4"
+          return view
+        end
+      end
+    }
+
+    nil
   end
 
-  def copy
+  def copy(sender)
+    puts "in copy"
+    iv = self.imageContainsPoint(@touch_point)
+    if iv
+      puts "before copy"
+      UIPasteboard.generalPasteboard.image = iv.image 
+      puts "after copy"
+    end
   end
 
-  def paste
+  def cut(sender)
+    puts "in cut"
+    iv = self.imageContainsPoint(@touch_point)
+    if iv
+      UIPasteboard.generalPasteboard.image = iv.image if iv
+      iv.removeFromSuperview
+    end
   end
 
-  def cut
+  def paste(sender)
+    puts "in paste"
+    pb = UIPasteboard.generalPasteboard
+    if pb.image
+      puts "has image"
+      bug = UIImageView.alloc.initWithImage(pb.image)
+      bug.center = @touch_point
+      self.view.addSubview(bug)
+      pb.image = bug.image
+      puts "bingo"
+    end
   end
 
 end
