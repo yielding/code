dff56116bc822c9c3a0cddf386bb16be1cf2b73c
Chang Ha Lee <yielding@gmail.com>
2012-10-09 23:44:44 +0900

    ..added two projecteuler probs

---
+++ algorithm/euler/032.pandigital.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby19
+
+require_relative "numeric_ext"
+
+class Numeric
+  def digits
+    val = self
+    res = []
+    while val > 0
+      val, r = val.divmod(10)
+      res << r
+    end
+    res.reverse
+  end
+
+  def concat b
+    tmp, pow = b, 1
+    while tmp > 0
+      pow *= 10
+      tmp /= 10
+    end
+
+    self * pow + b
+  end
+end
+
+total = []
+for i in 1..99
+  start = i > 9 ? 123 : 1234
+  final = 10000 / i + 1
+  for j in start..final
+    res = i * j
+    concated = res.concat(i).concat(j)
+    if concated.is_pandigital?
+      p concated
+      total << res
+    end
+  end
+end
+
+p total.uniq.reduce(:+)
+
+=begin rdoc
+  
+sum = []
+for i in 1..99
+  start = i > 9 ? 123 : 1234
+  final = 10000 / i + 1
+    
+  for j in start..final
+    res = i * j
+    arr = i.digits + j.digits + res.digits
+
+    if arr.uniq.size == 9 and arr.size == 9 and arr.reduce(:+) == 45
+      p "#{i.digits}, #{j.digits}, #{res.digits}"
+      sum << res 
+    end
+  end
+end
+
+p sum.uniq.reduce(:+)
+
+=end
---
+++ algorithm/euler/033.unorthodox.cancelling.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby19
+
+a = [*10..99]
+p a.product(a) \
+   .select { |a, b| a.quo(b) < 1 } \
+   .select { |a, b| a % 10 == b / 10 } \
+   .select { |a, b| b % 10 != 0 } \
+   .select { |a, b| a.quo(b) == (a/10).quo(b%10) } \
+   .map    { |a, b| a.quo(b) } \
+   .reduce(:*)
+
+=begin rdoc
+arr = []
+for i in 10..99
+  for j in 10..99
+    a, b  = i % 10, j / 10
+    next unless a == b
+
+    nom, denom = i / 10, j % 10
+    next if denom == 0
+    next if i.quo(j) >= 1
+
+    if nom.quo(denom) == i.quo(j)
+      puts "#{i}, #{j}, #{i.quo(j)}" 
+      arr << i.quo(j)
+    end
+  end
+end
+
+p arr.reduce(:*)
+=end
