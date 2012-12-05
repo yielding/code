3108f86d061ab28f66a4cdf86215941daf6976ee
Chang Ha Lee <yielding@gmail.com>
2012-12-05 21:38:12 +0900

    .....

--- algorithm/euler/049.prime.seq.rb
+++ algorithm/euler/049.prime.seq.rb
@@ -7,20 +7,45 @@ class Numeric
   def digits
     self.to_s.scan(/./).map { |x| x.to_i }
   end
+
+  def same_permutation? n
+    arr = [0] * 10
+    tmp = self 
+    while tmp > 0
+      arr[tmp % 10] += 1
+      tmp /= 10
+    end
+
+    tmp = n
+    while tmp > 0
+      arr[tmp % 10] -= 1
+      tmp /= 10
+    end
+
+    return arr.all? { |v| v == 0 }
+  end
 end
 
 class Array
   def to_num
-     self.reduce { |s, e| s*10 + e} 
+    self.reduce { |s, e| s*10 + e }
   end
 end
 
 base = eratosthenes_sieve(10000)
-data = base.select { |n| n > 1000                }
+data = base.select { |n| n > 1498                }
            .select { |n| not n.digits.include? 0 }
-           .select { |n| n.digits.uniq.size == 4 }
 
-data.each { |n|
-  p n.digits.permutation.map.sort
-     .map { |e| e.to_num }
-}
+for i in (0...data.size)
+  for j in (i + 1...data.size)
+    k = 2 * data[j] - data[i]
+    next if k > 10000 or not data.include? k
+    a = data[i]
+    b = data[j]
+    c = k
+    if a.same_permutation?(b) and b.same_permutation?(c)
+      pp "#{data[i]}#{data[j]}#{k}"
+      exit
+    end
+  end
+end
