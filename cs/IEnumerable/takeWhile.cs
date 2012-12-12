a7dd4ee705ae1b5366f32cbf6b63a36b8e79552d
Chang Ha Lee <yielding@gmail.com>
2012-12-12 14:27:02 +0900

    .....

---
+++ cs/IEnumerable/takeWhile.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+class CSharpApp
+{
+    static void Main(string[] args)
+    {
+        byte[] bytes = new byte[] { 3, 1, 5, 3, 4, 5 };
+
+        var l0 = Enumerable.TakeWhile(bytes, x => x % 2 == 1);
+        foreach (var i in l0) Console.WriteLine(i);
+
+        var arr = new List<int>() { 1, 2, 3, 4, 5 };
+        var l1  = Enumerable.TakeWhile(arr, x => x % 2 == 1);
+    }
+}
