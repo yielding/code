83ccd9a22cbd7595e35f86976cf5007c9e958cc9
Chang Ha Lee <yielding@gmail.com>
2012-12-13 16:22:57 +0900

    ..

--- algorithm/euler/006.sum.square.rb
+++ algorithm/euler/006.sum.square.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby19 -wKU
 
-a = [*1..100]
-
-a1 = a.map { |e| e ** 2 }.reduce(:+)
-a2 = a.reduce(:+) ** 2
+a1 = (1..100).map { |e| e ** 2 }.reduce(:+)
+a2 = (1..100).reduce(:+) ** 2
 
 p a2 - a1
---
+++ algorithm/euler/045.tri.pent.hex.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby19
+require "pp"
+
+tris = (1..10).map { |n| n * (  n + 1) / 2 }
+pent = (1..10).map { |n| n * (3*n - 1) / 2 }
+hexa = (1..10).map { |n| n * (2*n - 1)     }
+
+pp tris
+pp pent
+pp hexa
