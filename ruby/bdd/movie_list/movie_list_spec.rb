da8879fea2feb1f5d259a3765b7970e62c60b380
Chang Ha Lee <yielding@gmail.com>
2013-01-09 21:44:07 +0900

    ..

---
+++ ruby/bdd/movie_list/movie_list_spec.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby1.9
+# encoding: utf-8
+
+class MovieList
+  def initialize
+  end
+
+  def empty?
+    true
+  end
+end
+
+describe MovieList do
+  context "처음 만들어질 때" do
+    it "반드시 비어 있어야 한다" do
+      movie_lsit = MovieList.new
+      movie_lsit.should be_empty
+    end
+  end
+end
