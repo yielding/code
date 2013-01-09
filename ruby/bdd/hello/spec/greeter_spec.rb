f81e845782603430db1e7a846c72b441ad050a0e
Chang Ha Lee <yielding@gmail.com>
2013-01-09 22:14:16 +0900

    ..

---
+++ ruby/bdd/hello/spec/greeter_spec.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby1.9
+
+class RSpecGreeter 
+  def greet
+    "Hello RSpec!"
+  end 
+end
+
+describe "RSpec Greeter" do
+  it "should say 'Hello RSpec!' when it receives the greet() message" do
+    greeter  = RSpecGreeter.new 
+    greeting = greeter.greet 
+    greeting.should == "Hello RSpec!"
+  end 
+end
