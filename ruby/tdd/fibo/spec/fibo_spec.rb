c2a72e769bedec3aaf802d80bee7201e0bd39efa
Chang Ha Lee <yielding@gmail.com>
2013-01-09 21:06:28 +0900

    ...

--- ruby/tdd/fibo/spec/fibo_spec.rb
+++ ruby/tdd/fibo/spec/fibo_spec.rb
@@ -1,9 +1,28 @@
 require "fibo.rb"
 
 describe Fibo do
+  before(:each) do
+    @book = double("book")
+    @book.stub(:title).and_return("The RSpec Book")
+  end
+
   it "should calculate for the numbers" do
     Fibo.fibo(0).should == 0
     Fibo.fibo(1).should == 1
     Fibo.fibo(2).should == 1
+  end
+
+  it "should have the book" do
+    @book.title.should == "The RSpec Book"
+  end
+
+  it "spec_name" do
+    die = double("die")
+    die.stub(:roll).and_return(1,2,3)
+    die.roll.should == 1 # => 1
+    die.roll.should == 2 # => 2
+    die.roll.should == 3 # => 3
+    die.roll.should == 3 # => 3
+    die.roll.should == 3 # => 3
   end
 end
