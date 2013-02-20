f63c7c4f17ba8a5335182a5213344a1f6bb04f6e
Chang Ha Lee <yielding@gmail.com>
2013-02-20 22:03:47 +0900

    ..

--- motion.ruby/scribe/app/main_view_controller.rb
+++
@@ -1,8 +0,0 @@
-class MainViewController < UIViewController
-  def viewDidLoad
-    super
-
-    paint_view = PaintView.alloc.initWithFrame(self.view.bounds)
-    view.addSubview(paint_view)
-  end
-end
