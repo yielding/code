d6716317adf7df2f3da0b86a94a60c282d0c1e8e
Chang Ha Lee <yielding@gmail.com>
2013-03-20 18:06:01 +0900

    ...

--- app/usbmux/develop/include/usbmux.h
+++ app/usbmux/develop/include/usbmux.h
@@ -1,17 +1,17 @@
-#include <string>
+#include "ByteBuffer.h"
+
 #include <boost/asio.hpp>
+#include <string>
+#include <map>
 
 using boost::asio::ip::tcp;
+using utility::hex::ByteBuffer;
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-//
+// SafeStreamSocket is already implemented in ASIO
 //
 ////////////////////////////////////////////////////////////////////////////////
-class SafeStreamSocket
-{
-};
-
 class MuxDevice
 {
 public:
@@ -32,13 +32,35 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-//
+// REMARK packet type을 만드는 것보다 Byte Buffer를 만드는 것이 유연하다.
+// 타입을 하나라도 적게 만드는 것이..
 //
 ////////////////////////////////////////////////////////////////////////////////
+/*
+struct usbmux_header
+{
+    uint32_t length;
+    uint32_t version;
+    uint32_t type;          // request, or response
+    uint32_t tag;
+};
+*/
+
 class BinaryProtocol
 {
 public:
+    enum { VERSION = 0, 
+           TYPE_RESULT, TYPE_CONNECT, TYPE_LISTEN,
+           TYPE_DEVICE_ADD, TYPE_DEVICE_REMOVE };
+public:
     BinaryProtocol(tcp::socket& s);
+
+    void send_packet(uint32_t req, uint32_t tag, ByteBuffer& payload);
+    void recv_packet();
+
+private:
+    auto pack(uint32_t req, std::map<std::string, int>& payload) -> ByteBuffer;
+    auto unpack(uint32_t resp, ByteBuffer const& payload) -> std::map<std::string, int>;
 
 protected:
     tcp::socket& _socket;
@@ -53,7 +75,7 @@ public:
 ////////////////////////////////////////////////////////////////////////////////
 //
 // MuxConnection은 여러 가지 Protocol 중에서 하나를 선택하는 것이므로
-// Template 인자로서의 Protocol은 적당하지 않다. 상속이나 독립 클래스가 적당하지.
+// Template 인자로서의 Protocol은 적당하지 않다. 상속이나 독립 클래스가 적당하지
 //
 ////////////////////////////////////////////////////////////////////////////////
 class MuxConnection
@@ -98,6 +120,7 @@ public:
 private:
     std::string _socket_path;
     MuxConnection* _listener;
+    int _version;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
--- app/usbmux/develop/src/usbmux.cpp
+++ app/usbmux/develop/src/usbmux.cpp
@@ -1,9 +1,11 @@
 #ifndef USBMUX_H
 #define USBMUX_H
 
+#include "stdafx.h"
 #include "usbmux.h"
 
 using namespace std;
+using namespace utility::hex;
 ////////////////////////////////////////////////////////////////////////////////
 //
 //
@@ -14,6 +16,75 @@ BinaryProtocol::BinaryProtocol(tcp::socket& s)
 {
 }
 
+void BinaryProtocol::send_packet(uint32_t reqest, uint32_t tag, ByteBuffer& payload)
+{
+}
+
+auto BinaryProtocol::pack(uint32_t req, map<string, int>& payload) 
+     -> ByteBuffer
+{
+    ByteBuffer packet;
+    if (req == TYPE_CONNECT)
+    {
+        uint32_t device_id = payload["DeviceID"];
+        uint16_t port_no   = payload["PortNumber"];
+
+        packet.set_uint4_le(device_id).set_uint2_le(port_no)
+              .set_uint1(0).set_uint1(0);
+
+        return packet;
+    }
+    else if (req == TYPE_LISTEN)
+    {
+        return packet;
+    }
+
+    throw std::runtime_error("Invalid outgoint request");
+}
+
+auto BinaryProtocol::unpack(uint32_t resp, ByteBuffer const& payload) 
+    -> map<string, int>
+{
+    map<string, int> result;
+
+    if (resp == TYPE_RESULT)
+    {
+        uint32_t no = payload.get_uint4_le();
+        result["Number"] = no;
+        return result;
+    }
+
+    if (resp == TYPE_DEVICE_ADD)
+    {
+        uint32_t device_id;
+        uint16_t usbpid;
+        string   serial;
+        uint16_t padding;
+        uint32_t location;
+
+        result["DeviceID"]     = device_id;
+        result["LocationID"]   = location;
+        // TODO
+        // result["SerialNumber"] = serial;
+        result["ProductID"]    = usbpid;
+
+        return result;
+    }
+
+    if (resp == TYPE_DEVICE_REMOVE)
+    {
+        result["DeviceID"] = payload.get_uint4_le();
+        return result;
+    }
+
+    throw std::runtime_error("Invalid incoming response type");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
 PlistProtocol::PlistProtocol(tcp::socket& s)
     : BinaryProtocol(s)
 {
@@ -62,17 +133,16 @@ void MuxConnection::exchange()
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-//
+//  REMARK!!
+//    원격지에 있는 컴퓨터와 통신이 가능한지 확인해본다.
 //
 ////////////////////////////////////////////////////////////////////////////////
 USBMux::USBMux(string const& socket_path)
 {
-    if (socket_path.empty())
-    {
-    }
+    _version = 0;
 
     _socket_path = socket_path;
-    _listener = new MuxConnection(_socket_path);
+    _listener    = new MuxConnection(_socket_path);
 }
 
 USBMux::~USBMux()
