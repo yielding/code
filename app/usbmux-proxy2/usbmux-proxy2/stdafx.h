f21eff221477cc24f0e49e8a9e6fc9e851ce6513
Chang Ha Lee <yielding@gmail.com>
2013-03-29 14:24:12 +0900

    ...

---
+++ app/usbmux-proxy2/usbmux-proxy2/stdafx.h
@@ -0,0 +1,14 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifdef WIN32
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#endif
+
