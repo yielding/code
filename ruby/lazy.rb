5a4dc0ae03b8f3524ff8f34630bec9ddcef82c8f
Chang Ha Lee <yielding@gmail.com>
2013-04-24 23:37:46 +0900

    .

---
+++ ruby/lazy.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby2.0
+
+def Integer.all
+  Enumerator.new do |yielder, n: 0|
+    loop { yielder.yield(n += 1) }
+  end.lazy
+end
+
+def palindrome?(n)
+  n = n.to_s
+  n == n.reverse
+end
+
+p Integer.all.first(10),
+  Integer.all
+         .select { |i| (i % 3).zero?  }
+         .select { |i| palindrome?(i) }
+         .first(10)
