7c09dfc930e316435b92f85639c2e23cab0ff51a
Chang Ha Lee <yielding@gmail.com>
2013-07-05 16:54:46 +0900

    ..

--- antlr/prop/PropertyFileVisitor.java
+++
@@ -1,9 +0,0 @@
-// Generated from PropertyFile.g4 by ANTLR 4.0
-import org.antlr.v4.runtime.tree.*;
-import org.antlr.v4.runtime.Token;
-
-public interface PropertyFileVisitor<T> extends ParseTreeVisitor<T> {
-	T visitFile(PropertyFileParser.FileContext ctx);
-
-	T visitProp(PropertyFileParser.PropContext ctx);
-}
