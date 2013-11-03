6fe4d452fab695b756ba13341ac00daf455ae1d1
Lee Chang Ha at saturn <yielding@icloud.com>
2013-11-03 23:13:11 +0900

    ..

--- algorithm/count_of_obvious.rb
+++ algorithm/count_of_obvious.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env ruby
 
+class Float
+  def equal? x
+    diff = (self - x).abs
+    return true if diff < 1e-10
+    diff <= (1e-8 * [self.abs, x.abs].max)
+  end
+end
+
 def count_of_obvious n
   same = 0
-  0.upto(n) { |x|
+  1.upto(n) { |x|
     y = 1.0 / x
     puts y * x 
     same += 1 if y * x - 1.0 == 0.0
@@ -11,4 +19,15 @@ def count_of_obvious n
   same
 end
 
-puts count_of_obvious(50)
+def count_of_obvious2 n
+  same = 0
+  1.upto(n) { |x|
+    y = 1.0 / x
+    puts y * x 
+    same += 1 if (y * x).equal?(1.0)
+  }
+
+  same
+end
+
+puts count_of_obvious2(50)
