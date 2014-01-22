871d621790afa68be11aa61bb7668a3267c3d2e6
Chang Ha Lee <yielding@gmail.com>
2014-01-22 13:25:09 +0900

    ..

--- cpp/split_join.cpp
+++ cpp/split_join.cpp
@@ -17,6 +17,7 @@ vector<string> split(string const& s, char delim)
 {
   vector<string> elems;
   split(s, delim, elems);
+
   return elems;
 }
 
@@ -25,29 +26,27 @@ string join(ForwardIterator f, ForwardIterator l, string const& sep=", ")
 {
   stringstream ss;
 
-  for (; f != l - 1; ++f) ss << *f << sep;
-
   ss << *f;
+  for (++f; f != l; ++f) ss << sep << *f;
+
 
   return ss.str();
 }
 
 template <typename ForwardIterator> 
-string join(ForwardIterator f, int count, string const& sep=", ")
+string join(ForwardIterator from, int count, string const& sep=", ")
 {
-  auto from = f;
-  auto to   = f + count;
+  auto to = from + count;
 
-  if (distance(from, to) <= 0)
-    return "";
-
-  return join(from, to, sep);
+  return distance(from, to) <= 0
+    ? ""
+    : join(from, to, sep);
 }
 
 int main(int argc, const char *argv[])
 {
   auto ss = split("/var/mobile/Applications/Kakao/Kakao.sqlite", '/');
   cout << join(ss.begin(), 5, "/") << endl;
-  
+
   return 0;
 }
