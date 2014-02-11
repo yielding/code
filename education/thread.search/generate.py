60fc8d39b85aabfa691b7f9be17d033c9062ba3f
Chang Ha Lee <yielding@gmail.com>
2014-02-11 15:57:34 +0900

    ..

--- education/thread.search/generate.py
+++ education/thread.search/generate.py
@@ -2,11 +2,9 @@
 
 h = open("data.bin", "wb")
 
-for i in xrange(1024*4):
-  h.write("\x00")
 
 h.seek(1)
 h.write("monday")
-h.seek(2048)
+h.seek(1024*1024*1024-100)
 h.write("monday")
 h.close()
---
+++ education/thread.search/search.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+def search_block(block, key)
+  pos_list = []
+  pos = block.index(key)
+  while pos
+    yield pos if block_given?
+    pos_list << pos
+    pos = block.index(key, pos+1)
+  end
+
+  pos_list
+end
+
+def search_file(s, beg, end_, key)
+  size = end_ - beg + 1
+  blks = (size + 1023) / 1024
+  offsets = []
+  blks.times { |n|
+    s.seek(beg + n*1024)
+    block = s.read(1024 + key.size - 1)
+    roffsets = search_block(block, key)
+    offsets += roffsets.map { |e| beg + e + n * 1024 }
+  }
+
+  offsets
+end
+
+block = "\x00leech\x00leech"
+search_block(block, "leech") { |pos|
+  p pos 
+}
+
+path = "data0.bin"
+f    = File.open(path, "rb")
+size = File.size(path)
+p search_file(f, 0, size-1, "monday") 
