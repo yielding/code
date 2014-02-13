c4df99cc2cc1547e4b47a7242f77f10ea5b2f907
Lee Chang Ha at saturn <yielding@icloud.com>
2014-02-13 21:12:50 +0900

    ..

--- cs/Read.cs
+++ cs/Read.cs
@@ -5,21 +5,21 @@ using System.Collections.Generic;
 
 class MainApp
 {
-    public static void Main(string[] args)
-    {
-        var xys = new List<Tuple<int, int>>();
+  public static void Main(string[] args)
+  {
+    var xys = new List<Tuple<int, int>>();
 
-        xys.Add(new Tuple<int, int>(1, 2));
-        xys.Add(Tuple.Create(1, 2));
-        xys.Add(Tuple.Create(1, 2));
+    xys.Add(new Tuple<int, int>(1, 2));
+    xys.Add(Tuple.Create(1, 2));
+    xys.Add(Tuple.Create(1, 2));
 
-        var xx = Tuple.Create(3, 4);
-        xys.Add(xx);
+    var xx = Tuple.Create(3, 4);
+    xys.Add(xx);
 
-        Console.WriteLine(xx);
-        Console.WriteLine(xx.Item1);
-        Console.WriteLine(xx.Item2);
+    Console.WriteLine(xx);
+    Console.WriteLine(xx.Item1);
+    Console.WriteLine(xx.Item2);
 
-        foreach (var i in xys) Console.WriteLine(i);
-    }
+    foreach (var i in xys) Console.WriteLine(i);
+  }
 }
