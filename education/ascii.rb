6a03030044b14f62b772d28399fb0fd51b9d1b49
Lee Chang Ha at saturn <yielding@icloud.com>
2014-07-15 16:35:49 +0900

    ..

--- education/ascii.rb
+++ education/ascii.rb
@@ -1,9 +1,9 @@
-#!/usr/bin/env ruby -wKU
+#!/usr/bin/env ruby
 
-a = "123abc123abc"
+data = ["I am a boy", "You are a girl", "We are good friends"]
 
-p a =~/[a-z]/
-
-#fname = (ARGV.size == 1) ? ARGV[0] : 'ascii.rb'
-#File.open(fname) { |f|
-#}
+dict = Hash.new { |h, key| h[key] = 0 }
+data.each { |line| line.downcase.each_char { |ch| dict[ch] += 1 } }
+dict.sort.each { |key, val|
+  puts " #{key} - #{val} " if key =~ /[a-z]/
+}
---
+++ education/sort_string.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+
+p ["bbb", "cccc", "aa"].sort_by { |a| a.length }
