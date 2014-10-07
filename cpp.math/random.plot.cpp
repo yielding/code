cd13426d3cbe1e93e6d2994491f1ac4af596d5aa
Lee Chang Ha at saturn <yielding@icloud.com>
2014-10-07 23:40:29 +0900

    ..

--- cpp.math/random.plot.cpp
+++ cpp.math/random.plot.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <string>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
--- ruby.mruby/basic/mytest.rb
+++ ruby.mruby/basic/mytest.rb
@@ -3,3 +3,6 @@ t = MyTest.new
 1.upto(10) { |e| t.add(e) }
 
 p t.to_s
+
+config = JSON::parse '{"foo": "bar"}'
+p config["foo"]
