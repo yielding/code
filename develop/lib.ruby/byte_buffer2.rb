3e5f5d91a15397246b581fa3e28e42cc139657b3
Lee Chang Ha at saturn <yielding@icloud.com>
2015-04-05 19:23:14 +0900

    ..

--- develop/lib.ruby/byte_buffer2.rb
+++ develop/lib.ruby/byte_buffer2.rb
@@ -1,13 +1,74 @@
+=begin rdoc
+
+  The variable-length integer encoding is as follows:
+
+  KEY:
+          A = 0xxxxxxx    7 bits of data and one flag bit
+          B = 1xxxxxxx    7 bits of data and one flag bit
+          C = xxxxxxxx    8 bits of data
+
+   7 bits - A
+  14 bits - BA
+  21 bits - BBA
+  28 bits - BBBA
+  35 bits - BBBBA
+  42 bits - BBBBBA
+  49 bits - BBBBBBA
+  56 bits - BBBBBBBA
+  64 bits - BBBBBBBBC
+
+  Write a 64-bit variable-length integer to memory starting at p[0].
+  The length of data write will be between 1 and 9 bytes.  The number
+  of bytes written is returned.
+
+  A variable-length integer consists of the lower 7 bits of each byte
+  for all bytes that have the 8th bit set and one byte with the 8th
+  bit clear.  Except, if we get to the 9th byte, it stores the full
+  8 bits and is the last byte.
+
+=end
+
+class Numeric
+  def get_varint
+
+    val = self
+    res, buf = [0] * 10, [0] * 10
+    #
+    # we treat this specially because we have 'C' type at the end
+    #
+    if (val & (0xff000000 << 32)) != 0
+      res[8] = val & 0xff
+      val >>= 8
+      7.downto(0) do |i| 
+        res[i] = (val & 0x7f) | 0x80 
+        val >>= 7 
+      end
+
+      return res.take(9).reverse
+    end
+
+    n = 0
+    until val == 0
+      buf[n], n = (val & 0x7f) | 0x80, n+1 
+      val >>= 7
+    end
+
+    buf[0] &= 0x7f
+    return buf.take(n).reverse
+  end
+end
+
+#
+#
+#
 class ByteBuffer2
-  attr_reader :pos, :buffer
+  attr_reader :pos, :buffer, :count
 
   def initialize(b, offset=0, length=-1)
     check_type(b)
 
     count = length == -1 ? b.size - offset : length
-    if count > b.size - offset 
-      raise IndexError 
-    end
+    raise IndexError if count > b.size - offset 
 
     if b.class == Array
       raise IndexError if offset + count > b.size
@@ -162,7 +223,7 @@ class ByteBuffer2
     arr.pack("C*").unpack("Q")[0]
   end
 
-  def get_varint_with_size
+  def from_varint_with_size
     viBytes  = @buffer[@pos..-1].bytes
     byte_no  = 0
     value    = 0
@@ -188,8 +249,8 @@ class ByteBuffer2
     return value, byte_no
   end
 
-  def get_varint
-    value, _ = get_varint_with_size
+  def from_varint
+    value, _ = from_varint_with_size
     value
   end
 
