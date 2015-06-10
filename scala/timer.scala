39323559d53d0a648d03fabc801f14eaf0b00206
Lee Chang Ha at saturn <yielding@icloud.com>
2015-06-10 22:40:49 +0900

    ..

--- scala/timer.scala
+++ scala/timer.scala
@@ -1,12 +1,12 @@
 object TimerAnonymous {
   def oncePerSecond(callback: () => Unit) {
-    while(true) { 
+    while (true) { 
       callback();
       Thread sleep 1000
     }
   }
 
-  def main(arg: Array[String]) {
+  def main(arg: Array [String]) {
     oncePerSecond(() => println("time flies like an arrow.."))
   }
 }
