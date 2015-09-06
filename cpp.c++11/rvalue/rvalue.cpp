f384f3af1271e7c268ad13cc8cc597a039214a6c
Lee Chang Ha at saturn <yielding@icloud.com>
2015-09-06 20:39:27 +0900

    ..

---
+++ cpp.c++11/rvalue/rvalue.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+using namespace std;
+
+class str
+{
+public:
+  str(string s="def string")
+  {
+    cout << "def ctor is called" << endl;
+    this->s = s;
+  }
+
+  str(str const& rhs) 
+  {
+    cout << "copy ctor is called" << endl;
+    if (this != &rhs)
+    {
+      s = rhs.s;
+    }
+  }
+
+  str& operator=(str const& rhs)
+  {
+    cout << "assign operator is called" << endl;
+
+    if (this != &rhs) s = rhs.s;
+
+    return *this;
+  }
+
+  str(str && s)
+  {
+    cout << "move ctor is called" << endl;
+  }
+
+  string s;
+};
+
+class value 
+{
+public:
+  value(): s("leech")
+  {
+  }
+
+  str&& get_value()
+  {
+    return move(s);
+  }
+
+private:
+  str s;
+};
+
+int main(int argc, char *argv[])
+{
+  value v;
+
+  cout << v.get_value().s;
+  
+  return 0;
+}
