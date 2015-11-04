558028d1d420029ede2472aee70adfaef396a81d
Lee Chang Ha at saturn <yielding@icloud.com>
2015-11-04 14:20:26 +0900

    ..

--- cpp.boost/iostream/zlib/test_zlib.cpp
+++ cpp.boost/iostream/zlib/test_zlib.cpp
@@ -1,21 +1,51 @@
-#include <fstream>
-#include <iostream>
-#include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <iostream>
+#include <sstream>
 
-int main(int argc, char const* argv[])
+template <class cha_type, class iterator_type>
+struct my_source 
 {
-  using namespace std;
-  namespace io = boost::iostreams;
+  typedef cha_type char_type;
+  typedef boost::iostreams::source_tag category;
 
-  ifstream file("/tmp/zlib.cpp.gz", ios_base::in | ios_base::binary);
+  iterator_type& it;
+  iterator_type end;
 
-  io::filtering_streambuf<io::input> in;
-  in.push(io::gzip_decompressor());
-  in.push(file);
+  my_source(iterator_type& it, iterator_type end = {}) : it(it), end(end) 
+  {}
 
-  io::copy(in, cout);
-  
+  std::streamsize read(char* s, std::streamsize n)
+  {
+    std::streamsize result = 0;
+    while ((it!=end) && n--) 
+    {
+      ++result;
+      *s++ = *it++;
+    }
+
+    return result;
+  }
+};
+
+int main() 
+{
+  std::string const rawdata {
+    'x', '\234', '\313', 'H', '\315', '\311', 
+    '\311', 'W', '(', '\317', '/', '\312', 'I', 
+    '\341', '\002', '\0', '\036', 'r', '\004', 'g' };
+
+  std::istringstream iss(rawdata, std::ios::binary);
+
+  auto start = std::istreambuf_iterator<char>(iss);
+  my_source<char, decltype(start)> data(start);
+
+  boost::iostreams::filtering_istreambuf def;
+  def.push(boost::iostreams::zlib_decompressor());
+  def.push(data);
+
+  boost::iostreams::copy(def, std::cout);
+
   return 0;
 }
