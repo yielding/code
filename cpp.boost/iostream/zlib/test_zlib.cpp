f8c1ad5dd0bfb6e3718c8dd85d5481613c603a82
Lee Chang Ha at saturn <yielding@icloud.com>
2015-11-04 14:15:32 +0900

    ..

--- cpp.boost/iostream/zlib/test_zlib.cpp
+++ cpp.boost/iostream/zlib/test_zlib.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
 
 int main(int argc, char const* argv[])
 {
   using namespace std;
   namespace io = boost::iostreams;
 
-  ifstream file("fd.4.gz", ios_base::in | ios_base::binary);
+  ifstream file("/tmp/zlib.cpp.gz", ios_base::in | ios_base::binary);
 
   io::filtering_streambuf<io::input> in;
-  in.push(io::zlib_decompressor());
+  in.push(io::gzip_decompressor());
   in.push(file);
 
   io::copy(in, cout);
