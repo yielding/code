d698df376c213bab20c25244a6a91c1b3d8a785c
Chang Ha Lee <yielding@gmail.com>
2015-12-08 17:40:54 +0900

    ..

--- ruby/tdd/fibo/spec/fibo_spec.rb
+++ ruby/tdd/fibo/spec/fibo_spec.rb
@@ -7,22 +7,22 @@ describe Fibo do
   end
 
   it "should calculate for the numbers" do
-    Fibo.fibo(0).should == 0
-    Fibo.fibo(1).should == 1
-    Fibo.fibo(2).should == 1
+    expect(Fibo.fibo(0)).to eq(0)
+    expect(Fibo.fibo(1)).to eq(1)
+    expect(Fibo.fibo(2)).to eq(1)
   end
 
   it "should have the book" do
-    @book.title.should == "The RSpec Book"
+    expect(@book.title).to eq("The RSpec Book")
   end
 
   it "spec_name" do
     die = double("die")
     die.stub(:roll).and_return(1,2,3)
-    die.roll.should == 1 # => 1
-    die.roll.should == 2 # => 2
-    die.roll.should == 3 # => 3
-    die.roll.should == 3 # => 3
-    die.roll.should == 3 # => 3
+    expect(die.roll).to eq(1) # => 1
+    expect(die.roll).to eq(2) # => 2
+    expect(die.roll).to eq(3) # => 3
+    expect(die.roll).to eq(3) # => 3
+    expect(die.roll).to eq(3) # => 3
   end
 end
