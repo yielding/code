d5a83219b95f4dc2caae07bccd69307b339f8a33
Lee Chang Ha at saturn <yielding@icloud.com>
2016-02-11 08:30:13 +0900

    ..

--- cpp.rx/distinct.cpp
+++ cpp.rx/distinct.cpp
@@ -3,9 +3,10 @@
 int main(int argc, char *argv[])
 {
     auto values = rxcpp::observable<>::from(1, 2, 2, 3, 3, 3, 4, 5, 5, 6)
-                        .distinct_until_changed();
-                        //.filter([](int v) { return v % 2; })
-                        //.max();
+                        .distinct()
+                        //.distinct_until_changed();
+                        .ilter([](int v) { return v % 2; })
+                        .max();
 
     values.subscribe([](int v) { printf("%d ", v); }, []{});
     
