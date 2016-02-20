01a444de83e4c983f74a8c8b416d07e9089384dd
Lee Chang Ha at saturn <yielding@icloud.com>
2016-02-20 23:09:07 +0900

    ..

---
+++ swift/point.swift
@@ -0,0 +1,15 @@
+#!/usr/bin/env swift
+
+struct Point {
+  var x = 0.0, y = 0.0
+
+  mutating func moveByX(deltaX: Double, y deltaY: Double) {
+    x += deltaX
+    y += deltaY
+  }
+}
+
+var somePoint = Point(x: 1.0, y: 1.0)
+somePoint.moveByX(2.0, y: 3.0)
+
+print("The point is now at (\(somePoint.x), \(somePoint.y))")
