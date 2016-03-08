3f7cc4fde37d2d7332fcbcaf5d972e21a8d5ca43
Lee Chang Ha at saturn <yielding@icloud.com>
2016-03-09 08:56:13 +0900

    ..

---
+++ swift/utf8read.swift
@@ -0,0 +1,13 @@
+#!/usr/bin/env swift
+
+import Foundation 
+
+var ss = try String(contentsOfFile: "/Users/yielding/.vimrc", encoding:NSUTF8StringEncoding)
+
+var arr = ss.componentsSeparatedByString("\r\n")
+
+for s in arr {
+  print(s)
+}
+
+
