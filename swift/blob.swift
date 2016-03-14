b81eaca918aa321171e596eab539f96fb977e2cd
Lee Chang Ha at saturn <yielding@icloud.com>
2016-03-14 21:19:39 +0900

    ..

---
+++ swift/blob.swift
@@ -0,0 +1,36 @@
+public struct Blob {
+
+	public let bytes: [UInt8]
+
+	public init(bytes: [UInt8]) {
+		self.bytes = bytes
+	}
+
+	public init(bytes: UnsafePointer<Void>, length: Int) {
+		self.init(bytes: [UInt8](UnsafeBufferPointer(
+			start: UnsafePointer(bytes), count: length
+			)))
+	}
+
+	public func toHex() -> String {
+		return bytes.map {
+			($0 < 16 ? "0" : "") + String($0, radix: 16, uppercase: false)
+		}.joinWithSeparator("")
+	}
+
+}
+
+extension Blob : CustomStringConvertible {
+
+	public var description: String {
+		return "x'\(toHex())'"
+	}
+
+}
+
+extension Blob : Equatable {
+}
+
+public func ==(lhs: Blob, rhs: Blob) -> Bool {
+	return lhs.bytes == rhs.bytes
+}
