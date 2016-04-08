a50b8f2697b63c59fda691eab18ac836aa06fa8b
Lee Chang Ha at saturn <yielding@icloud.com>
2016-04-08 16:24:10 +0900

    ..

---
+++ swift/sum.swift
@@ -0,0 +1,13 @@
+#!/usr/bin/env swift
+
+func sumNumbers(numbers: Int...) -> Int {
+    var total = 0
+    for number in numbers {
+        total += number
+    }
+
+    return total
+}
+
+let sum = sumNumbers(2, 3, 4, 5)
+print(sum)
