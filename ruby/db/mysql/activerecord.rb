0830f8f8221ea76112abc13b4b755b25dd6fa99a
Lee Chang Ha at saturn <yielding@icloud.com>
2016-05-12 10:20:34 +0900

    ..

---
+++ ruby/db/mysql/activerecord.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'active_record'
+require 'yaml'
+require 'pp'
+
+dbconf = YAML::load(File.open('db.yaml'))
+ActiveRecord::Base.establish_connection(dbconf)
+
+class App < ActiveRecord::Base
+  self.primary_key = 'id'
+  self.table_name  = 'apps'
+end
+
+puts App.count
+
+pp App.find_each { |s| pp s }
+# pp App.find_by(name: "QQ")
+
+# App.find(:all).each { |s| puts s }
+
