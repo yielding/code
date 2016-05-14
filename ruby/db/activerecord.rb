b1ed7bc15fad6c8ac4893d1028d2158a4e075245
Chang Ha Lee <yielding@gmail.com>
2016-05-14 14:33:08 +0900

    ..

--- ruby/db/activerecord.rb
+++ ruby/db/activerecord.rb
@@ -7,12 +7,12 @@ require 'pp'
 dbconf = YAML::load(File.open('db.yaml'))
 ActiveRecord::Base.establish_connection(dbconf)
 
-class Dept < ActiveRecord::Base
-  set_table_name 'dept'
+class User < ActiveRecord::Base
+  self.table_name = 'user'
 end
 
-puts Dept.count
+puts User.count
 
-Dept.find(:all).each { |s| puts s.dname }
+#Dept.find(:all).each { |s| puts s.dname }
 
 #Dept.find(:all, :select => "dname").each { |s| puts s.dname }
