9bd068d00904030f084eef8d5159909a4e5fdeed
Lee Chang Ha at saturn <yielding@icloud.com>
2016-07-11 11:00:20 +0900

    ..

--- swift/utf8read.swift
+++ swift/utf8read.swift
@@ -2,7 +2,7 @@
 
 import Foundation 
 
-var ss = try String(contentsOfFile: "/Users/yielding/.vimrc", encoding: NSUTF8StringEncoding)
+var ss = try String(contentsOfFile: "/Users/yielding/.vimrc", encoding: String.Encoding.utf8)
 
 var arr = ss.components(separatedBy: "\r\n")
 for s in arr { print(s) }
