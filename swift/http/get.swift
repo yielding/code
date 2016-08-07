f864dc5de1fdb7766a7b4133b4e23bfd6cca8528
Chang Ha Lee <yielding@gmail.com>
2016-08-07 17:21:47 +0900

    ..

--- swift/http/get.swift
+++ swift/http/get.swift
@@ -6,7 +6,7 @@ let page = "http://macnews.tistory.com/4406"
 var req  = URLRequest(url: URL(string: page)!)
 req.httpMethod = "GET"
 
-let task = URLSession.shared().dataTask(with: req) {
+let task = URLSession.shared.dataTask(with: req) {
   data, response, error in
   guard error == nil && data != nil else {
     print("error =\(error)")
