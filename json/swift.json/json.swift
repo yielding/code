a89cffa9b18361c25770981ea5ea421f320f7855
Lee Chang Ha at saturn <yielding@icloud.com>
2016-08-09 09:59:32 +0900

    ..

--- json/swift.json/json.swift
+++ json/swift.json/json.swift
@@ -3,8 +3,9 @@
 import Foundation
 // {"id":4,"table":"Message","script":"ddd","created_at":"2016-05-12T02:33:34.235Z","updated_at":"2016-05-12T02:33:34.235Z"}
 
-let d1 = NSData(contentsOfFile: "/Users/yielding/code/swift/json/data1.json")!
-let s0 = try! String(contentsOfFile: "/Users/yielding/code/swift/json/data1.json", encoding: String.Encoding.utf8)
+let dataPath = "/Users/yielding/code/json/swift.json/data1.json"
+let d1 = NSData(contentsOfFile: dataPath)!
+let s0 = try! String(contentsOfFile: dataPath, encoding: String.Encoding.utf8)
 let d2 = s0.data(using: String.Encoding.utf8)!
 
 let json = try! JSONSerialization.jsonObject(with: d2, options: JSONSerialization.ReadingOptions())
