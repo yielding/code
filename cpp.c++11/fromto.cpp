101a159ea40880507d779144840382618f7755f1
Lee Chang Ha at saturn <yielding@icloud.com>
2016-08-11 19:47:54 +0900

    ..

--- cpp.c++11/fromto.cpp
+++ cpp.c++11/fromto.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 auto fromto = [](auto start, auto finish) 
 {
-    return [=]() mutable {
-        if (start < finish)
-            return start++;
-        else
-            throw::runtime_error("complete");
-    };
+  return [=]() mutable {
+    if (start < finish)
+      return start++;
+    else
+      throw::runtime_error("complete");
+  };
 };
 
 int main(int argc, char *argv[])
 {
-    auto range = fromto(0, 10);
-    cout << range() << endl;
-    cout << range() << endl;
-    cout << range() << endl;
-    
-    return 0;
+  auto range = fromto(0, 10);
+  cout << range() << endl;
+  cout << range() << endl;
+  cout << range() << endl;
+
+  return 0;
 }
 
