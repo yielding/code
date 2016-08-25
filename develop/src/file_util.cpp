e3474647d2a9ff03ff8629a083168f0a06f1de34
Lee Chang Ha at saturn <yielding@icloud.com>
2016-08-25 10:34:47 +0900

    ..

--- develop/src/file_util.cpp
+++ develop/src/file_util.cpp
@@ -8,6 +8,16 @@ namespace util {
 //
 //
 ////////////////////////////////////////////////////////////////////////////////
+bool file_exists(string const& path)
+{
+  return ifstream{path}.good();
+}
+
+bool file_exists(char const* path)
+{
+  return ifstream{path}.good();
+}
+
 auto file_size(const char* filename) -> long
 {
   ifstream ifs(filename, ios::ate | ios::binary);
@@ -27,6 +37,15 @@ auto file_size(ifstream& ifs) -> long
   ifs.seekg(backup_pos, ios::beg);
 
   return long(result);
+}
+
+auto file_read(std::istream& in, long offset, int size) -> uint8_t*
+{
+  in.seekg(offset);
+  auto buffer = new uint8_t[size];
+  in.read((char*)buffer, size);
+
+  return buffer;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
