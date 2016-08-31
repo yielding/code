f76a45ec20e27a56232f080c702f637986d0d7c5
Lee Chang Ha at saturn <yielding@icloud.com>
2016-09-01 08:26:49 +0900

    ..

--- cpp.c++11/unicode/to_utf8.cpp
+++ cpp.c++11/unicode/to_utf8.cpp
@@ -18,17 +18,18 @@ int main(int argc, char *argv[])
   { 0xec, 0x9d, 0xb4, 0xec, 0xb0, 0xbd, 0xed, 0x95, 0x98 };
 
   string x(u8, u8+9);
-  cout << x << endl;
+  cout << x << endl;  // 이창하
 
 
   // uint8_t u16[] = { 0xff, 0xfe, 0x74, 0xc7, 0x3d, 0xcc, 0x58, 0xd5 };
   //char16_t u16[] = { 0x74c7, 0x3dcc, 0x58d5 };
+  uint8_t u16[] =  { 0x74, 0xc7, 0x3d, 0xcc, 0x58, 0xd5 };
 
   // utf16-be -> utf8
   wstring_convert<codecvt_utf8_utf16<char16_t, 0x10ffff, (codecvt_mode)1>, 
                   char16_t> convert2;
   //string dest = convert2.to_bytes(u"\uc774\ucc3d\ud558");
-  string dest = convert2.to_bytes(u"\uc774\ucc3d\ud558");
+  string dest = convert2.to_bytes((char16_t*)u16);
   cout << dest << endl;
 
 
