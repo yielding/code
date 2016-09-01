30e139be8e905e6302c4746bc787bca424dc003e
Chang Ha Lee <yielding@gmail.com>
2016-09-01 21:21:56 +0900

    ..

---
+++ ruby/uv.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'libuv'
+require 'libuv/coroutines'
+
+loop = Libuv::Loop.default
+loop.run do
+	begin
+		timer = loop.timer do
+			puts "5 seconds passed"
+			timer.close
+		end
+		timer.start(5000)
+
+		# co-routine waits for timer to close
+		co timer
+
+		puts "timer handle was closed"
+	rescue => error
+		puts "error with timer: #{error}"
+	end
+end
