c5231d760e2d9a12721138983fe4bbe78c13ffd8
Chang Ha Lee <yielding@gmail.com>
2016-09-01 21:22:18 +0900

    ..

--- ruby/uv.rb
+++ ruby/uv.rb
@@ -5,18 +5,18 @@ require 'libuv/coroutines'
 
 loop = Libuv::Loop.default
 loop.run do
-	begin
-		timer = loop.timer do
-			puts "5 seconds passed"
-			timer.close
-		end
-		timer.start(5000)
+  begin
+    timer = loop.timer do
+      puts "5 seconds passed"
+      timer.close
+    end
+    timer.start(5000)
 
-		# co-routine waits for timer to close
-		co timer
+    # co-routine waits for timer to close
+    co timer
 
-		puts "timer handle was closed"
-	rescue => error
-		puts "error with timer: #{error}"
-	end
+    puts "timer handle was closed"
+  rescue => error
+    puts "error with timer: #{error}"
+  end
 end
