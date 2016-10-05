0b6821db23b925252e65f13e0eb192f8b6095ebc
Chang Ha Lee <yielding@gmail.com>
2016-10-05 23:31:12 +0900

    ..

---
+++ hash/sha1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+#include <boost/uuid/sha1.hpp>
+#include <boost/format.hpp>
+
+using namespace std; 
+using namespace boost;
+
+auto to_sha1(string s) -> string
+{
+  uuids::detail::sha1 sh;
+  sh.process_bytes(s.c_str(), s.length());
+
+  unsigned int digest[5];
+  sh.get_digest(digest);
+
+  string r;
+  for (auto i : digest)
+    r += str(format("%08x") % i);
+
+  return r;
+}
+
+int main(int argc, char *argv[])
+{
+  try   
+  {
+    cout << to_sha1("abcdefg") << endl;
+  }
+  catch(...)
+  {
+  }
+  
+  return 0;
+}
