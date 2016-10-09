873511df83910566cb68681d6b609b67a795e801
Chang Ha Lee <yielding@gmail.com>
2016-10-09 12:11:29 +0900

    ..

--- algorithm/dp_fibo2.rb
+++ algorithm/dp_fibo2.rb
@@ -13,4 +13,6 @@ class Fibo
 end
 
 f = Fibo.new
-p (1..12000).reduce { |s, n| s + f.fibo(n) }
+p (1..10000).reduce { |s, n| s + f.fibo(n) }
+            .to_s.scan(/./)
+            .reduce(0) { |sum, n| sum + n.to_i }
