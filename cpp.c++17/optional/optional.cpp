625497c7845386f3c125c26f393952a2d6947ebc
Lee Chang Ha at saturn <yielding@icloud.com>
2017-01-06 19:07:37 +0900

    ..

--- cpp.c++17/optional/optional.cpp
+++ cpp.c++17/optional/optional.cpp
@@ -2,25 +2,24 @@
 #include <iostream>
 #include <optional>
 
-using namespace std::string_literals;
+using namespace string_literals;
 using namespace std;
 
 // optional can be used as the return type of a factory that may fail
-std::optional<std::string> create(bool b) 
+optional<string> create(bool b) 
 {
-	if (b)
-		return "Godzilla"s;
+  if (b)
+    return "Godzilla"s;
   else
     return {};
 }
 
 int main()
 {
-	cout << "create(false) returned "
-		<< create(false).value_or("empty") << '\n';
+  cout << "create(false) returned "
+       << create(false).value_or("empty") << '\n';
 
-	// optional-returning factory functions are usable as conditions of while and if
-	if (auto str = create(true)) {
-		std::cout << "create(true) returned " << *str << '\n';
-	}
+  // optional-returning factory functions are usable as conditions of while and if
+  if (auto str = create(true))
+    cout << "create(true) returned " << *str << '\n';
 }
