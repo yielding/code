50e9e19bf2b5ff2d45352452d5157694abef03e1
Lee Chang Ha at saturn <yielding@icloud.com>
2017-02-01 17:18:19 +0900

    ..

---
+++ haskell/and_or.hs
@@ -0,0 +1,15 @@
+
+and_ :: [Bool] -> Bool
+and_ []     = True
+and_ (x:xs) = x && and_ xs
+
+reverse bs = foldr (\b g x -> g (b : x)) id bs []
+
+-- and_1 :: [Bool] -> Bool
+-- and_1 = foldr (\x xs -> x && xs) []
+
+
+or_ :: [Bool] -> Bool
+or_ []     = False
+or_ (x:xs) = x || or_ xs
+
