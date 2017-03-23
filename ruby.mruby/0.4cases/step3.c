51085201c94b0e5de29b606a6ca02aed19573b46
Chang Ha Lee <yielding@gmail.com>
2017-03-23 23:47:18 +0900

    ..

--- ruby.mruby/0.4cases/step3.c
+++ ruby.mruby/0.4cases/step3.c
@@ -6,7 +6,8 @@
 #include "mruby/proc.h"
 #include <stdio.h>
 
-void _error(const char* s){
+void _error(const char* s)
+{
   printf("ERROR: %s\n", s);
   exit(1);
 }
@@ -23,16 +24,15 @@ void plus(mrb_state *mrb, mrb_value a, mrb_value b)
 
 int main()
 {
-  mrb_state *mrb;
-  int n;
-  FILE* f;
   mrb_value str;
   mrb_value z;
 
-  mrb = mrb_open();
-  f = fopen("step3.rb", "r");
-  if(f==NULL){ _error("file not found."); }
-  n = mrb_load_file(mrb, f);
+  mrb_state *mrb = mrb_open();
+  FILE* f = fopen("step3.rb", "r");
+  if (f == NULL) 
+    _error("file not found."); 
+
+  int n = mrb_load_file(mrb, f);
   fclose(f);
   mrb_run(mrb, mrb_proc_new(mrb, mrb->irep[n]), mrb_nil_value());
 
@@ -43,16 +43,17 @@ int main()
   mrb_funcall(mrb, mrb_top_self(mrb), "p", 1, str);
 
   z = mrb_funcall(mrb, mrb_top_self(mrb), "tak", 3,
-    mrb_fixnum_value(24),
-    mrb_fixnum_value(16),
-    mrb_fixnum_value( 8)
-  );
+      mrb_fixnum_value(24),
+      mrb_fixnum_value(16),
+      mrb_fixnum_value( 8)
+      );
 
   str = mrb_str_new_cstr(mrb, "z=");
   mrb_funcall(mrb, mrb_top_self(mrb), "print", 2, str, z);
   mrb_funcall(mrb, mrb_top_self(mrb), "puts", 0);
 
-  if(mrb_type(z) == MRB_TT_FIXNUM){
+  if (mrb_type(z) == MRB_TT_FIXNUM)
+  {
     int int_z = mrb_fixnum(z);
     printf("z=%d\n", int_z);
   }
