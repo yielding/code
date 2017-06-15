00abb0e83ee50510d9d139cba956d2ccc8c1003e
Lee Chang Ha at saturn <yielding@icloud.com>
2017-06-15 16:59:30 +0900

    ..

--- py/xml/etree.py
+++ py/xml/etree.py
@@ -3,4 +3,14 @@
 
 import xml.etree.ElementTree as ET
 
-print "hello"
+doc  = ET.parse("test.xml")
+root = doc.getroot()
+
+for country in root.iter("country"):
+    print "=" * 60
+
+    print "Country : ", country.attrib["name"]
+    print "Rank : ", country.findtext("rank")
+    print "Year : ", country.findtext("year")
+    for neighber in country.iter("neighbor"):
+        print "Neighbor :", neighber.attrib
