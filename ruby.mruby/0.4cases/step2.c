36996d79f230784a7926c890e5729665f80afeb1
Chang Ha Lee <yielding@gmail.com>
2017-10-12 22:06:24 +0900

    ..

--- ruby.mruby/0.4cases/step2.c
+++ ruby.mruby/0.4cases/step2.c
@@ -1,12 +1,13 @@
 /*
- [step2] run ruby code in file.
-*/
+   [step2] run ruby code in file.
+   */
 
 #include "mruby.h"
 #include "mruby/proc.h"
 #include <stdio.h>
 
-void _error(const char* s){
+void _error(const char* s)
+{
   printf("ERROR: %s\n", s);
   exit(1);
 }
@@ -15,11 +16,14 @@ int main()
 {
   mrb_state *mrb;
   int n;
-  FILE* f;
 
   mrb = mrb_open();
-  f = fopen("step2.rb", "r");
-  if(f==NULL){ _error("file not found."); }
+  FILE* f = fopen("step2.rb", "r");
+  if (f == NULL)
+  { 
+    _error("file not found."); 
+  }
+
   n = mrb_load_file(mrb, f);
   fclose(f);
   mrb_run(mrb, mrb_proc_new(mrb, mrb->irep[n]), mrb_nil_value());
@@ -27,29 +31,29 @@ int main()
 }
 
 /*
-% gcc -o step2 step2.c -I ../include -I ../src ../mrblib/mrblib.o ../lib/libmruby.a -lm
-% ./step2
-welcome to step2.
-fib(0) = 0
-fib(1) = 1
-fib(2) = 1
-fib(3) = 2
-fib(4) = 3
-fib(5) = 5
-fib(6) = 8
-fib(7) = 13
-fib(8) = 21
-fib(9) = 34
-fib(10) = 55
-n=0
-i=1
-i=2
-i=3
-i=4
-i=5
-i=6
-i=7
-i=8
-i=9
-i=10
-*/
+   % gcc -o step2 step2.c -I ../include -I ../src ../mrblib/mrblib.o ../lib/libmruby.a -lm
+   % ./step2
+   welcome to step2.
+   fib(0) = 0
+   fib(1) = 1
+   fib(2) = 1
+   fib(3) = 2
+   fib(4) = 3
+   fib(5) = 5
+   fib(6) = 8
+   fib(7) = 13
+   fib(8) = 21
+   fib(9) = 34
+   fib(10) = 55
+   n=0
+   i=1
+   i=2
+   i=3
+   i=4
+   i=5
+   i=6
+   i=7
+   i=8
+   i=9
+   i=10
+   */
