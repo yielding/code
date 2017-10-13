ca3a5f8110d2f9dbe5e8538d746e2461e12434cf
Lee Chang Ha at saturn <yielding@icloud.com>
2017-10-14 00:13:41 +0900

    ..

---
+++ cpp.c++17/if.new.cpp
@@ -0,0 +1,20 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+pair<int, bool> return_pair()
+{
+  return {1, true};
+}
+
+int main(int argc, char *argv[])
+{
+  if (auto[it, succeeded] = return_pair(); succeeded)
+  {
+    cout << "ok" << endl;
+  }
+  
+  return 0;
+}
