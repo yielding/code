03da04dc8ec006502652f891c81be8971a4f336e
Chang Ha Lee <yielding@river.local>
2017-11-23 23:23:16 +0900

    ..

--- ruby/ffi/fibo.rb
+++ ruby/ffi/fibo.rb
@@ -5,7 +5,7 @@ require "ffi"
 module FfiCustomTest
   extend FFI::Library
   ffi_lib 'c'
-  ffi_lib './ffi_test.so'
+  ffi_lib './ffi_fibo.so'
   attach_function :ffi_pow, [ :int, :int ], :int
   attach_function :ffi_factorial, [ :int ], :int
   attach_function :ffi_fibonacci, [ :int ], :int
