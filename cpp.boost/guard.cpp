5ffb06d84a59a791fe85b05ef6794f7b917f2fdf
Lee Chang Ha <yielding@gmail.com>
2017-12-19 23:08:50 +0900

    ..

--- cpp.boost/guard.cpp
+++ cpp.boost/guard.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <boost/shared_ptr.hpp>
 
 using namespace std;
@@ -6,7 +7,7 @@ using namespace std;
 void deleter(uint8_t* p)
 {
     cout << "deleting...\n";
-    printf("%x\n", p);
+    cout << addressof(p) << endl;
     delete [] p;
 }
 
@@ -15,9 +16,8 @@ int main(int argc, char const* argv[])
     uint8_t* x = new uint8_t[10];
 
     boost::shared_ptr<uint8_t> ptr(x, std::ptr_fun(deleter));
- // boost::shared_ptr<T> ptr(new T, std::mem_fun_ref(&T::deleter));
     cout << "exiting ...\n";
-    printf("%x\n", x);
+    cout << addressof(x) << endl;
     
     return 0;
 }
