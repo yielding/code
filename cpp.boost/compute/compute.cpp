a5f9aa948d9987bdc30ec62b23ad5ae49847d0e9
Lee Chang Ha <yielding@gmail.com>
2018-02-06 09:28:34 +0900

    ..

--- cpp.boost/compute/compute.cpp
+++ cpp.boost/compute/compute.cpp
@@ -6,11 +6,13 @@ namespace compute = boost::compute;
 
 int main()
 {
-  // get the default device
-  compute::device device = compute::system::default_device();
+    // get the default device
+    for (auto device : compute::system::devices())
+    {
+        // print the device's name
+        std::cout << "hello from " << device.name() << std::endl;
 
-  // print the device's name
-  std::cout << "hello from " << device.name() << std::endl;
+    }
 
-  return 0;
+    return 0;
 }
