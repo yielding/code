a87396d68de2fe6a056491ca20251aa6b361828a
Lee Chang Ha <yielding@gmail.com>
2018-03-04 01:34:15 +0900

    .

--- gmail.body.py
+++ gmail.body.py
@@ -5,7 +5,7 @@ import sqlite3
 import zlib
 
 good_chars =',.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~ '
- 
+
 conn   = sqlite3.connect('gmail.db')
 cursor = conn.cursor()
 cursor.execute("select _id, fromAddress, subject, bodyCompressed from messages")
