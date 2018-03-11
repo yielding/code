99981a873c306f7d230e174b799d57d73e7e0393
Lee Chang Ha <yielding@gmail.com>
2018-03-11 16:54:57 +0900

    ..

--- ruby/utf8_to_unicode.rb
+++ ruby/utf8_to_unicode.rb
@@ -1,37 +1,18 @@
 #!/usr/bin/env ruby
 
-require 'pp'
-
 class String
   def to_codepoint
-    chars = self
-    chars.bytes.each_slice(3).map { |s| 
+    self.bytes.each_slice(3).map do |s| 
       first = (s[0] & 0xf0) >> 4
-      return [] unless first == 0b1110
+      return nil unless first == 0b1110
 
-      res = s[0] & 0x0f
-      res = res << 6 | s[1] & 0x3f
-      res = res << 6 | s[2] & 0x3f
-    }
+      first = s.shift & 0x0f
+      s.reduce(first) { |result, e| result << 6 | e & 0x3f }
+    end
   end
 end
 
-def read_utf8_from path
-  File.open(path, "r:UTF-8") do |f| 
-    f.read.chomp
-  end
-end
-
-
-if __FILE__ == $PROGRAM_NAME
-  path = if ARGV.length != 1
-           "yielding.txt"  
-         else
-           ARGV[1]
-         end
-
-  chars = read_utf8_from(path)
-  chars.to_codepoint.each { |cp|
-    p "U+#{cp.to_s(16)}"
-  }
+chars = File.open("yielding.txt", "r:UTF-8") { |f| f.read.chomp }
+chars.to_codepoint.each do |cp|
+  p "U+#{cp.to_s(16).upcase}"
 end
