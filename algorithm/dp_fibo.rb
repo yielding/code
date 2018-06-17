aa6e5a2693d851e568968547a315c8f6224f496e
Lee Chang Ha <yielding@gmail.com>
2018-06-17 16:46:06 +0900

    ..

--- algorithm/dp_fibo.rb
+++ algorithm/dp_fibo.rb
@@ -3,12 +3,25 @@
 def fibo n
   return n if n <= 1
 
-  cache    = [0] * (n+1)
+  cache = [0] * (n+1)
   cache[1] = 1
 
   2.upto(n) { |i| cache[i] = cache[i-1] + cache[i-2] }
   cache[n]
 end
 
-1.upto(1000) { |i| p fibo i }
-p (1..2000).reduce(0) { |r, n| r + fibo(n) }
+def fibo2 n
+  return n if n <= 1
+  return fibo2(n-1) + fibo2(n-2)
+end
+
+
+for i in 1..10000
+  result = fibo(i)
+  puts result
+
+  break if result > 1000000000
+end
+
+#1.upto(1000) { |i| p fibo i }
+#p (1..2000).reduce(0) { |r, n| r + fibo(n) }
--- education/thumb_nail.rb
+++ education/thumb_nail.rb
@@ -5,4 +5,6 @@ image = File.binread("1.jpg")
 b = (image[1..-1] =~ /\xFF\xD8\xFF/n) + 1
 e = (image =~ /\xFF\xD9/n)
 
+p b
+p e
 File.binwrite("1_thumb.jpg", image[b, e - b])
