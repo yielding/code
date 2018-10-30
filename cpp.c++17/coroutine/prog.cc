49284c714538c978a314fe688f8f7e4d0ee19cc5
Lee Chang Ha <yielding@gmail.com>
2018-10-31 08:42:35 +0900

    ..

---
+++ cpp.c++17/coroutine/prog.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <experimental/coroutine>
+
+struct generator 
+{
+  struct promise_type;
+  using handle = std::experimental::coroutine_handle<promise_type>;
+  struct promise_type 
+  {
+    int current_value;
+    auto get_return_object() { return generator{handle::from_promise(*this)}; }
+    auto initial_suspend()   { return std::experimental::suspend_always{}; }
+    auto final_suspend()     { return std::experimental::suspend_always{}; }
+    void unhandled_exception() { std::terminate(); }
+    void return_void() {}
+    auto yield_value(int value) 
+    {
+      current_value = value;
+      return std::experimental::suspend_always{};
+    }
+  };
+
+  bool move_next()    { return coro ? (coro.resume(), !coro.done()) : false; }
+  int current_value() { return coro.promise().current_value; }
+
+  generator(generator const&) = delete;
+  generator(generator && rhs) : coro(rhs.coro) { rhs.coro = nullptr; }
+  ~generator() { if (coro) coro.destroy(); }
+
+private:
+  generator(handle h) : coro(h) {}
+  handle coro;
+};
+
+generator f() 
+{ 
+  co_yield 1; 
+  co_yield 2; 
+}
+
+int main() 
+{
+  auto g = f();
+  while (g.move_next()) 
+    std::cout << g.current_value() << std::endl;
+}
+
