d7f99ccf5fba2bc95f7eb20ee274968ccb3a211f
Lee Chang Ha <yielding@gmail.com>
2019-03-08 00:35:42 +0900

    ..

--- cpp.c++17/chrono/time_point.cpp
+++ cpp.c++17/chrono/time_point.cpp
@@ -1,31 +1,17 @@
 #include <chrono>
 #include <ctime>
 #include <iostream>
+#include <iomanip>
+#include "date.h"
 
 using namespace std;
-using namespace std::chrono;
-
-void print_time(system_clock::time_point t)
-{
-  auto c_time = system_clock::to_time_t(t);
-  auto  tmptr = localtime(&c_time);
-  system_clock::duration d = t.time_since_epoch();
-
-  cout << tmptr->tm_hour << ':' 
-       << tmptr->tm_min  << ':' 
-       << tmptr->tm_sec  << '.' 
-       << (d - duration_cast<seconds>(d)).count();
-
-}
+using namespace date;
 
 int main(int argc, char const* argv[])
 {
-  //auto t = system_clock::now();
-  //print_time(t);
-  auto unix_ts = chrono::seconds(time(nullptr)).count();
-  cout << unix_ts << endl;
-  cout << chrono::milliseconds(unix_ts).count() << endl;
-
+  // 1. time_point
+  auto now = chrono::system_clock::now();
+  cout << now;
 
   return 0;
 }
