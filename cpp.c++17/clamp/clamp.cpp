1f3dd6dc457a66623d7600e7b5936cce6b3e7ca9
Lee Chang Ha <yielding@gmail.com>
2019-05-11 12:13:54 +0900

    ..

---
+++ cpp.c++17/clamp/clamp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  const int lo = -32768;
+  const int hi =  32767;
+
+  cout << clamp( 12000, lo, hi) << endl;
+  cout << clamp(-36000, lo, hi) << endl;
+  cout << clamp( 40000, lo, hi) << endl;
+  
+  return 0;
+}
