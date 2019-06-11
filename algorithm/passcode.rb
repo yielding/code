62fe321e0181958aad801c14c36c3cea0647e040
Lee Chang Ha <yielding@gmail.com>
2019-06-12 00:31:36 +0900

    ..

--- algorithm/passcode.rb
+++ algorithm/passcode.rb
@@ -1,40 +1,48 @@
-#!/usr/bin/env ruby -wKU
-#
+#!/usr/bin/env ruby
+
+require "deep_clone"
 require "digest/sha1"
 
-class Object
-  def deep_copy
-    Marshal.load(Marshal.dump(self))
-  end
-end
+#class Object
+#  def deep_copy
+#    Marshal.load(Marshal.dump(self))
+#  end
+#end
 
 class Passcode
   def initialize
     @sha1 = Digest::SHA1.new
     @passcode = {}
-    prepare
+    @candidates_hash = {
+      0 => [1, 2, 3, 4, 5, 6, 7, 8, 9], # for start
+
+      1 => [2, 4, 5], 
+      2 => [1, 3, 4, 5, 6],
+      3 => [2, 5, 6],
+      4 => [1, 2, 5, 7, 8],
+      5 => [1, 2, 3, 4, 6, 7, 8, 9],
+      6 => [2, 3, 5, 8, 9],
+      7 => [4, 5, 8],
+      8 => [4, 5, 6, 7, 9],
+      9 => [5, 6, 8]
+    }
+
+    prepare_rainbow_table
   end
   
-  def candidates beg
-    candidates_hash = {
-      0 => [1, 2, 3, 4, 5, 6, 7, 8, 9], # for start
-      1 => [2, 3, 4, 5, 6, 7, 8, 9], 
-      2 => [1, 3, 4, 5, 6, 7, 8, 9],
-      3 => [1, 2, 4, 5, 6, 7, 8, 9],
-      4 => [1, 2, 3, 5, 6, 7, 8, 9],
-      5 => [1, 2, 3, 4, 6, 7, 8, 9],
-      6 => [1, 2, 3, 4, 5, 7, 8, 9],
-      7 => [1, 2, 3, 4, 5, 6, 8, 9],
-      8 => [1, 2, 3, 4, 5, 6, 7, 9],
-      9 => [1, 2, 3, 4, 5, 6, 7, 8]
-    }
-    candidates_hash[beg]
+  def passcode_of hash
+    @passcode[hash]
+  end
+
+private  
+  def prepare_rainbow_table
+    4.upto(9) { |i| backtrack([], 0, i) }
   end
   
   def process arr
     @sha1.reset
     arr.each { |e| @sha1 << (e-1).chr } 
-    s = arr.reduce("") { |res, e| res + sprintf("%d", e-1) } # [1, 2, 3, 4 ] => "1234"
+    s = arr.map(&:to_s).reduce(:+) # [1, 2, 3, 4] => "1234"
     e = @sha1.hexdigest.upcase
     @passcode[e] = s
     printf("%s => %s\n",e, s)
@@ -43,26 +51,13 @@ class Passcode
   def backtrack(arr, beg, depth)
     process(arr) && return if arr.length == depth
   
-    candidates(beg).each do |c| 
+    @candidates_hash[beg].each do |c| 
       unless arr.include?(c)
-        backtrack(arr.deep_copy.push(c), c, depth)
+        backtrack(DeepClone.clone(arr).push(c), c, depth)
       end
-    end
+    end 
   end
-  
-  def prepare
-    4.upto(9) { |i| backtrack([], 0, i) }
-  end
-  
-  def passcode key
-    @passcode[key]
-  end
-  
 end
 
 decryptor = Passcode.new
-
-puts "begin"
-
-p decryptor.passcode("ED5AAA6F798262758E2D878C8A9D0197D3CF17BB")
-p decryptor.passcode("59A3556203C8F6C908D6C6BCE4C5E03F6BF343E3")
+p decryptor.passcode_of("59A3556203C8F6C908D6C6BCE4C5E03F6BF343E3")
--- algorithm/string_add.cpp
+++ algorithm/string_add.cpp
@@ -17,7 +17,7 @@ string add(string const& a, string const& b)
     auto f = (acur == a.rend()) ? 0 : (*acur++ - '0');
     auto s = (bcur == b.rend()) ? 0 : (*bcur++ - '0');
 
-    res <<  (f + s + carry) % 10;
+    res  << (f + s + carry) % 10;
     carry = (f + s + carry) / 10;
   }
 
