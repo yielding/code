7e99458e1830cd55a21b383f8680d41ac70b1cf1
Lee Chang Ha <yielding@gmail.com>
2019-06-11 23:26:38 +0900

    ..

--- algorithm/n.queen.py
+++ algorithm/n.queen.py
@@ -8,5 +8,5 @@ for v in permutations(cols):
   l1 = len(set(v[i] + i for i in cols))  # 배열의 각 엔트리(row)에 칼럼값을 더하거나 빼면
   l2 = len(set(v[i] - i for i in cols))  # 같은 대각선에 있는 놈들은 같은 값을 가진다.
                                          # 기울기의 절대값이 1인것이 아이디어
-  if (l1 == l2 == n):
-      print v
+  if l1 == l2 == n:
+      print(v)
