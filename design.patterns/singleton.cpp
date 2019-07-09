7ea63014a36017e8df6944d9faa362e1fa42cc99
Lee Chang Ha <yielding@gmail.com>
2019-07-09 23:17:15 +0900

    ..

---
+++ design.patterns/singleton.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Database // : public boost::noncopyable
+{
+public:
+  static Database& get()
+  {
+    static Database db;
+    return db;
+  }
+
+protected:
+  Database() 
+  {
+  }
+};
+
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
