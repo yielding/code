2f6d1b6ec0423fd438fbdf500aaf24ed11be3dc9
Lee Chang Ha <yielding@gmail.com>
2019-09-08 22:49:41 +0900

    ..

--- zeromq/serv_cli.cpp
+++ zeromq/serv_cli.cpp
@@ -9,99 +9,108 @@
 #include <memory>
 #include <functional>
 
-#include "zmq.hpp"
-#include "zhelpers.hpp"
+#include <zmq.hpp>
+#include <zhelpers.hpp>
 
+using namespace std;
 
 //  This is our client task class.
 //  It connects to the server, and then sends a request once per second
 //  It collects responses as they arrive, and it prints them out. We will
 //  run several client tasks in parallel, each with a different random ID.
 //  Attention! -- this random work well only on linux.
-
-class client_task {
+class client_task 
+{
 public:
-    client_task()
-        : ctx_(1),
-          client_socket_(ctx_, ZMQ_DEALER)
-    {}
+  client_task()
+    : ctx_(1), client_socket_(ctx_, ZMQ_DEALER)
+  {}
 
-    void start() {
-        // generate random identity
-        char identity[10] = {};
-        sprintf(identity, "%04X-%04X", within(0x10000), within(0x10000));
-        printf("%s\n", identity);
-        client_socket_.setsockopt(ZMQ_IDENTITY, identity, strlen(identity));
-        client_socket_.connect("tcp://localhost:5570");
+  void start() 
+  {
+    // generate random identity
+    char identity[10] = {};
+    sprintf(identity, "%04X-%04X", within(0x10000), within(0x10000));
+    printf("%s\n", identity);
+    client_socket_.setsockopt(ZMQ_IDENTITY, identity, strlen(identity));
+    client_socket_.connect("tcp://localhost:5570");
 
-        zmq::pollitem_t items[] = {
-            { static_cast<void*>(client_socket_), 0, ZMQ_POLLIN, 0 } };
-        int request_nbr = 0;
-        try {
-            while (true) {
-                for (int i = 0; i < 100; ++i) {
-                    // 10 milliseconds
-                    zmq::poll(items, 1, 10);
-                    if (items[0].revents & ZMQ_POLLIN) {
-                        printf("\n%s ", identity);
-                        s_dump(client_socket_);
-                    }
-                }
-                char request_string[16] = {};
-                sprintf(request_string, "request #%d", ++request_nbr);
-                client_socket_.send(request_string, strlen(request_string));
-            }
+    zmq::pollitem_t items[] = {
+      { static_cast<void*>(client_socket_), 0, ZMQ_POLLIN, 0 } };
+
+    int request_nbr = 0;
+    try 
+    {
+      while (true) 
+      {
+        for (int i = 0; i < 100; ++i) 
+        {
+          // 10 milliseconds
+          zmq::poll(items, 1, 10);
+          if (items[0].revents & ZMQ_POLLIN) 
+          {
+            printf("\n%s ", identity);
+            s_dump(client_socket_);
+          }
         }
-        catch (std::exception &e) {}
+        char request_string[16] = {};
+        sprintf(request_string, "request #%d", ++request_nbr);
+        client_socket_.send(request_string, strlen(request_string));
+      }
     }
+    catch (std::exception &e) 
+    {}
+  }
 
 private:
-    zmq::context_t ctx_;
-    zmq::socket_t client_socket_;
+  zmq::context_t ctx_;
+  zmq::socket_t client_socket_;
 };
-
 
 //  .split worker task
 //  Each worker task works on one request at a time and sends a random number
 //  of replies back, with random delays between replies:
 
-class server_worker {
+class server_worker 
+{
 public:
-    server_worker(zmq::context_t &ctx, int sock_type)
-        : ctx_(ctx),
-          worker_(ctx_, sock_type)
-    {}
+  server_worker(zmq::context_t &ctx, int sock_type)
+    : ctx_(ctx), worker_(ctx_, sock_type)
+  {}
 
-    void work() {
-            worker_.connect("inproc://backend");
+  void work() 
+  {
+    worker_.connect("inproc://backend");
 
-        try {
-            while (true) {
-                zmq::message_t identity;
-                zmq::message_t msg;
-                zmq::message_t copied_id;
-                zmq::message_t copied_msg;
-                worker_.recv(&identity);
-                worker_.recv(&msg);
+    try 
+    {
+      while (true) 
+      {
+        zmq::message_t identity;
+        zmq::message_t msg;
+        zmq::message_t copied_id;
+        zmq::message_t copied_msg;
+        worker_.recv(&identity);
+        worker_.recv(&msg);
 
-                int replies = within(5);
-                for (int reply = 0; reply < replies; ++reply) {
-                    s_sleep(within(1000) + 1);
-                    copied_id.copy(&identity);
-                    copied_msg.copy(&msg);
-                    worker_.send(copied_id, ZMQ_SNDMORE);
-                    worker_.send(copied_msg);
-                }
-            }
+        int replies = within(5);
+        for (int reply = 0; reply < replies; ++reply) 
+        {
+          s_sleep(within(1000) + 1);
+          copied_id.copy(&identity);
+          copied_msg.copy(&msg);
+          worker_.send(copied_id, ZMQ_SNDMORE);
+          worker_.send(copied_msg);
         }
-        catch (std::exception &e) {}
+      }
     }
-
-
+    catch (std::exception &e) 
+    {}
+  }
 
 private:
-    zmq::context_t &ctx_;
-    zmq::socket_t worker_;
+  zmq::context_t &ctx_;
+  zmq::socket_t worker_;
 };
 
 
@@ -113,48 +122,50 @@ private:
 //  once.
 
 
-class server_task {
+class server_task 
+{
 public:
-    server_task()
-        : ctx_(1),
-          frontend_(ctx_, ZMQ_ROUTER),
-          backend_(ctx_, ZMQ_DEALER)
-    {}
+  server_task()
+    : ctx_(1), frontend_(ctx_, ZMQ_ROUTER), backend_(ctx_, ZMQ_DEALER)
+  {}
 
-    enum { kMaxThread = 5 };
+  enum { kMaxThread = 5 };
 
-    void run() {
-        frontend_.bind("tcp://*:5570");
-        backend_.bind("inproc://backend");
+  void run() 
+  {
+    frontend_.bind("tcp://*:5570");
+    backend_.bind("inproc://backend");
 
-        std::vector<server_worker *> worker;
-        std::vector<std::thread *> worker_thread;
-        for (int i = 0; i < kMaxThread; ++i) {
-            worker.push_back(new server_worker(ctx_, ZMQ_DEALER));
+    std::vector<server_worker *> worker;
+    std::vector<std::thread *> worker_thread;
+    for (int i = 0; i < kMaxThread; ++i) 
+    {
+      worker.push_back(new server_worker(ctx_, ZMQ_DEALER));
 
-            worker_thread.push_back(new std::thread(std::bind(&server_worker::work, worker[i])));
-            worker_thread[i]->detach();
-        }
-
-
-        try {
-            zmq::proxy(static_cast<void*>(frontend_),
-                       static_cast<void*>(backend_),
-                       nullptr);
-        }
-        catch (std::exception &e) {}
-
-        for (int i = 0; i < kMaxThread; ++i) {
-            delete worker[i];
-            delete worker_thread[i];
-        }
+      worker_thread.push_back(new std::thread(std::bind(&server_worker::work, worker[i])));
+      worker_thread[i]->detach();
     }
 
+    try 
+    {
+      zmq::proxy(static_cast<void*>(frontend_),
+          static_cast<void*>(backend_),
+          nullptr);
+    }
+    catch (std::exception &e) 
+    {}
+
+    for (int i = 0; i < kMaxThread; ++i) 
+    {
+      delete worker[i];
+      delete worker_thread[i];
+    }
+  }
 
 private:
-    zmq::context_t ctx_;
-    zmq::socket_t frontend_;
-    zmq::socket_t backend_;
+  zmq::context_t ctx_;
+  zmq::socket_t frontend_;
+  zmq::socket_t backend_;
 };
 
 
@@ -163,21 +174,21 @@ private:
 
 int main (void)
 {
-    client_task ct1;
-    client_task ct2;
-    client_task ct3;
-    server_task st;
+  client_task ct1;
+  client_task ct2;
+  client_task ct3;
+  server_task st;
 
-    std::thread t1(std::bind(&client_task::start, &ct1));
-    std::thread t2(std::bind(&client_task::start, &ct2));
-    std::thread t3(std::bind(&client_task::start, &ct3));
-    std::thread t4(std::bind(&server_task::run, &st));
+  thread t1(bind(&client_task::start, &ct1));
+  thread t2(bind(&client_task::start, &ct2));
+  thread t3(bind(&client_task::start, &ct3));
+  thread t4(bind(&server_task::run, &st));
 
-    t1.detach();
-    t2.detach();
-    t3.detach();
-    t4.detach();
+  t1.detach();
+  t2.detach();
+  t3.detach();
+  t4.detach();
 
-    getchar();
-    return 0;
+  getchar();
+  return 0;
 }
