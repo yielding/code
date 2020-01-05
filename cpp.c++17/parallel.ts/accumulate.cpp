647e578ed52f8eddd66d3ad2e07cec7119958afc
Lee Chang Ha <yielding@gmail.com>
2020-01-05 23:47:38 +0900

    ..

--- cpp.c++17/parallel.ts/accumulate.cpp
+++ cpp.c++17/parallel.ts/accumulate.cpp
@@ -14,16 +14,16 @@ int main()
     auto t1 = chrono::high_resolution_clock::now();
     auto result = accumulate(v.begin(), v.end(), 0.0);
     auto t2 = chrono::high_resolution_clock::now();
-    chrono::duration<double, std::milli> ms = t2 - t1;
+    chrono::duration<double, milli> ms = t2 - t1;
     cout << std::fixed << "std::accumulate result " << result
          << " took " << ms.count() << " ms\n";
   }
 
   {
     auto t1 = chrono::high_resolution_clock::now();
-    auto result = reduce(std::execution::par, v.begin(), v.end());
+    auto result = reduce(execution::par, v.begin(), v.end());
     auto t2 = chrono::high_resolution_clock::now();
-    chrono::duration<double, std::milli> ms = t2 - t1;
+    chrono::duration<double, milli> ms = t2 - t1;
     cout << "std::reduce result "
          << result << " took " << ms.count() << " ms\n";
   }
