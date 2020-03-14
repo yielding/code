25dfa69baf6e9d24d5d215196d603db299f056c4
Lee Chang Ha <yielding@gmail.com>
2020-03-14 10:37:14 +0900

    .

--- dtrace/prime/prime.cpp
+++ dtrace/prime/prime.cpp
@@ -9,35 +9,35 @@ long primecount = 1;
 
 int main(int argc, char const* argv[])
 {
-    long divisor = 0;
-    long currentprime = 5;
-    long isprime = 1;
+  long divisor = 0;
+  long currentprime = 5;
+  long isprime = 1;
 
-    if (PRIMES_PRIMECALC_START_ENABLED())
-        cout << "1\n";
-    else
-        cout << "0\n";
+  if (PRIMES_PRIMECALC_START_ENABLED())
+    cout << "1\n";
+  else
+    cout << "0\n";
 
-    while (currentprime < SIZE)
+  while (currentprime < SIZE)
+  {
+    isprime = 1;
+    PRIMES_PRIMECALC_START(currentprime);
+    for (divisor=0; divisor<primecount; divisor++)
     {
-        isprime = 1;
-        PRIMES_PRIMECALC_START(currentprime);
-        for (divisor=0; divisor<primecount; divisor++)
-        {
-            if (currentprime % primes[divisor] == 0)
-                isprime = 0;
-        }
-
-        PRIMES_PRIMECALC_DONE(currentprime, isprime);
-        if (isprime)
-        {
-            primes[primecount++] = currentprime;
-            PRIMES_PRIMECALC_TABLESIZE(primecount);
-            printf("%ld is a prime\n", currentprime);
-        }
-
-        currentprime = currentprime + 2;
+      if (currentprime % primes[divisor] == 0)
+        isprime = 0;
     }
 
-    return 0;
+    PRIMES_PRIMECALC_DONE(currentprime, isprime);
+    if (isprime)
+    {
+      primes[primecount++] = currentprime;
+      PRIMES_PRIMECALC_TABLESIZE(primecount);
+      printf("%ld is a prime\n", currentprime);
+    }
+
+    currentprime = currentprime + 2;
+  }
+
+  return 0;
 }
