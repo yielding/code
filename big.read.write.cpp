2a58e13524aa68a3d7a021ce146f89417c3fcff4
Lee Chang Ha <yielding@gmail.com>
2020-12-15 11:08:04 +0900

    added some more codes

---
+++ big.read.write.cpp
@@ -0,0 +1,69 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// NOTICE
+// intel cpy는 기본적으로 little endian
+// 1) little을 big으로 바꾸고 저장
+// 2) big을 읽어 little로 바꾸어 화면에 출력
+int toggle_endian(int num)
+{
+    return ((num >> 24) & 0x000000ff) | 
+           ((num <<  8) & 0x00ff0000) | 
+           ((num >>  8) & 0x0000ff00) | 
+           ((num << 24) & 0xff000000);
+}
+
+// 함수 작성 규칙
+// 1) 한 함수는 "오직" 한가지 일만 한다
+// 2) 그 일을 "잘" 해야한다
+int read_int4_be(ifstream& ifs)
+{
+    char buffer[4] = { 0 };
+    ifs.read(buffer, 4);
+
+    int res = *(int *)buffer;
+
+    return toggle_endian(res);
+}
+
+void write_int4_be(ofstream& ofs, int value)
+{
+    int res = toggle_endian(value);
+    ofs.write((char *)&res, 4);
+}
+
+int main(int argc, char *argv[])
+{
+    string path = "/Users/yielding/Desktop/data.bin";
+    ofstream ofs(path, ios_base::binary);
+
+    // 에러 처리는 확실하게
+    if (!ofs.good())
+    {
+        cout << "failed to open data file";
+        return EXIT_FAILURE;
+    }
+
+    for (int i=1; i<=10; i++)
+        write_int4_be(ofs, i);
+
+    // 동일 파일을 다시 open할 예정이므로 반드시 close
+    ofs.close();
+
+    ifstream ifs(path, ios_base::binary);
+    if (!ifs.good())
+    {
+        cout << "failed to open data file";
+        return EXIT_FAILURE;
+    }
+
+    for (int i=0; i<10; i++)
+        cout << read_int4_be(ifs) << endl;
+
+    return EXIT_SUCCESS;
+}
---
+++ cpp.boost/fs/fs.copy_file2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdexcept>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+      namespace fs = std::filesystem;
+
+int main(int argc, char const* argv[])
+{
+  string  src = "/Users/yielding/Desktop/deep.pdf";
+  string dest = "/tmp/a/b/c/d/e/f";
+
+  if (!fs::exists(dest))
+  {
+    auto ok = fs::create_directories(dest);
+    if (!ok)
+    {
+      cout << "failed to create " << dest << endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  try
+  {
+    fs::copy_file(src, dest + "/deep.pdf");
+  }
+  catch(fs::filesystem_error& e)
+  {
+    cout << "Could not copy: " << e.what() << endl;
+  }
+
+  return 0;
+}
