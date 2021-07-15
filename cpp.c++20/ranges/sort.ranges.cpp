c161234403fe03ed2148b7b9d3faff4cd48d9bd5
Lee Chang Ha <yielding@gmail.com>
2021-07-15 21:03:55 +0900

    ..

--- cpp.c++20/ranges/sort.ranges.cpp
+++ cpp.c++20/ranges/sort.ranges.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-// #include <format>
+#include <ranges>
+
 #include <boost/format.hpp>
 
 using namespace std;
+using namespace boost;
 
 struct user
 {
@@ -15,19 +17,18 @@ struct user
   auto to_s() const -> string 
   {
     // return format("name: {}, age, {}", name, age);
-    using namespace boost;
     return str(format("name: %s, age: %d") % name % age);
   }
 };
 
 int main(int argc, char *argv[])
 {
-  vector<user> users = { {"leech", 49}, {"kamin", 47}};
+  vector<user> users = { {"leech", 49}, {"kamin", 47} };
   auto sort_by_name = [](auto& l, auto& r) {
     return l.age < r.age;
   };
 
-  ranges::sort(users, sort_by_name);
+  std::ranges::sort(users, sort_by_name);
   for (auto& u: users)
     cout << u.to_s() << endl;
   
