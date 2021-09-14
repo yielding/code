9111987513fdecb1669d14e0160f6872c3888f93
Lee Chang Ha <yielding@gmail.com>
2021-09-14 23:18:48 +0900

    added scoped_array retrun test

--- cpp.c++17/scoped_array/unique_ptr.cpp
+++ cpp.c++17/scoped_array/unique_ptr.cpp
@@ -5,15 +5,29 @@ using namespace std;
 
 struct Test
 {
+  Test()
+  { cout << "default created..\n"; }
+
+  Test(Test const& t)
+  { cout << "copied..\n"; }
+
+  Test(Test &&t)
+  { cout << "moved..\n"; }
+
   ~Test()
-  {
-    cout <<"Test::dtor" <<endl;
-  }
+  { cout << "Test::dtor..\n"; }
 };
+
+auto return_array_of_uniq_ptr(int count) -> unique_ptr<Test[]>
+{
+  unique_ptr<Test[]> array(new Test[count]);
+
+  return array;
+}
 
 int main(int argc, char *argv[])
 {
-  unique_ptr<Test[]> array(new Test[3]);
+  auto arr = return_array_of_uniq_ptr(5);
   
   return 0;
 }
