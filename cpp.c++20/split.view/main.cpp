046d8cbc82be80c0cc92c49fcd099d5937fafe88
Lee Chang Ha <yielding@gmail.com>
2021-09-22 00:02:55 +0900

    added but not completed string.view split

---
+++ cpp.c++20/split.view/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ranges>
+#include <string_view>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  constexpr string_view works{""};
+  
+  return 0;
+}
