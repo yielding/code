0361dfbf4e43a69bd9d3ea140b886a75dcf40171
Lee Chang Ha <yielding@gmail.com>
2021-09-22 21:52:00 +0900

    with range-v3 1st

--- cpp.c++20/ranges/filter.user.cpp
+++ cpp.c++20/ranges/filter.user.cpp
@@ -1,20 +1,26 @@
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 #include <functional> 
-// #include <format>
-#include <ranges>
+#include <string>
+#include <range/v3/view/filter.hpp>
+#include <range/v3/view/transform.hpp>
+#include <range/v3/algorithm/copy.hpp>
+
 #include <boost/format.hpp>
 
-using namespace std;
+using namespace ranges;
+
+using std::cout;
+using std::vector;
+using std::ostream_iterator;
+using std::not_fn;
 
 struct user
 {
-  string name;
+  std::string name;
   int age;
 
-  auto to_s() const -> string 
+  auto to_s() const -> std::string 
   {
     using namespace boost;
     return str(format("name: %s, age: %d") % name % age);
@@ -27,12 +33,11 @@ int main(int argc, char *argv[])
 {
   vector<user> users = { {"leech", 49}, {"kamin", 47}, {"gunhee", 17} };
   auto result = users
-    | views::filter(not_fn(underage))
+    | views::filter(std::not_fn(underage))
     | views::transform([] (const auto& u) { return u.age; });
 
-  
-  // ranges::copy(result, ostream_iterator<int>(cout, "\n"));
-  cout << ranges::views::all(result);
+  copy(result, ostream_iterator<int>(cout, "\n"));
+  cout << views::all(result);
   
   return 0;
 }
