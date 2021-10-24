cf93ad2a19fcc14cc47ada0c401584cf4d4abb48
Chang Ha Lee <yielding@gmail.com>
2021-10-24 21:09:04 +0900

    merged code

--- cpp.c++20/views/cycle/main.cpp
+++ cpp.c++20/views/cycle/main.cpp
@@ -16,7 +16,6 @@ int main(int argc, char *argv[])
   auto v = {1, 3, 9};
 
   auto rng = v | views::cycle;
-<<<<<<< HEAD
 
   int c=0;
   for (auto i: rng) 
@@ -24,9 +23,8 @@ int main(int argc, char *argv[])
     std::cout << i;
     if (c++ == 10) break;
   }
-=======
+
   repeat(10, [&] { std::cout << rng << " "; });
->>>>>>> 33070fd591db709987dfed7d6fdb064f78488a54
   
   return 0;
 }
