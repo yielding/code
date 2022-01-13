14d44d8bb1bb84da5ed6a623d1fe1212988d3aa3
Chang Ha Lee <yielding@gmail.com>
2022-01-13 23:28:48 +0900

    minor

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -1,17 +1,19 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  RenderWindow window(VideoMode(200, 200), "SFML works!");
+  CircleShape shape(100.f);
+  shape.setFillColor(Color::Green);
 
   while (window.isOpen())
   {
-    sf::Event event;
+    Event event;
     while (window.pollEvent(event))
     {
-      if (event.type == sf::Event::Closed)
+      if (event.type == Event::Closed)
         window.close();
     }
 
