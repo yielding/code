5d09d880f59f78d717fc593968298e974c8db116
Chang Ha Lee <yielding@gmail.com>
2022-03-14 21:59:50 +0900

    update to correctly generate dot.ccls

--- cpp.c++20/jthread/jthread.cpp
+++ cpp.c++20/jthread/jthread.cpp
@@ -2,29 +2,33 @@
 #include <iostream>
 #include <thread>
 
-using namespace std::chrono;
+using namespace std;
+using namespace chrono_literals;
 
-int main() {
+int main() 
+{
   // Let's create a jthread an pass it a stop token
-  std::jthread jt0([](std::stop_token tk) {
-    // Print in a loop
-    for (int i = 0; i < 10; i++) {
-      std::cout << "Printing value: " << i << '\n';
+  jthread jt0([](stop_token tk) {
+    // print in a loop
+    for (int i = 0; i < 10; i++)
+    {
+      cout << "Printing value: " << i << '\n';
 
       // Stop, if that has been requested
-      if (tk.stop_requested()) {
-        std::cout << "Stopping the jthread!\n";
+      if (tk.stop_requested()) 
+      {
+        cout << "Stopping the jthread!\n";
         return;
       }
 
-      std::this_thread::sleep_for(seconds(1));
+      this_thread::sleep_for(1s);
     }
   });
 
   // Pause the main thread before cancelling the jthread
-  std::this_thread::sleep_for(seconds(5));
+  this_thread::sleep_for(5s);
 
-  // Request for our jthread to stop
+  // request for our jthread to stop
   jt0.request_stop();
 
   return 0;
