a99a28bde40e951a94b757c514c8acec0dd2cbf4
Chang Ha Lee <yielding@gmail.com>
2022-03-25 10:24:45 +0900

    added homography

---
+++ opencv/homography/main.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+void find_homography()
+{
+  auto src1 = imread("", IMREAD_GRAYSCALE);
+  auto src2 = imread("", IMREAD_GRAYSCALE);
+
+}
+
+int main(int argc, char* argv[])
+{
+
+  return 0;
+}
