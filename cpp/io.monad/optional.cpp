eaa5930ca6ef9073075cc8bee760e9b33532c5ec
Lee Chang Ha <yielding@gmail.com>
2022-07-26 21:08:44 +0900

    optional.cpp

---
+++ cpp/io.monad/optional.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+template<class T> 
+class optional 
+{
+public:
+  optional() : _isValid(false) {} // Nothing
+  optional(T x) : _isValid(true) , _v(x) {} // Just
+
+  bool isValid() const { return _isValid; }
+  T val() const { return _v; }
+
+  template <typename R>
+  auto fmap(function<R(T)>& f) -> optional<R>
+  {
+      return this->isValid()
+        ? optional<R> { f(_v) }
+        : optional<R> {};
+  }
+
+private:
+  bool _isValid; // the tag
+  T _v; 
+};
+
+template <typename A, typename B>
+auto fmap(function<B(A)> f) -> function<optional<B>(optional<A>)>
+{
+  return [f](optional<A> opt) {
+    return opt.isValid()
+      ? optional<B> { f(opt.val()) }
+      : optional<B> {};
+  };
+}
+
+template <typename A, typename B>
+auto fmap(function<B(A)> f, optional<A> opt) -> optional<B>
+{
+  return opt.isValid()
+    ? optional<B> { f(opt.val()) }
+    : optional<B> { };
+}
+
+int main(int argc, char *argv[])
+{
+  optional<int> a(2);
+  optional<int> b(4);
+
+  function<int(int)> f = 
+    [](int i) -> int { return i*i; };
+
+  auto c = a.fmap(f);
+
+  cout << c.val();
+
+  return 0;
+}
