81a051792f66b632fb47f148c0ab20d01bb39392
Lee Chang Ha <yielding@gmail.com>
2022-08-26 08:09:51 +0900

    ..

--- haskell/read.maybe.hs
+++ haskell/read.maybe.hs
@@ -10,3 +10,13 @@ interactiveDoubling = do
       putStrLn "This is not a valid number. Retrying..."
       interactiveDoubling
 
+interactiveDoubleing2 = do 
+  putStrLn "Choose a number:"
+  s <- getLine
+  let mx = readMaybe s :: Maybe Double
+  case fmap (2*) mx of
+    Just d -> putStrLn("The double of your number is " ++ show d)
+    Nothing -> do
+      putStrLn "This is not a valid number. Retrying..."
+      interactiveDoubleing2
+
--- haskell/read.maybe2.hs
+++ haskell/read.maybe2.hs
@@ -4,7 +4,7 @@ interactiveDoubleing2 = do
   putStrLn "Choose a number:"
   s <- getLine
   let mx = readMaybe s :: Maybe Double
-  case fmap (2*x) mx of
+  case fmap (2*) mx of
     Just d -> putStrLn("The double of your number is " ++ show d)
     Nothing -> do
       putStrLn "This is not a valid number. Retrying..."
