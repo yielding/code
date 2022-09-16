56eb88d48199a931a1cb242857c5d00a6ec6a580
Lee Chang Ha <yielding@gmail.com>
2022-09-16 17:22:09 +0900

    ..

---
+++ haskell/qsort.hs
@@ -0,0 +1,7 @@
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+               where
+                   smaller = [a | a <- xs, a <= x]
+                   larger  = [b | b <- xs, b > x]
+
