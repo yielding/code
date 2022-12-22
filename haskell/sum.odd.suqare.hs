ca21f0382adcbe864cfd89dae27c488fc033074f
Chang Ha Lee <yielding@gmail.com>
2022-12-23 08:26:35 +0900

    ..

--- haskell/sum.odd.suqare.hs
+++ haskell/sum.odd.suqare.hs
@@ -5,12 +5,12 @@ sum_odd_square1 (x:xs)
 
 sum_odd_square2 [] = 0
 sum_odd_square2 xs = foldr (+) 0 $ map square $ filter odd xs
-  where 
+  where
     square x = x * x
 
-sum_odd_square3 = sum . map (^2) . filter odd 
+sum_odd_square3 = sum . map (^2) . filter odd
 
-main = do 
+main = do
   print $ sum_odd_square3 [1, 3]
   print $ sum_odd_square2 [1, 3]
   print $ sum_odd_square1 [1, 3]
