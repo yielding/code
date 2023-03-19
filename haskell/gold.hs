c61303a3b4cc3ee1f066cb34658543ba24e64595
Chang Ha Lee <yielding@gmail.com>
2023-03-19 12:50:08 +0900

    ..

---
+++ haskell/gold.hs
@@ -0,0 +1,13 @@
+module Gold where
+
+phi :: Double
+phi = (sqrt 5 + 1) / 2
+
+polynomial :: Double -> Double
+polynomial x = x ^ 2 - x + 1
+
+f x = polynomial (polynomial x)
+
+main = do
+  print(polynomial phi)
+  print(f phi)
