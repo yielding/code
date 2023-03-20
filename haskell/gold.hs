2a81f6993bd37be08ac948cf97ba407d8c96a3f6
Chang Ha Lee <yielding@gmail.com>
2023-03-20 20:48:05 +0900

    ..

--- haskell/gold.hs
+++ haskell/gold.hs
@@ -1,13 +1,14 @@
 module Gold where
 
+-- The golden ratio
 phi :: Double
 phi = (sqrt 5 + 1) / 2
 
 polynomial :: Double -> Double
-polynomial x = x ^ 2 - x + 1
+polynomial x = x ^ 2 - x - 1
 
 f x = polynomial (polynomial x)
 
 main = do
-  print(polynomial phi)
-  print(f phi)
+  print (polynomial phi)
+  print (f phi)
