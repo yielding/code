40b7dce1c671ea92099e5e9dc81c1e7abe18bafa
Chang Ha Lee <yielding@gmail.com>
2023-03-23 21:53:55 +0900

    ..

--- cpp.boost/active.object/active.object.cpp
+++ cpp.boost/active.object/active.object.cpp
@@ -14,7 +14,11 @@ int main(int argc, char* argv[])
   cout << "now start...\n";
 
   o.post([] { cout << "hello world first " << endl; });
-  o.dispatch([] { cout << "hello world second" << endl; });
+  cout << "in between 1 ...\n";
+  cout << "in between 2 ...\n";
+  cout << "in between 3 ...\n";
+  o.post([] { cout << "hello world second" << endl; });
+  //o.dispatch([] { cout << "hello world second" << endl; });
 
   this_thread::sleep_for(1s);
 
