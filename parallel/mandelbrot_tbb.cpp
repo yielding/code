aed9d8a36eead7fdde0e61769c2342cf8f759aaa
Chang Ha Lee <yielding@gmail.com>
2023-03-26 21:32:59 +0900

    .

--- parallel/mandelbrot_tbb.cpp
+++ parallel/mandelbrot_tbb.cpp
@@ -1,4 +1,3 @@
-
 #include <limits>
 #include <complex>
 #include <chrono>
@@ -11,67 +10,68 @@
 #include "stb_image_write.h"
 
 template <typename T, typename U>
-inline std::complex<T> operator*(const std::complex<T>& lhs, const U& rhs)
+inline complex<T> operator*(const complex<T>& lhs, const U& rhs)
 {
-    return lhs * T(rhs);
+  return lhs * T(rhs);
 }
 
 template <typename T, typename U>
-inline std::complex<T> operator*(const U& lhs, const std::complex<T>& rhs)
+inline complex<T> operator*(const U& lhs, const complex<T>& rhs)
 {
-    return T(lhs) * rhs;
+  return T(lhs) * rhs;
 }
 
 template<typename Iterate, typename IterationMap, typename T>
-int boundedorbit(Iterate f, std::complex<T> seed, int bound, int bailout=100, 
-                 IterationMap itmap = [](int n, std::complex<T> z, int bailout) { return n; })
+int boundedorbit(Iterate f, complex<T> seed, int bound, int bailout=100, 
+                 IterationMap itmap = [](int n, complex<T> z, int bailout) { return n; })
 {
-    auto z = f(seed);
-    for (auto k : boost::irange(1, bailout)) {
-        if (abs(z) > bound) 
-            return itmap(k, z, bailout);
-        z = f(z);
-    }
-    return std::numeric_limits<int>::min();
+  auto z = f(seed);
+  for (auto k : boost::irange(1, bailout)) {
+    if (abs(z) > bound) 
+      return itmap(k, z, bailout);
+    z = f(z);
+  }
+
+  return numeric_limits<int>::min();
 }
 
 template<typename T>
-float normalized_iterations(int n, std::complex<T> zn, int bailout)
+float normalized_iterations(int n, complex<T> zn, int bailout)
 {
-    return n + (log(log(bailout))-log(log(abs(zn))))/log(2);
+  return n + (log(log(bailout))-log(log(abs(zn))))/log(2);
 }
 
 int main(int, char**)
 {
-    // allow complex literal
-    using namespace std::literals;
-    
-    // initialize TBB
-    tbb::task_scheduler_init init;
+  // allow complex literal
+  using namespace literals;
+  
+  // initialize TBB
+  tbb::task_scheduler_init init;
 
-    // allocate storage
-    // TODO don't leak memory
-    unsigned char* iteration_counts = new unsigned char[3500*2500];
+  // allocate storage
+  // TODO don't leak memory
+  auto iteration_counts = new unsigned char[3500*2500];
 
-    auto start = std::chrono::high_resolution_clock::now();
+  auto start = chrono::high_resolution_clock::now();
 
-    const int GRAIN = 100;
-    
-    tbb::parallel_for(tbb::blocked_range2d<int>(0, 2500, GRAIN, 0, 3500, GRAIN), [&](auto r) {
-        for (auto j = r.rows().begin(); j != r.rows().end(); j++) {
-            for (auto k = r.cols().begin(); k != r.cols().end(); k++) {
-                auto c = (-2.5 + k*3.5/3500.0) + (-1.25i + j*2.5i/2500.0);
-                iteration_counts[3500*j + k] = boundedorbit([&c](auto z) { return z*z + c; }, 0.0i, 2, 200, &normalized_iterations<double>);
-            }
-        }
-    });
+  const int GRAIN = 100;
+  
+  tbb::parallel_for(tbb::blocked_range2d<int>(0, 2500, GRAIN, 0, 3500, GRAIN), [&](auto r) {
+      for (auto j = r.rows().begin(); j != r.rows().end(); j++) {
+          for (auto k = r.cols().begin(); k != r.cols().end(); k++) {
+              auto c = (-2.5 + k*3.5/3500.0) + (-1.25i + j*2.5i/2500.0);
+              iteration_counts[3500*j + k] = boundedorbit([&c](auto z) { return z*z + c; }, 0.0i, 2, 200, &normalized_iterations<double>);
+          }
+      }
+  });
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    
-    std::cout << "computation took " << elapsed_seconds.count() << "s" << std::endl;
+  auto end = chrono::high_resolution_clock::now();
+  chrono::duration<double> elapsed_seconds = end - start;
+  
+  cout << "computation took " << elapsed_seconds.count() << "s" << endl;
 
-    stbi_write_png("mandelbrot_tbb.png", 3500, 2500, 1, iteration_counts, 3500);
+  stbi_write_png("mandelbrot_tbb.png", 3500, 2500, 1, iteration_counts, 3500);
 
-    return 0;
+  return 0;
 }
--- parallel/mandelbrotcl.cpp
+++ parallel/mandelbrotcl.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <chrono>
@@ -9,98 +8,101 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
+  try {
+    vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    vector<cl::Device> platformDevices;
+    platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &platformDevices);
+
+    cl::Context context(platformDevices);
+    auto contextDevices = context.getInfo<CL_CONTEXT_DEVICES>();
+    for (auto dev : contextDevices)
+      cout << "Using " << dev.getInfo<CL_DEVICE_NAME>() << endl;
+    
+
+    ifstream programFile("mandelbrot.cl");
+    string programString(istreambuf_iterator<char>(programFile), (istreambuf_iterator<char>()));
+
+    cl::Program::Sources source(1, make_pair(programString.c_str(), programString.length()+1));
+    cl::Program program(context, source);
     try {
-        std::vector<cl::Platform> platforms;
-        cl::Platform::get(&platforms);
-
-        std::vector<cl::Device> platformDevices;
-        platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &platformDevices);
-
-        cl::Context context(platformDevices);
-        auto contextDevices = context.getInfo<CL_CONTEXT_DEVICES>();
-        for (auto dev : contextDevices) {
-            std::cout << "Using " << dev.getInfo<CL_DEVICE_NAME>() << std::endl;
-        }
-
-        std::ifstream programFile("mandelbrot.cl");
-        std::string programString(std::istreambuf_iterator<char>(programFile), (std::istreambuf_iterator<char>()));
-
-        cl::Program::Sources source(1, std::make_pair(programString.c_str(), programString.length()+1));
-        cl::Program program(context, source);
-        try {
-            program.build(contextDevices);
-        } catch (cl::Error e) {
-            // FIXME may not be the device that failed
-            std::cout << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(contextDevices[0]) << std::endl;
-        }
-        
-        cl::Kernel mandelbrot(program, "mandelbrot");
-
-        // command queues
-        std::vector<cl::CommandQueue> queues;
-        for (auto device : contextDevices) {
-            cl::CommandQueue queue(context, device, CL_QUEUE_PROFILING_ENABLE);
-            queues.push_back(queue);
-        }
-
-unsigned char* iteration_counts = new unsigned char[3500*2500];
-
-        auto start = std::chrono::high_resolution_clock::now();
-
-        // partition the "y" dimension
-        int i = 0;
-        int workItemsPerQueue = 2500/queues.size(); // FIXME requires work size to be evenly divisible by number of queues
-        std::vector<cl::Buffer> outputs;
-        
-        for (auto queue : queues) {
-            cl::NDRange offset(0, 0); //i*workItemsPerQueue);
-            cl::NDRange global_size(3500, workItemsPerQueue);
-
-            // storage for results per device        
-            cl_int err = CL_SUCCESS;
-            cl::Buffer output(context, CL_MEM_WRITE_ONLY, (size_t)3500*workItemsPerQueue, (void*)NULL, &err);
-            mandelbrot.setArg(0, output);
-            mandelbrot.setArg(1, i*workItemsPerQueue);
-            outputs.push_back(output);
-            
-            queue.enqueueNDRangeKernel(mandelbrot, offset, global_size);
-            queue.enqueueBarrierWithWaitList();
-
-            std::cout << "enqueued range " << i*workItemsPerQueue << " of length " << workItemsPerQueue << std::endl;
-
-            i++;
-        }
-
-        // read results
-        unsigned char* results = new unsigned char[3500*2500];
-        std::vector<cl::Event> readWaitList;
-
-        i = 0;
-        for (auto queue : queues) {
-            size_t offset = i*3500*workItemsPerQueue;
-            cl::Event readDoneEvent;
-            queue.enqueueReadBuffer(outputs[i], CL_FALSE, 0, 3500*workItemsPerQueue, &(results[offset]), NULL, &readDoneEvent);
-
-            // NOTE: can't push onto vector until the event is valid, since it will be copied
-            readWaitList.push_back(readDoneEvent);
-
-            i++;
-        }
-            
-        cl::Event::waitForEvents(readWaitList);
-
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        
-        std::cout << "computation took " << elapsed_seconds.count() << "s" << std::endl;
-
-        stbi_write_png("mandelbrot_cl.png", 3500, 2500, 1, results, 3500);
-
+      program.build(contextDevices);
     } catch (cl::Error e) {
-        std::cout << e.what() << ": Error code " << e.err() << std::endl;
+      // FIXME may not be the device that failed
+      cout << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(contextDevices[0]) << endl;
     }
 
-    return 0;
+    cl::Kernel mandelbrot(program, "mandelbrot");
+
+    // command queues
+    vector<cl::CommandQueue> queues;
+    for (auto device : contextDevices) 
+    {
+      cl::CommandQueue queue(context, device, CL_QUEUE_PROFILING_ENABLE);
+      queues.push_back(queue);
+    }
+
+    auto iteration_counts = new unsigned char[3500*2500];
+
+    auto start = chrono::high_resolution_clock::now();
+
+    // partition the "y" dimension
+    int i = 0;
+    int workItemsPerQueue = 2500/queues.size(); // FIXME requires work size to be evenly divisible by number of queues
+    vector<cl::Buffer> outputs;
+
+    for (auto queue : queues) {
+      cl::NDRange offset(0, 0); //i*workItemsPerQueue);
+      cl::NDRange global_size(3500, workItemsPerQueue);
+
+      // storage for results per device        
+      cl_int err = CL_SUCCESS;
+      cl::Buffer output(context, CL_MEM_WRITE_ONLY, (size_t)3500*workItemsPerQueue, (void*)NULL, &err);
+      mandelbrot.setArg(0, output);
+      mandelbrot.setArg(1, i*workItemsPerQueue);
+      outputs.push_back(output);
+
+      queue.enqueueNDRangeKernel(mandelbrot, offset, global_size);
+      queue.enqueueBarrierWithWaitList();
+
+      cout << "enqueued range " << i*workItemsPerQueue << " of length " << workItemsPerQueue << endl;
+
+      i++;
+    }
+
+    // read results
+    auto results = new unsigned char[3500*2500];
+    vector<cl::Event> readWaitList;
+
+    i = 0;
+    for (auto queue : queues) {
+      size_t offset = i*3500*workItemsPerQueue;
+      cl::Event readDoneEvent;
+      queue.enqueueReadBuffer(outputs[i], CL_FALSE, 0, 3500*workItemsPerQueue, &(results[offset]), NULL, &readDoneEvent);
+
+      // NOTE: can't push onto vector until the event is valid, since it will be copied
+      readWaitList.push_back(readDoneEvent);
+
+      i++;
+    }
+
+    cl::Event::waitForEvents(readWaitList);
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed_seconds = end - start;
+
+    cout << "computation took " << elapsed_seconds.count() << "s" << endl;
+
+    stbi_write_png("mandelbrot_cl.png", 3500, 2500, 1, results, 3500);
+
+  } catch (cl::Error e) {
+    cout << e.what() << ": Error code " << e.err() << endl;
+  }
+
+  return 0;
 }
