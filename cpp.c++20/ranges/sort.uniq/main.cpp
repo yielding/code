1e4bf09046fdf8de7b582e356e15ee02dbd70d9a
Chang Ha Lee <yielding@gmail.com>
2023-03-29 23:13:23 +0900

    added sort.uniq

---
+++ cpp.c++20/ranges/sort.uniq/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+#include <range/v3/action/sort.hpp>
+#include <range/v3/action/unique.hpp>
+#include <range/v3/view/all.hpp>
+
+using namespace ranges;
+using namespace std;
+
+int main()
+{
+  vector<int> vi{
+    9, 4, 5, 2, 9, 1, 0, 2, 6, 7, 4, 5, 6, 5, 9, 2, 7,
+    1, 4, 5, 3, 8, 5, 0, 2, 9, 3, 7, 5, 7, 5, 5, 6, 1,
+    4, 3, 1, 8, 4, 0, 7, 8, 8, 2, 6, 5, 3, 4, 5};
+
+  vi |= actions::sort | actions::unique;
+  // prints: [0,1,2,3,4,5,6,7,8,9]
+
+  cout << views::all(vi) << '\n';
+
+  return 0;
+}
