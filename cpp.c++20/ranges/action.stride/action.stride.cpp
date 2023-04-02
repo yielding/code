5ccf9ce563949fe3bd186d5ef338cd8e91d7795d
Chang Ha Lee <yielding@gmail.com>
2023-04-02 20:56:31 +0900

    updated

---
+++ cpp.c++20/ranges/action.stride/action.stride.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+#include <range/v3/algorithm/copy.hpp>
+#include <range/v3/all.hpp>
+
+using namespace ranges;
+using std::cout, 
+      std::endl;
+
+int main(int argc, char *argv[])
+{
+  auto v1 = views::ints(0, 100) | to<std::vector>();
+
+  auto v2 = v1 | copy | actions::stride(10);
+  cout << views::all(v2) << endl; // {0, 10, 20, 30, ..., 90}
+
+  v2 |= actions::stride(4);
+  cout << views::all(v2) << endl; // {0, 40, 80}
+
+  v2 |= actions::stride(2);
+  cout << views::all(v2) << endl; // {0, 80}
+
+  v2 |= actions::stride(10);
+  cout << views::all(v2) << endl;
+  
+  return 0;
+}
