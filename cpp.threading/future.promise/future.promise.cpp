a2beffb161e93c9bdf8ea1bb412256fa9184b726
Chang Ha Lee <yielding@gmail.com>
2023-04-30 00:02:44 +0900

    ..

--- cpp.threading/future.promise/future.promise.cpp
+++ cpp.threading/future.promise/future.promise.cpp
@@ -6,13 +6,19 @@
 
 using namespace std;
 
+
+// NOTICE
+// from https://modoocode.com/284#page-heading-4
+// 이 예제는 promise / future를 thread의 동시 시작을 atomic하게 하기 위한 방법
+//
+
 void runner(shared_future<void> start) 
 {
   start.get();
   cout << "출발!" << endl;
 }
 
-int main() 
+int main()
 {
   promise<void> p;
   shared_future<void> start = p.get_future();
@@ -22,7 +28,7 @@ int main()
   thread t3(runner, start);
 
   // 참고로 cerr 는 std::cout 과는 다르게 버퍼를 사용하지 않기 때문에 
-  // 터미널에 바로 출력된다.
+  // 터미널에 바로 출력.
   cerr << "준비...";
   this_thread::sleep_for(chrono::seconds(1));
   cerr << "땅!" << endl;
