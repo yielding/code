8b59caef265d4f000e6014e09d318c17c5b46e98
Chang Ha Lee <yielding@gmail.com>
2023-04-29 20:33:23 +0900

    future to cpp.threading

---
+++ cpp.threading/future.promise/future.promise.cpp
@@ -0,0 +1,35 @@
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+void runner(shared_future<void> start) 
+{
+  start.get();
+  cout << "출발!" << endl;
+}
+
+int main() 
+{
+  promise<void> p;
+  shared_future<void> start = p.get_future();
+
+  thread t1(runner, start);
+  thread t2(runner, start);
+  thread t3(runner, start);
+
+  // 참고로 cerr 는 std::cout 과는 다르게 버퍼를 사용하지 않기 때문에 
+  // 터미널에 바로 출력된다.
+  cerr << "준비...";
+  this_thread::sleep_for(chrono::seconds(1));
+  cerr << "땅!" << endl;
+
+  p.set_value();
+
+  t1.join(); t2.join(); t3.join();
+
+  return 0;
+}
