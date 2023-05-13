50d1a5d0a5e951688e66cd29d22d864d5825e883
Chang Ha Lee <yielding@gmail.com>
2023-05-13 22:19:10 +0900

    ..

--- cpp.fp/seconds.to.duration/main.cpp
+++ cpp.fp/seconds.to.duration/main.cpp
@@ -7,30 +7,32 @@ namespace rv = ranges::views;
 namespace rg = ranges;
 using namespace std;
 
+//
 // TODO
-// 1. review exclusive_scan
+// 1. review exclusive_scan(0) of {1, 2, 3}
+//    = {0, 0+1, 0+1+2, 0+1+2+3}
 // 2. why separate rv::reverse in line 35
+//
 int main(int argc, char* argv[])
 {
   auto sec = 123456;
-  auto const v = vector{60, 60, 24, 7};
-  auto const names = vector<string> {
-    "s", "min", "h", "d" };
+  auto const times = vector{ 60, 60, 24, 7 };
+  auto const names = vector<string> { "s", "min", "h", "d" };
 
-  auto div_pos = v 
+  auto div_pos = times
     | rv::exclusive_scan(sec, divides{})
-    | rv::take_while([](int x) { return x > 0; })
-    ;
+    | rv::take_while([](int x) { return x > 0; });
 
   auto mods = rv::zip_with(
-      [](int a, int b) { return a % b; },
-      div_pos, v);
+      [](int a, int b) { return a % b; },  // 여기서 % 연산이 빛난다.
+      div_pos, times);
 
   auto pairs = rv::zip_with(
       [](int a, auto const& s) { return to_string(a) + s; },
       mods, names)
-    | rg::to<vector<string>>
-    ;
+    | rg::to<vector<string>>;
+
+  cout << rv::all(pairs) << endl;
 
   auto res = pairs | rv::reverse;
 
