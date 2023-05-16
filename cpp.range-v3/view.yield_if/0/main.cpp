58c63b2d50697be3d8f522c0852496dac7a4b875
Chang Ha Lee <yielding@gmail.com>
2023-05-16 23:57:42 +0900

    ..

--- cpp.range-v3/view.yield_if/0/main.cpp
+++ cpp.range-v3/view.yield_if/0/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <format>
 #include <range/v3/all.hpp>
 
 namespace g = ranges;
@@ -10,13 +9,12 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  auto v = vector{7, 0, 2, 3, 1, 4, 6}; 
+  auto const& data = vector{7, 0, 2, 3, 1, 4, 6}; 
 
-  auto rng = v 
+  auto rng = data 
     | v::enumerate 
     | v::for_each([](auto const& p) {
-        return g::yield_if(p.first == p.second, 
-                           p.second);
+        return g::yield_if(p.first == p.second, p.second);
       });
 
   cout << v::all(rng) << endl;
