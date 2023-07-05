726a5cc6e722ad2e5a096f3600bba09777f2e219
Chang Ha Lee <yielding@gmail.com>
2023-07-05 19:21:02 +0900

    .

--- cpp.range-v3/range.to/string_to_lower.cpp
+++ cpp.range-v3/range.to/string_to_lower.cpp
@@ -4,24 +4,25 @@
 
 #include <range/v3/all.hpp>
 
-using namespace ranges::v3;
-using std::string;
+namespace v = ranges::views;
+namespace g = ranges;
+
+using namespace std;
 
 auto string_to_lower(string const& s) -> string 
 {
-  return s | views::transform(tolower)
-           | to<string>;
+  return s | v::transform(::tolower)
+           | g::to<string>;
 }
 
 auto string_only_alnum(string const& s) -> string
 {
-  return s | views::filter(isalnum)
-           | to<string>;
+  return s | v::filter(::isalnum)
+           | g::to<string>;
 }
 
 int main(int argc, char* argv[])
 {
-  using namespace std;
   cout << string_to_lower("HHH") << endl;
   cout << string_only_alnum("H!H2H") << endl;
 
