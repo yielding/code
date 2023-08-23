0e06507b9cec49a178faca027b2761f6878df048
Chang Ha Lee <yielding@gmail.com>
2023-08-24 00:35:31 +0900

    ..

--- cpp.fp/thousand.comma/main.cpp
+++ cpp.fp/thousand.comma/main.cpp
@@ -17,10 +17,26 @@ auto add_comma(string const& s, int count) -> string
   return r0 | reverse | to<string>;
 }
 
+auto add_comma2(string const& s, int count) -> string
+{
+  auto res = s;
+  auto len = (int)res.length();
+  auto pos = count;
+  while (len - pos > 0)
+  {
+    res.insert(len - pos, ",");
+    pos += count;
+  }
+
+  return res;
+}
+
 int main(int argc, char* argv[])
 {
-  assert(add_comma("1234000"s, 3) == "1,234,000");
-  assert(add_comma("12"s, 3) == "12");
+  // assert(add_comma("1234000"s, 3) == "1,234,000");
+  // assert(add_comma("12"s, 3) == "12");
+
+  cout << add_comma2("1234000"s, 4);
 
   return 0;
 }
