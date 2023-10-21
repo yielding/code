a8621b364ed6ff82e1eafd51baddba80693ce532
Chang Ha Lee <yielding@gmail.com>
2023-10-21 09:51:32 +0900

    ..

--- cpp.threading/producer.consumer/producer.consumer.cpp
+++ cpp.threading/producer.consumer/producer.consumer.cpp
@@ -13,17 +13,14 @@ void producer(queue<string>* downloaded_pages, mutex* m, int index, condition_va
 {
   for (int i = 0; i < 5; i++) 
   {
-    // 웹사이트를 다운로드 하는데 걸리는 시간이라 생각하면 된다.
-    // 각 쓰레드 별로 다운로드 하는데 걸리는 시간이 다르다.
+    // fake random work for explanation
     this_thread::sleep_for(chrono::milliseconds(100 * index));
     string content = "site : " + to_string(i) + " from thread(" + to_string(index) + ")\n";
 
-    // data 는 쓰레드 사이에서 공유되므로 critical section 에 넣어야 한다.
     m->lock();
     downloaded_pages->push(content);
     m->unlock();
 
-    // consumer 에게 content 가 준비되었음을 알린다.
     cv->notify_one();
   } 
 } 
@@ -31,7 +28,7 @@ void producer(queue<string>* downloaded_pages, mutex* m, int index, condition_va
 void consumer(queue<string>* downloaded_pages, mutex* m,
               int* num_processed, condition_variable* cv) 
 {
-  while (*num_processed < 25)  // 25는 탈출조건
+  while (*num_processed < 25)  // 25: exit condition
   {
     unique_lock<mutex> lk(*m);
 
