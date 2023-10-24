a9854395f831aa655c4e85d4d1400ed67d9235e0
Chang Ha Lee <yielding@gmail.com>
2023-10-24 12:50:57 +0900

    ..

--- lang.haskell/monad/turnstile.hs
+++ lang.haskell/monad/turnstile.hs
@@ -1,13 +1,26 @@
+
 data TurnstileState = Locked | Unlocked
   deriving (Eq, Show)
-
 
 data TurnstileOutput = Thank | Open | Tut
   deriving (Eq, Show)
 
-coin, push :: TurnstileState -> TurnstileOutput
+coin, push :: TurnstileState -> (TurnstileOutput, TurnstileState)
 
-coin _ = Thank
+coin _ = (Thank, Unlocked)
 
-push Unlocked = Open
-push Locked   = Tut
+push Locked   = (Tut , Locked)
+push Unlocked = (Open, Locked)
+
+monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+monday s0 =
+  let (a1, s1) = coin s0
+      (a2, s2) = push s1
+      (a3, s3) = push s2
+      (a4, s4) = coin s3
+      (a5, s5) = push s4
+  in ([a1, a2, a3, a4, a5], s5)
+
+main :: IO ()
+main = do 
+  print "hi"
