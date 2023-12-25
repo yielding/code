94e4d59cd2d4c5cc524d5f94ceaf62f66ba104f7
Chang Ha Lee <yielding@gmail.com>
2023-12-25 15:37:41 +0900

    ..

--- cpp.boost/asio/http.client/sync1.client.cpp
+++ cpp.boost/asio/http.client/sync1.client.cpp
@@ -7,23 +7,23 @@ using namespace boost;
 
 int main(int argc, const char *argv[])
 {
-    asio::io_service ios;
+  asio::io_service ios;
 
-    tcp::resolver resolver(ios);
-    // query는 ip, server name, port, service name 모두 다 동작하는 군.
-    tcp::resolver::query query(tcp::v4(), "218.145.28.200", "80");
-    auto iterator = resolver.resolve(query);
+  tcp::resolver resolver(ios);
+  // query는 ip, server name, port, service name 모두 다 동작하는 군.
+  tcp::resolver::query query(tcp::v4(), "218.145.28.200", "80");
+  auto iterator = resolver.resolve(query);
 
-    tcp::socket socket(ios);
-    asio::connect(socket, iterator);
-    cout << "Connected\n";
+  tcp::socket socket(ios);
+  asio::connect(socket, iterator);
+  cout << "Connected\n";
 
-    auto req = "GET\n\r";
-    asio::write(socket, asio::buffer(req, 5));
-    asio::streambuf response;
-    asio::read_until(socket, response, "\r\n\r\n");
-    if (response.size() > 0)
-        cout << &response;
+  auto req = "GET\n\r";
+  asio::write(socket, asio::buffer(req, 5));
+  asio::streambuf response;
+  asio::read_until(socket, response, "\r\n\r\n");
+  if (response.size() > 0)
+    cout << &response;
 
-    return 0;
+  return 0;
 }
--- cpp.boost/asio/http.client/sync2.client.cpp
+++ cpp.boost/asio/http.client/sync2.client.cpp
@@ -24,96 +24,97 @@ using namespace boost;
 class client 
 {
 public:
-    client() : _socket(_ios), _deadline(_ios)
-    {
-        _deadline.expires_at(posix_time::pos_infin); 
-        check_deadline();
-    }
+  client() : _socket(_ios), _deadline(_ios)
+  {
+    _deadline.expires_at(posix_time::pos_infin); 
+    check_deadline();
+  }
 
-    ~client()
-    {
-    }
+  ~client()
+  {
+  }
 
-    void connect(string const& host, string const& service, posix_time::time_duration timeout)
-    {
-        tcp::resolver::query query(tcp::v4(), host, service);
-        auto iter = tcp::resolver(_ios).resolve(query);
-        _deadline.expires_from_now(timeout);
+  void connect(string const& host, string const& service, posix_time::time_duration timeout)
+  {
+    tcp::resolver::query query(tcp::v4(), host, service);
+    auto iter = tcp::resolver(_ios).resolve(query);
+    _deadline.expires_from_now(timeout);
 
-        system::error_code ec = asio::error::would_block;
-        asio::async_connect(_socket, iter, var(ec) = _1);
+    system::error_code ec = asio::error::would_block;
+    asio::async_connect(_socket, iter, var(ec) = _1);
 
-        do _ios.run_one(); while (ec == asio::error::would_block);
-        if (ec || !_socket.is_open())
-            throw system::system_error(ec ? ec : asio::error::operation_aborted);
-    }
+    do _ios.run_one(); while (ec == asio::error::would_block);
+    if (ec || !_socket.is_open())
+      throw system::system_error(ec ? ec : asio::error::operation_aborted);
+  }
 
-    string read_all(posix_time::time_duration timeout)
-    {
-        _deadline.expires_from_now(timeout);
-        //asio::read_until(_socket, _input_buffer, "\r\n\r\n");
-        system::error_code ec = asio::error::would_block;
-        asio::async_read_until(_socket, _input_buffer, "\r\n\r\n", var(ec) = _1);
-        do _ios.run_one(); while (ec == asio::error::would_block);
-        if (ec)
-            throw system::system_error(ec);
+  string read_all(posix_time::time_duration timeout)
+  {
+    _deadline.expires_from_now(timeout);
+    //asio::read_until(_socket, _input_buffer, "\r\n\r\n");
+    system::error_code ec = asio::error::would_block;
+    asio::async_read_until(_socket, _input_buffer, "\r\n\r\n", var(ec) = _1);
+    do _ios.run_one(); while (ec == asio::error::would_block);
+    if (ec)
+      throw system::system_error(ec);
 
-        auto sz = _input_buffer.size();
-        if (sz <= 0) return "";
+    auto sz = _input_buffer.size();
+    if (sz <= 0) return "";
 
-        std::istream is(&_input_buffer);
-        char res[1024*100];
-        is.read(res, sz);
+    std::istream is(&_input_buffer);
+    char res[1024*100];
+    is.read(res, sz);
 
-        return string(res);
-    }
+    return string(res);
+  }
 
-    void write_line(string const& line, posix_time::time_duration timeout)
-    {
-        string data = line + "\n\r";
+  void write_line(string const& line, posix_time::time_duration timeout)
+  {
+    string data = line + "\n\r";
 
-        _deadline.expires_from_now(timeout);
+    _deadline.expires_from_now(timeout);
 
-        system::error_code ec = asio::error::would_block;
+    system::error_code ec = asio::error::would_block;
 
-        asio::async_write(_socket, asio::buffer(data), var(ec) = _1);
+    asio::async_write(_socket, asio::buffer(data), var(ec) = _1);
 
-        // Block until the asynchronous operation has completed.
-        do _ios.run_one(); while (ec == asio::error::would_block);
+    // Block until the asynchronous operation has completed.
+    do _ios.run_one(); while (ec == asio::error::would_block);
 
-        if (ec)
-            throw system::system_error(ec);
-    }
-
-private:
-    void check_deadline()
-    {
-        if (_deadline.expires_at() <= deadline_timer::traits_type::now())
-        {
-            system::error_code ignored_ec;
-            _socket.close(ignored_ec);
-
-            _deadline.expires_at(posix_time::pos_infin);
-        }
-
-        _deadline.async_wait(bind(&client::check_deadline, this));
-    }
+    if (ec)
+      throw system::system_error(ec);
+  }
 
 private:
-    asio::io_service _ios;
-    tcp::socket _socket;
-    asio::deadline_timer _deadline;
-    asio::streambuf _input_buffer;
+  void check_deadline()
+  {
+    if (_deadline.expires_at() <= deadline_timer::traits_type::now())
+    {
+      system::error_code ignored_ec;
+      _socket.close(ignored_ec);
+
+      _deadline.expires_at(posix_time::pos_infin);
+    }
+
+    _deadline.async_wait(bind(&client::check_deadline, this));
+  }
+
+private:
+  asio::io_service _ios;
+  tcp::socket _socket;
+  asio::deadline_timer _deadline;
+  asio::streambuf _input_buffer;
 };
-
 
 int main(int argc, const char *argv[])
 {
-    client c;
-    c.connect("218.145.28.200", "http", posix_time::seconds(10));
-    c.write_line("GET", posix_time::seconds(10));
-    auto line = c.read_all(posix_time::seconds(10));
-    cout << line;
+  client c;
 
-    return 0;
+  c.connect("218.145.28.200", "http", posix_time::seconds(10));
+  c.write_line("GET", posix_time::seconds(10));
+  auto line = c.read_all(posix_time::seconds(10));
+
+  cout << line;
+
+  return 0;
 }
