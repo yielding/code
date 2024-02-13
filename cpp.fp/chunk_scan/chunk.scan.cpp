67ef60a9124565179effcadb19b39dcd5e5377bf
Chang Ha Lee <yielding@gmail.com>
2024-02-13 12:24:15 +0900

    added some haskell code

--- cpp.fp/chunk_scan/chunk.scan.cpp
+++ cpp.fp/chunk_scan/chunk.scan.cpp
@@ -22,12 +22,13 @@ auto chunk_scan(vector<int> arr, int n, auto f) -> vector<int>
 
 int main(int argc, char* argv[])
 {
-  auto vec = vector{ 1, 2, 3, 4, 5, 6 };
+  auto vec = vector{1, 2, 3, 4, 5, 6};
   auto res = chunk_scan(vec, 2, plus{});
 
   cout << v::all(res) << endl;
-  string buffer;
 
+  string buffer;
+  format_to(back_inserter(buffer), "Hello, C++{}!\n", "20");
   cout << buffer;
 
   return 0;
--- lang.haskell/my.inits.hs
+++ lang.haskell/my.inits.hs
@@ -1,6 +1,13 @@
-myInits :: [a] -> [[a]]
-myInits = map reverse . scanl (flip (:)) []
+--                 (:) ::  a -> [a] -> [a]
+--            flip (:) :: [a] -> a -> [a]
+-- scanl (flip (:)) [] :: [a] -> [[a]]
+myInits0 :: [a] -> [[a]]
+myInits0 = scanl (flip (:)) []
+
+myInits1 :: [a] -> [[a]]
+myInits1 = map reverse . scanl (flip (:)) []
 
 main :: IO ()
-main = 
-  print $ myInits [1..5]
+main = do
+  print $ myInits0 [1..5]
+  print $ myInits1 [1..5]
