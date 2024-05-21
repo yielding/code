9ab6b81b1a2c37d31cea4f128d13ee5474aefa1e
Chang Ha Lee <yielding@gmail.com>
2024-05-22 01:51:16 +0900

    srital array from the center

---
+++ 0.algorithm/spiral.center/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+
+using namespace std;
+
+class Matrix
+{
+public:
+  explicit Matrix(int dim, int val) : _value {val}
+  {
+    for (int i=0; i<dim; i++)
+    {
+      vector<int> row(dim);
+      _board.push_back(row);
+    }
+
+    int xy = dim / 2;
+    _board[xy][xy] = val;
+  }
+
+  void go()
+  {
+    auto dim= _board.size();
+    auto cur= make_pair((dim / 2), int(dim / 2));
+    auto val = _value;
+    for (int step = 1; ; ++step)
+    {
+      cur = update_board(cur, next_dir(), step, val);
+      if (val >= dim*dim) return;
+
+      cur = update_board(cur, next_dir(), step, val);
+      if (val >= dim*dim) return;
+    }
+  }
+
+  auto next_dir() -> pair<int, int>
+  {
+    auto res = _dirs[0];
+    rotate(_dirs.begin(), _dirs.begin()+1, _dirs.end());
+
+    return res;
+  }
+
+  auto update_board(auto cur, auto dir, int step, int& v) -> pair<int, int>
+  {
+    auto [x, y] = cur;
+    auto [dx, dy] = dir;
+
+    for (int i=0; i<step; i++)
+    {
+      x += dx;
+      y += dy;
+      _board[y][x] = ++v;
+    }
+
+    return make_pair(x, y);
+  }
+
+  void print()
+  {
+    for (auto& row: _board)
+    {
+      for (auto cell: row) printf("%3d", cell);
+      cout << endl;
+    }
+
+    cout << endl;
+  }
+
+private:
+  vector<vector<int>> _board;
+  vector<pair<int,int>> _dirs { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+  int _value;
+};
+
+int main(int argc, char* argv[])
+{
+  Matrix m(7, 1);
+
+  m.print();
+  m.go();
+  m.print();
+
+  return 0;
+}
