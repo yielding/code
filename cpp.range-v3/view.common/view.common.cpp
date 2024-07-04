03c3e7503b378394a1ceaae99614121511edec3b
Chang Ha Lee <yielding@gmail.com>
2024-07-04 21:51:07 +0900

    .

--- cpp.range-v3/view.common/view.common.cpp
+++ cpp.range-v3/view.common/view.common.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <print>
 #include <vector>
 #include <algorithm>
 
@@ -14,9 +15,7 @@ int main(int argc, char* argv[])
   auto v = vector { 8, 7, 3 };
 
   auto r0 = v | v::filter([](int x) { return x > 4; });
-  auto s0 = accumulate(r0.begin(), r0.end(), 0);
-
-  cout << s0 << endl;
+  println("{}", accumulate(r0.begin(), r0.end(), 0));
 
   // NOTICE
   // common_range provides 
@@ -25,8 +24,7 @@ int main(int argc, char* argv[])
   auto r1 = v | v::take_while([](int x) { return x > 4; })
               | v::common;
 
-  auto s1 = accumulate(r1.begin(), r1.end(), 0);
-  cout << s1 << endl;
+  println("{}", accumulate(r1.begin(), r1.end(), 0));
 
   return 0;
 }
