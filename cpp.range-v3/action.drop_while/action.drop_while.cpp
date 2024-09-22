e0d2f2e1538a05703f18fb8085f5f733ab50e0f4
Chang Ha Lee <yielding@gmail.com>
2024-09-22 15:33:32 +0900

    .

--- cpp.range-v3/action.drop_while/action.drop_while.cpp
+++ cpp.range-v3/action.drop_while/action.drop_while.cpp
@@ -3,7 +3,7 @@
 
 #include <range/v3/all.hpp>
 
-using namespace ranges::v3;
+using namespace ranges;
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -16,3 +16,4 @@ int main(int argc, char* argv[])
   
   return 0;
 }
+
