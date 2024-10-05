2ece6d39217543e73aec7305cf2efe1eb6d53d7c
Chang Ha Lee <yielding@gmail.com>
2024-10-05 22:21:56 +0900

    ..

--- cpp.range-v3/view.istream/view.istream.cpp
+++ cpp.range-v3/view.istream/view.istream.cpp
@@ -11,10 +11,13 @@ int main(int argc, char *argv[])
 {
   using v::take, v::all, g::istream;
 
-  auto ss = stringstream{"a b c"};
-  auto rs = istream<char>(ss) | take(2);
+  auto s0 = stringstream{"a b c"};
+  auto r0 = istream<char>(s0) | take(2);
+  cout << all(r0) << endl;
 
-  cout << all(rs);
+  auto s1 = stringstream{"1 23 456"};
+  auto r1 = istream<int>(s1) | take(2);
+  cout << all(r1) << endl;
 
   return 0;
 }
