1cae3e6a0824d34db1f34048646279165871637f
Chang Ha Lee <yielding@gmail.com>
2024-10-31 12:45:36 +0900

    ..

--- ffmpeg/transcode.rb
+++ ffmpeg/transcode.rb
@@ -2,6 +2,8 @@
 
 require "streamio-ffmpeg"
 
+FFMPEG.ffmpeg_binary='/opt/homebrew/bin/ffmpeg'
+
 movie = FFMPEG::Movie.new("/Users/yielding/work/IMG_4164.mov")
 movie.transcode("movie.mp4") { |progress|
   puts progress
