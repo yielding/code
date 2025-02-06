6be5b864bd0a208b62fa247b42f4cf0974b149ae
Chang Ha Lee <yielding@gmail.com>
2025-02-06 09:31:49 +0900

    clarified entent a little more

--- design.patterns/mediator/main.cpp
+++ design.patterns/mediator/main.cpp
@@ -1,9 +1,6 @@
 #include <print>
 #include <vector>
 
-//#include <range/v3/view/filter.hpp>
-// namespace v = ranges::views;
-
 using namespace std;
 
 class ControlCenter;
@@ -30,16 +27,12 @@ public:
   virtual void notify(AirCraft& c, int event) = 0;
 };
 
-class Airport : public ControlCenter
+class Airport: public ControlCenter
 {
 public:
   void notify(AirCraft& c, int event) override
   {
     print("AirCraft(id: {}) took off with {}\n", c.id(), event);
-
-    //auto plains = _aircrafts | v::filter([&c](auto p) { return c.id() != p->id(); });
-    //for (auto plain: plains) 
-    //  plain->message_from_center(format("AirCraft {}, be careful", plain->id()));
 
     for (auto plain: _aircrafts)
     {
@@ -57,7 +50,7 @@ private:
   vector<AirCraft*> _aircrafts;
 };
 
-class Drone : public AirCraft
+class Drone: public AirCraft
 { 
 public:
   Drone(ControlCenter& center) : AirCraft(center) {}
@@ -82,12 +75,12 @@ public:
 
 int main(int argc, char* argv[])
 {
-  Airport center;
-  Drone c1(center);
-  AirPlane c2(center);
+  Airport airport;
+  Drone c1(airport);
+  AirPlane c2(airport);
 
-  center.add(&c1);
-  center.add(&c2);
+  airport.add(&c1);
+  airport.add(&c2);
 
   c1.take_off();
   c2.take_off();
