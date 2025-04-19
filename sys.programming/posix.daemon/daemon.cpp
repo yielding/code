ee63a976f0f406c85387ad94c98dac8711a61455
Chang Ha Lee <yielding@gmail.com>
2025-04-19 22:22:49 +0900

    i added additional understanding of daemon

--- sys.programming/posix.daemon/daemon.cpp
+++ sys.programming/posix.daemon/daemon.cpp
@@ -1,14 +1,16 @@
 #include <signal.h>
 #include <unistd.h>
 #include <syslog.h>
+#include <fcntl.h>
 
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/file.h>
 
 #include <cstdio>
 #include <cstdlib>
 #include <string>
-#include <iostream>
+#include <print>
 
 using namespace std;
 
@@ -27,13 +29,21 @@ void daemon_init(const char *pname, int facility)
   umask(0);
   for (int i=0; i<MAXFD; i++) close(i);
 
+  int fd = open("/var/run/mydaemon.lock", O_CREAT | O_RDWR, 0666);
+  if (flock(fd, LOCK_EX | LOCK_NB) == -1) 
+  {
+    println("already instance is running..");
+    exit(1);
+  }
+
+  println("daomon successfully launched..");
+
   openlog(pname, LOG_PID, facility);
 }
 
 void daemon_init2()
 {
-  pid_t pid;
-  if ((pid = fork()) != 0) 
+  if (pid_t pid; (pid = fork()) != 0) 
     exit(0);
 
   setsid();
@@ -48,36 +58,26 @@ void daemon_init2()
 
 int main(int argc, char const* argv[])
 {
-  for (int i = 0; i < 10; ++i) 
+  if (argc < 2)
   {
-    openlog("hi", LOG_PID, LOG_USER);
-    sleep(1);
-    syslog(LOG_NOTICE, "test.syslog");
-    cout << "test.syslog" << endl;
-    closelog();
+    for (;;)
+    {
+      sleep(1);
+      println("sleep...");  
+    }
+
+    return 1;
   }
-  /*
-     if (argc < 2)
-     {
-     for (;;)
-     {
-     sleep(1);
-     cout << "sleep ...\n";
-     }
 
-     return 1;
-     }
-
-     if (string(argv[1]) == "daemon")
-     {
-     daemon_init("program-xxx", LOG_USER);
-     while (true)
-     {
-     sleep(1);
-     syslog(LOG_INFO, "test daemon");
-     }
-     }
-     */
+  if (string(argv[1]) == "daemon")
+  {
+    daemon_init("program-xxx", LOG_USER);
+    while (true)
+    {
+      sleep(1);
+      syslog(LOG_INFO, "test daemon");
+    }
+  }
 
   return 0;
 }
