7f77376cbdc965b405c5b0ba906bb29324790402
Chang Ha Lee <yielding@gmail.com>
2025-04-21 20:56:51 +0900

    .

---
+++ cpp.boost/serialize.png/driver.cpp
@@ -0,0 +1,32 @@
+#include "cv/png.serialize.hpp"
+#include <print>
+#include <cstdlib>
+
+using namespace std;
+using namespace cv::serializer;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+  auto home = getenv("HOME");
+  auto file = argc == 1 ? "1.jpeg" : argv[1];
+  auto img  = cv::imread(format("{}/Desktop/{}", home, file));
+  auto compressed = png::serialize(img);
+
+  try
+  {
+    auto img2 = png::deserialize(compressed);
+    cv::imshow("Ex1", img2);
+    cv::waitKey(0);
+  }
+  catch (exception& e)
+  {
+    println("Error: {}", e.what());
+  }
+
+  return 0;
+}
