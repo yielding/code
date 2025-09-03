bb86b48ba9d56c60327027b744e06c4298da79c5
Chang Ha Lee <yielding@gmail.com>
2025-09-03 21:03:33 +0900

    .

--- cpp.idioms/memoize/memoized.recursion.cpp
+++ cpp.idioms/memoize/memoized.recursion.cpp
@@ -8,51 +8,49 @@
 using namespace std;
 
 template <typename ReturnType, typename... Args>
-std::function<ReturnType (Args...)>
-memoize(ReturnType (*func) (Args...))
+auto memoize(ReturnType (*func) (Args...)) -> function<ReturnType (Args...)>
 {
-    auto cache = make_shared<map<tuple<Args...>, ReturnType>>();
-    return (
-        [=](Args... args) mutable {
-            tuple<Args...> t(args...);
+  auto cache = make_shared<map<tuple<Args...>, ReturnType>>();
+  return ([=](Args... args) mutable {
+    tuple<Args...> t(args...);
 
-            if (cache->find(t) == cache->end()) (*cache)[t] = func(args...);
+    if (cache->find(t) == cache->end()) 
+        (*cache)[t] = func(args...);
 
-            return (*cache)[t];
-        }
-    );
+    return (*cache)[t];
+  });
 }
 
 template <typename F_ret, typename...  F_args>
 function<F_ret (F_args...)>
 memoized_recursion(F_ret (*func)(F_args...))
 {
-    typedef function<F_ret (F_args...)> FunctionType;
-    static unordered_map<decltype(func), FunctionType> functor_map;
+  typedef function<F_ret (F_args...)> FunctionType;
+  static map<decltype(func), FunctionType> functor_map;
 
-    if (functor_map.find(func) == functor_map.end())
-        functor_map[func] = memoize(func);
+  if (functor_map.find(func) == functor_map.end())
+    functor_map[func] = memoize(func);
 
-    return functor_map[func];
+  return functor_map[func];
 }
 
 unsigned long fibonacci(unsigned n)
 {
-    return (n < 2) ? n :
-        memoized_recursion(fibonacci)(n - 1) +
-        memoized_recursion(fibonacci)(n - 2);
+  return (n < 2) ? n :
+    memoized_recursion(fibonacci)(n - 1) +
+    memoized_recursion(fibonacci)(n - 2);
 }
 
 unsigned long fibonacci_slow(unsigned n)
 {
-    return (n < 2) ? n :  fibonacci_slow(n - 1) + fibonacci_slow(n - 2);
+  return (n < 2) ? n :  fibonacci_slow(n - 1) + fibonacci_slow(n - 2);
 }
 
 int main(int argc, char *argv[])
 {
-    unsigned count = 3000;
-    cout << memoized_recursion(fibonacci)(count) << endl;
-    cout << fibonacci(count) << endl;
+  unsigned count = 30000;
+  cout << memoized_recursion(fibonacci)(count) << endl;
+  cout << fibonacci(count) << endl;
 
-    return 0;
+  return 0;
 }
