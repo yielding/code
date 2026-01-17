2cab88b85a81480a05d7ded3d0399909ddd97466
Chang Ha Lee <yielding@gmail.com>
2026-01-18 01:48:40 +0900

    transactional lock

--- sys.programming/lock.demo/main.cpp
+++ sys.programming/lock.demo/main.cpp
@@ -1,131 +1,175 @@
 // Simple lock manager demo with S, IS, IX, SIX, X and compatibility matrix
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <string.h>
+#include <array>
+#include <chrono>
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <vector>
 
-typedef enum { IS=0, IX, S, SIX, X, LOCK_MODE_COUNT } lock_mode_t;
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+namespace core
+{
+  using namespace std;
 
-const char *mode_name[] = { "IS", "IX", "S", "SIX", "X" };
+  auto tid_str() -> string
+  {
+    ostringstream oss;
+    oss << this_thread::get_id();
+    return oss.str();
+  }
 
-// compatibility[requesting][existing]
-int compatibility[LOCK_MODE_COUNT][LOCK_MODE_COUNT] = {
-    /* existing: IS IX  S  SIX X */
-    /* IS */ { 1,  1,  1,  1,  0 },
-    /* IX */ { 1,  1,  0,  0,  0 },
-    /* S  */ { 1,  0,  1,  0,  0 },
-    /* SIX*/ { 1,  0,  0,  0,  0 },
-    /* X  */ { 0,  0,  0,  0,  0 }
-};
+  enum class lock_mode { IS=0, IX, S, SIX, X, count };
 
-#define MAX_LOCKS 32
+  constexpr array mode_names = { "IS"sv, "IX"sv, "S"sv, "SIX"sv, "X"sv };
 
-typedef struct {
-    pthread_t owner;
-    lock_mode_t mode;
-} held_lock_t;
+  // compatibility[requesting][existing]
+  constexpr array<array<bool, 5>, 5> compatibility = {{
+    /* existing: IS    IX     S     SIX    X  */
+    /* IS  */ {{ true,  true,  true,  true,  false }},
+    /* IX  */ {{ true,  true,  false, false, false }},
+    /* S   */ {{ true,  false, true,  false, false }},
+    /* SIX */ {{ true,  false, false, false, false }},
+    /* X   */ {{ false, false, false, false, false }}
+  }};
 
-typedef struct {
-    const char *name;
-    pthread_mutex_t mtx;
-    pthread_cond_t  cv;
-    held_lock_t held[MAX_LOCKS];
-    int held_count;
-} resource_t;
+  constexpr int max_locks = 32;
 
-void resource_init(resource_t *r, const char *name) {
-    r->name = name;
-    pthread_mutex_init(&r->mtx, NULL);
-    pthread_cond_init(&r->cv, NULL);
-    r->held_count = 0;
-}
+  struct held_lock
+  {
+    thread::id owner;
+    lock_mode mode;
+  };
 
-int is_compatible(lock_mode_t req, resource_t *r) {
-    for (int i = 0; i < r->held_count; ++i) {
-        if (!compatibility[req][r->held[i].mode]) return 0;
+  class resource
+  {
+  public:
+    resource(const string_view name)
+      : _name(name)
+    {
     }
-    return 1;
-}
 
-void acquire_lock(resource_t *r, lock_mode_t mode) {
-    pthread_mutex_lock(&r->mtx);
-    while (!is_compatible(mode, r)) {
-        // wait until some other lock is released
-        pthread_cond_wait(&r->cv, &r->mtx);
-    }
-    if (r->held_count < MAX_LOCKS) {
-        r->held[r->held_count].owner = pthread_self();
-        r->held[r->held_count].mode  = mode;
-        r->held_count++;
-        printf("[%lu] Acquired %s on %s (held=%d)\n",
-               (unsigned long)pthread_self(), mode_name[mode], r->name, r->held_count);
-    } else {
-        fprintf(stderr, "lock table full\n");
-    }
-    pthread_mutex_unlock(&r->mtx);
-}
+  public:
+    auto acquire(const lock_mode mode) -> void
+    {
+      unique_lock lock(_mtx);
 
-void release_locks_by_owner(resource_t *r) {
-    pthread_mutex_lock(&r->mtx);
-    pthread_t me = pthread_self();
-    int write_idx = 0;
-    for (int i = 0; i < r->held_count; ++i) {
-        if (!pthread_equal(r->held[i].owner, me)) {
-            r->held[write_idx++] = r->held[i];
-        } else {
-            printf("[%lu] Released %s on %s\n", (unsigned long)me, mode_name[r->held[i].mode], r->name);
+      _cv.wait(lock, [this, mode] { return is_compatible(mode); });
+
+      if (_held.size() < max_locks)
+      {
+        _held.push_back({ this_thread::get_id(), mode });
+        cout << "[" << tid_str() << "] Acquired "
+             << mode_names[static_cast<int>(mode)]
+             << " on " << _name
+             << " (held=" << _held.size() << ")\n";
+      }
+      else
+      {
+        cerr << "lock table full\n";
+      }
+    }
+
+    auto release_by_owner() -> void
+    {
+      lock_guard lock(_mtx);
+
+      auto me = this_thread::get_id();
+      auto it = _held.begin();
+
+      while (it != _held.end())
+      {
+        if (it->owner == me)
+        {
+          cout << "[" << tid_str() << "] Released "
+               << mode_names[static_cast<int>(it->mode)]
+               << " on " << _name << "\n";
+          it = _held.erase(it);
         }
+        else
+        {
+          ++it;
+        }
+      }
+
+      _cv.notify_all();
     }
-    r->held_count = write_idx;
-    pthread_cond_broadcast(&r->cv);
-    pthread_mutex_unlock(&r->mtx);
-}
 
-// Demo scenario:
-// Reader: acquire IS on table, then S on row, hold, then release
-// Writer: acquire IX on table, then X on row, hold, then release
-resource_t table_res, row_res;
+  private:
+    auto is_compatible(const lock_mode req) const -> bool
+    {
+      for (auto const& h : _held)
+        if (!compatibility[static_cast<int>(req)][static_cast<int>(h.mode)])
+          return false;
 
-void *reader_thread(void *arg) {
-    (void)arg;
-    acquire_lock(&table_res, IS);
-    usleep(100 * 1000); // simulate slight delay
-    acquire_lock(&row_res, S);
-    // simulate reading
-    printf("[%lu] Reader: reading...\n", (unsigned long)pthread_self());
-    sleep(2);
-    // release row and table locks
-    release_locks_by_owner(&row_res);
-    release_locks_by_owner(&table_res);
-    return NULL;
-}
+      return true;
+    }
 
-void *writer_thread(void *arg) {
-    (void)arg;
+  private:
+    string _name;
+    mutex _mtx;
+    condition_variable _cv;
+    vector<held_lock> _held;
+  };
+
+  // Demo scenario:
+  // Reader: acquire IS on table, then S on row, hold, then release
+  // Writer: acquire IX on table, then X on row, hold, then release
+  resource table_res{"table_users"};
+  resource row_res{"row_42"};
+
+  auto reader_thread() -> void
+  {
+    table_res.acquire(lock_mode::IS);
+    this_thread::sleep_for(chrono::milliseconds(100));
+    row_res.acquire(lock_mode::S);
+
+    cout << "[" << tid_str() << "] Reader: reading...\n";
+    this_thread::sleep_for(chrono::seconds(2));
+
+    row_res.release_by_owner();
+    table_res.release_by_owner();
+  }
+
+  auto writer_thread() -> void
+  {
     // start slightly after reader to show interleaving
-    usleep(200 * 1000);
-    acquire_lock(&table_res, IX);
-    usleep(50 * 1000);
-    acquire_lock(&row_res, X);
-    printf("[%lu] Writer: writing...\n", (unsigned long)pthread_self());
-    sleep(2);
-    release_locks_by_owner(&row_res);
-    release_locks_by_owner(&table_res);
-    return NULL;
+    this_thread::sleep_for(chrono::milliseconds(200));
+    table_res.acquire(lock_mode::IX);
+    this_thread::sleep_for(chrono::milliseconds(50));
+    row_res.acquire(lock_mode::X);
+
+    cout << "[" << tid_str() << "] Writer: writing...\n";
+    this_thread::sleep_for(chrono::seconds(2));
+
+    row_res.release_by_owner();
+    table_res.release_by_owner();
+  }
 }
 
-int main() {
-    resource_init(&table_res, "table_users");
-    resource_init(&row_res, "row_42");
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+auto main() -> int
+{
+  using namespace core;
 
-    pthread_t r1, r2;
-    pthread_create(&r1, NULL, reader_thread, NULL);
-    pthread_create(&r2, NULL, writer_thread, NULL);
+  thread r1(reader_thread);
+  thread r2(writer_thread);
 
-    pthread_join(r1, NULL);
-    pthread_join(r2, NULL);
+  r1.join();
+  r2.join();
 
-    printf("Done\n");
-    return 0;
+  cout << "Done\n";
+
+  return 0;
 }
