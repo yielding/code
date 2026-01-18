2cd82084d9285303af3a68296d5ec1b7b6b9b4fb
Chang Ha Lee <yielding@gmail.com>
2026-01-18 16:45:36 +0900

    updated rust

--- lang.rust/minigrep/src/main.rs
+++ lang.rust/minigrep/src/main.rs
@@ -4,16 +4,16 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let config = Config::build(env::args()).unwrap_or_else( |err| {
-        println!("Problem arsisng arguments: {err}");
-        process::exit(1);
-    });
+  let config = Config::build(env::args()).unwrap_or_else( |err| {
+    println!("Problem arsisng arguments: {err}");
+    process::exit(1);
+  });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+  println!("Searching for {}", config.query);
+  println!("In file {}", config.file_path);
 
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
-    }
+  if let Err(e) = minigrep::run(config) {
+    eprintln!("Application error: {e}");
+    process::exit(1);
+  }
 }
--- lang.rust/webserver/src/lib.rs
+++ lang.rust/webserver/src/lib.rs
@@ -1,87 +1,85 @@
 use std::{
-    sync::{mpsc, Arc, Mutex},
-    thread,
+  sync::{mpsc, Arc, Mutex},
+  thread,
 };
 
 pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: Option<mpsc::Sender<Job>>,
+  workers: Vec<Worker>,
+  sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
 
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
+  pub fn new(size: usize) -> ThreadPool {
+    assert!(size > 0);
 
-        let (sender, receiver) = mpsc::channel();
+    let (sender, receiver) = mpsc::channel();
 
-        let receiver = Arc::new(Mutex::new(receiver));
+    let receiver = Arc::new(Mutex::new(receiver));
 
-        let mut workers = Vec::with_capacity(size);
+    let mut workers = Vec::with_capacity(size);
 
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
-        }
-
-        ThreadPool { 
-            workers, 
-            sender: Some(sender),
-        }
+    for id in 0..size {
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
 
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static
-    {
-        let job = Box::new(f);
-
-        self.sender.as_ref().unwrap().send(job).unwrap();
+    ThreadPool { 
+      workers, 
+      sender: Some(sender),
     }
+  }
+
+  pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static
+  {
+    let job = Box::new(f);
+
+    self.sender.as_ref().unwrap().send(job).unwrap();
+  }
 }
 
 impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        drop(self.sender.take());
+  fn drop(&mut self) {
+    drop(self.sender.take());
 
-        for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
+    for worker in &mut self.workers {
+      println!("Shutting down worker {}", worker.id);
 
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
-            }
-        }
+      if let Some(thread) = worker.thread.take() {
+        thread.join().unwrap();
+      }
     }
+  }
 }
 
 struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
+  id: usize,
+  thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let message = receiver.lock().unwrap().recv();
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    let thread = thread::spawn(move || loop {
+      let message = receiver.lock().unwrap().recv();
 
-            match message {
-                Ok(job) => {
-                    println!("Worker {id} get a job; executing.");
+      match message {
+        Ok(job) => {
+          println!("Worker {id} get a job; executing.");
 
-                    job();
-                }
-                Err(_) => {
-                    println!("Worker {id} disconnected; shutting down.");
-                    break;
-                }
-            }
-        });
-
-        Worker {
-            id,
-            thread: Some(thread),
+          job();
         }
+        Err(_) => {
+          println!("Worker {id} disconnected; shutting down.");
+          break;
+        }
+      }
+    });
+
+    Worker {
+      id,
+      thread: Some(thread),
     }
+  }
 }
 
