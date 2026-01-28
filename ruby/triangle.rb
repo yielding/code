2f92a967169332efe2eedd473b43cb870bd913ea
Chang Ha Lee <yielding@gmail.com>
2026-01-28 22:27:52 +0900

    .

--- ruby/triangle.rb
+++ ruby/triangle.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
-# encoding: utf-8
 
 size = 20
 
-1.upto(size) { |n|
-  1.upto(size - n) { |k| print " " }
-  1.upto(2*n - 1) { |k| print "*" }
-  puts ""
-}
+1.upto(size) do |n|
+  1.upto(size - n)  { print ' ' }
+  1.upto(2 * n - 1) { print '*' }
+  puts ''
+end
