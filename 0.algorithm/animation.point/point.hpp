8cbed4214671ec93a41895126825129736fdecfa
Chang Ha Lee <yielding@gmail.com>
2026-02-01 21:37:56 +0900

    .

--- 0.algorithm/animation.point/point.hpp
+++ 0.algorithm/animation.point/point.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <string>
 #include <cmath>
 #include <format>
+#include <string>
 
 ////////////////////////////////////////////////////////////////////////////////
 ///
@@ -11,40 +11,42 @@
 ////////////////////////////////////////////////////////////////////////////////
 struct Point
 {
-  Point(double x_, double y_)
-    : x{x_}, y{y_}
-  {}
+  Point(double x, double y)
+    : _x{x} , _y{y}
+  {
+  }
 
   auto translate(double x0, double y0) const -> Point
   {
-    return Point(x + x0, y + y0);
+    return Point(_x + x0, _y + y0);
   }
 
   auto reversed() const -> Point
   {
-    return Point(-x, -y);
+    return Point(-_x, -_y);
   }
 
   auto make_rel_to(const Point& p) -> Point&
   {
-    x -= p.x; y -= p.y;
+    _x -= p._x;
+    _y -= p._y;
 
     return *this;
   }
 
   auto rel_to(const Point& p) const -> Point
   {
-    return Point(x - p.x, y - p.y);
+    return Point(_x - p._x, _y - p._y);
   }
 
   auto dist() const -> double
   {
-    return hypot(x, y);
+    return hypot(_x, _y);
   }
 
   auto dist_to(const Point& p) const -> double
   {
-    return hypot(x - p.x, y - p.y);
+    return hypot(_x - p._x, _y - p._y);
   }
 
   auto area2(const Point& p0, const Point& p1) const -> double
@@ -54,22 +56,22 @@ struct Point
 
   auto inner(const Point& p) const -> double
   {
-    return x * p.x + y * p.y;
+    return _x * p._x + _y * p._y;
   }
 
   auto cross(const Point& p) const -> double
   {
-    return x * p.y - y * p.x;
+    return _x * p._y - _y * p._x;
   }
 
   auto is_lower(const Point& p) const -> bool
   {
-    return y < p.y || y == p.y && x < p.x;
+    return _y < p._y || (_y == p._y && _x < p._x);
   }
 
   auto mdist_() const -> double
   {
-    return abs(x) + abs(y);
+    return std::abs(_x) + std::abs(_y);
   }
 
   auto mdist(const Point& p) const -> double
@@ -100,17 +102,17 @@ struct Point
     return f < 0 || (f == 0 && !is_between(p0, p1));
   }
 
-  auto operator == (const Point& rhs) -> bool
+  auto operator==(const Point& rhs) -> bool
   {
-    return x == rhs.x && y == rhs.y;
+    return _x == rhs._x && _y == rhs._y;
   }
 
   auto to_s() const -> std::string
   {
-    return std::format("(x, y) = ({}, {})", x, y);
+    return std::format("(x, y) = ({}, {})", _x, _y);
   }
 
-  double x, y;
+  double _x, _y;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
